59771a7646158d520f9eb4f707ca47f30ef80c63
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-07 10:41:26 -0100

    Linked List Cycle

---
+++ Linked List/LinkedListCycleSolution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slowRunner = head, *fastRunner = head;
+        
+        for(;slowRunner != fastRunner; slowRunner->next, fastRunner->next->next)
+            if(fastRunner == NULL)
+                false;
+        return true;  
+    }
+};
