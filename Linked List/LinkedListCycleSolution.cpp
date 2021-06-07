1a03221b0b6d9c7c8ee12f1f5022cd6903cd7057
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-07 11:54:29 -0100

    Done

--- Linked List/LinkedListCycleSolution.cpp
+++ Linked List/LinkedListCycleSolution.cpp
@@ -16,16 +16,43 @@ public:
     bool hasCycle(ListNode *head) {
 
         if (head == NULL || head->next == NULL)
-        return false;
+            return false;
 
         ListNode *slowRunner = head, *fastRunner = head;
         
-        for(;fastRunner->next != NULL; slowRunner = slowRunner->next, fastRunner = fastRunner->next->next)
+        for(;fastRunner != NULL && fastRunner->next != NULL;){
+            slowRunner = slowRunner->next; fastRunner = fastRunner->next->next;
             if(slowRunner == fastRunner)
                 return true;
-        return false;  
+        }
+        return false;
+    }
+
+    bool hasCycle(ListNode *head) {
+        
+        if (!head || !head->next)
+            return false;       
+        
+        for(ListNode *slowRunner = head, *fastRunner = head->next->next; fastRunner && fastRunner->next; 
+            slowRunner = slowRunner->next, fastRunner = fastRunner->next->next)
+            if(slowRunner == fastRunner)
+                return true;
+        return false; 
     }
 };
+
+// if (head == NULL || head->next == NULL)
+//         return false;
+
+//         ListNode *slowRunner = head, *fastRunner = head;
+        
+//         while(fastRunner && fastRunner->next){
+//             slowRunner = slowRunner->next;
+//             fastRunner = fastRunner->next->next;
+//             if(slowRunner == fastRunner)
+//                 return true;
+//         }
+//         return false; 
 
 int main(){
     ListNode *head = new ListNode(1);
