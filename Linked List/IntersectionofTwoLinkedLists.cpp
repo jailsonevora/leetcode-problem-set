f5eb4da06422658ada7fbbfcc4a0d31bb5ee64b9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-07 16:30:05 -0100

    Intersection of Two Linked Lists

---
+++ Linked List/IntersectionofTwoLinkedLists.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * */
+struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(NULL) {}
+    };
+
+class Solution {    
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+        if (!head || !head->next)
+            return NULL;
+        
+        ListNode *f = head, *s = head;
+        int si, fi = 0;
+        
+        while(f && f->next){
+            s = s->next;
+            f = f->next->next;       
+            if(s == f) break;
+        }
+        
+        if (s != f)
+            return NULL;
+
+        s = head;
+        while (s != f) {
+            s = s->next;
+            f = f->next;
+        }    
+        return s;
+    }
+};
+
+int main(){
+    Solution s;
+
+    // headA
+    ListNode *headA = new ListNode(4);
+    headA->next = new ListNode(1);
+    headA->next->next = new ListNode(8);
+    headA->next->next->next = new ListNode(4);
+    headA->next->next->next->next = new ListNode(5);
+
+    //headB
+    ListNode *headB = new ListNode(5);
+    headB->next = new ListNode(6);
+    headB->next->next = new ListNode(1);
+    headB->next->next->next = headA->next->next;
+    headB->next->next->next = headA->next->next->next;
+    headB->next->next->next->next = headA->next->next->next->next;
+ 
+    ListNode* res = s.getIntersectionNode(headA, headB);
+
+    if (res == NULL)
+        cout << "The two lists do not intersect";
+    else
+        cout << "The intersected node's value is " << res->val;
+    return 0;
+}
+
