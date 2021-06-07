1d525681dfcec83f6c719f772f7127c14e7315b1
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-07 12:10:26 -0100

    more efficient with while

--- Linked List/LinkedListCycleSolution.cpp
+++ Linked List/LinkedListCycleSolution.cpp
@@ -14,45 +14,15 @@ struct ListNode {
 class Solution {    
 public:
     bool hasCycle(ListNode *head) {
-
-        if (head == NULL || head->next == NULL)
-            return false;
-
-        ListNode *slowRunner = head, *fastRunner = head;
-        
-        for(;fastRunner != NULL && fastRunner->next != NULL;){
-            slowRunner = slowRunner->next; fastRunner = fastRunner->next->next;
-            if(slowRunner == fastRunner)
-                return true;
+        ListNode *f = head, *s = head;
+        while(f && f->next){
+            s = s->next;
+            f = f->next->next;            
+            if(s == f) return true;
         }
         return false;
     }
-
-    bool hasCycle(ListNode *head) {
-        
-        if (!head || !head->next)
-            return false;       
-        
-        for(ListNode *slowRunner = head, *fastRunner = head->next->next; fastRunner && fastRunner->next; 
-            slowRunner = slowRunner->next, fastRunner = fastRunner->next->next)
-            if(slowRunner == fastRunner)
-                return true;
-        return false; 
-    }
 };
-
-// if (head == NULL || head->next == NULL)
-//         return false;
-
-//         ListNode *slowRunner = head, *fastRunner = head;
-        
-//         while(fastRunner && fastRunner->next){
-//             slowRunner = slowRunner->next;
-//             fastRunner = fastRunner->next->next;
-//             if(slowRunner == fastRunner)
-//                 return true;
-//         }
-//         return false; 
 
 int main(){
     ListNode *head = new ListNode(1);
