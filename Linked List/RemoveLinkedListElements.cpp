4642d6f2a0641ae793d723aa88f8b56990e9c836
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-08 15:12:27 -0100

    Remove Linked List Elements

--- Linked List/RemoveLinkedListElements.cpp
+++ Linked List/RemoveLinkedListElements.cpp
@@ -8,6 +8,13 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
