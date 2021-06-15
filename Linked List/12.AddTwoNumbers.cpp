49207035ddcdd0a8ea292636ce049076ef3b2ab0
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-15 09:23:25 -0100

    add two numbers

--- Linked List/12.AddTwoNumbers.cpp
+++ Linked List/12.AddTwoNumbers.cpp
@@ -8,6 +8,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
