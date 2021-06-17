3c399f88b00f3d68dbf851bf541a27d3aab7634a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-17 13:33:37 -0100

    main

--- Linked List/15.RotateList.cpp
+++ Linked List/15.RotateList.cpp
@@ -12,9 +12,28 @@
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *prev;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         
     }
 };
+
+int main(){
+    
+    Solution l1;
+    l1.pushBack(1);
+    l1.pushBack(2);
+    l1.pushBack(3);
+    l1.pushBack(4);
+    l1.pushBack(5);
+
+    l1.rotateRight(l1.head);
+}
