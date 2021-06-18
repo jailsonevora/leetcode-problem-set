cfc7e13d83f424b434e353f9d92d3154b9639427
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-18 11:28:38 -0100

    rotate list

--- Linked List/15.RotateList.cpp
+++ Linked List/15.RotateList.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 struct ListNode {
     int val;
-    ListNode *prev;
     ListNode *next;
     ListNode(int x) : val(x), next(nullptr) {}
 };
@@ -26,21 +25,38 @@ public:
 
     ListNode* rotateRight(ListNode* head, int k) {
 
+        // ListNode *temp = new ListNode(-1);
+        // temp->next = head;
         ListNode *second = head, *first = head;
-        
-        while (k-- > 0){
-            if(!first->next) first = head; 
-            first = first->next;
-        }
 
-        if (!first) return head->next;
+        if (k == 0 || !head->next)
+            return head;
+        if (!head)
+            return nullptr;     
+               
+        while (k-- > 0){
+
+            if(!first->next) 
+                first = head;
+            else
+                first = first->next;
+        }
 
         while (first->next){
-            if(!first->next) first = head;
-            if(!second->next) second = head;
-            first = first->next;
-            second = second->next; 
+
+            if(!first->next) 
+                first = head;
+            else
+                first = first->next;
+
+            if(!second->next) 
+                second = head;
+            else
+                second = second->next; 
         }
+
+        if(!second->next)
+            second->next = head;
 
         ListNode *remain = second->next;
         second->next = nullptr;
@@ -69,10 +85,15 @@ public:
 int main(){
     
     Solution l1;
-    
-    l1.pushBack(0);
     l1.pushBack(1);
     l1.pushBack(2);
+    l1.pushBack(3);
+    l1.pushBack(4);
+    l1.pushBack(5);
+    
+    // l1.pushBack(0);
+    // l1.pushBack(1);
+    // l1.pushBack(2);
 
-    l1.rotateRight(l1.head, 4);
+    l1.rotateRight(l1.head, 2);
 }
