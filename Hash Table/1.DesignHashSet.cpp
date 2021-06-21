eca515ca841283918a099845d392166a422ff651
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-21 11:27:37 -0100

    hash set

--- Hash Table/1.DesignHashSet.cpp
+++ Hash Table/1.DesignHashSet.cpp
@@ -1,25 +1,42 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
 class MyHashSet {
+    int size;
+    vector<vector<int>> buckets;
+
+    int hashFunction(int key){
+        return key % size;
+    }
 public:
     /** Initialize your data structure here. */
     MyHashSet() {
-        
+        size = 15000;
+        buckets = vector<vector<int>>(size, vector<int>{});        
     }
     
     void add(int key) {
-        
+        int i = hashFunction(key);
+        if(find(buckets[i].begin(), buckets[i].end(), key) == buckets[i].end())
+            buckets[i].push_back(key);                
     }
     
     void remove(int key) {
-        
+        int i = hashFunction(key);
+        auto it = find(buckets[i].begin(), buckets[i].end(), key);
+        if (it != buckets[i].end())
+            buckets[i].erase(it);  
     }
     
     /** Returns true if this set contains the specified element */
     bool contains(int key) {
-        
+        int i = hashFunction(key);
+        if(find(buckets[i].begin(), buckets[i].end(), key) != buckets[i].end())
+            return false;
+        return true;  
     }
 };
 
