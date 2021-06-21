c58612825d51428d7a1d523e41cfbf560dc52d4e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-21 15:22:56 -0100

    hash map done

--- Hash Table/2.DesignHashMap.cpp
+++ Hash Table/2.DesignHashMap.cpp
@@ -8,22 +8,22 @@ public:
     vector<int> hashMap;
     /** Initialize your data structure here. */
     MyHashMap() {
-        hashMap = vector<int>(1000001, false);
+        hashMap = vector<int>(1000001, -1);
     }
     
     /** value will always be non-negative. */
     void put(int key, int value) {
-        hashMap[key] = value;
+        hashMap[key % 99510] = value;
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        return hashMap[key]; 
+        return hashMap[key % 99510];
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        hashMap[key] = -1;
+        hashMap[key % 99510] = -1;
     }
 };
 
