dad55dc55f7e8e24281b2a8f810086b3137fff0c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-21 13:28:02 -0100

    hash map

--- Hash Table/2.DesignHashMap.cpp
+++ Hash Table/2.DesignHashMap.cpp
@@ -5,24 +5,25 @@ using namespace std;
 
 class MyHashMap {
 public:
+    vector<int> hashMap;
     /** Initialize your data structure here. */
     MyHashMap() {
-        
+        hashMap = vector<int>(1000001, false);
     }
     
     /** value will always be non-negative. */
     void put(int key, int value) {
-        
+        hashMap[key] = value;
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        
+        return hashMap[key]; 
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        
+        hashMap[key] = -1;
     }
 };
 
