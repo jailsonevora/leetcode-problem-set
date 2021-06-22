d4a5e61bdf8579edb3a10dc8df1889e7d2398bd0
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-22 11:14:20 -0100

    single number

--- Hash Table/4.SingleNumber.cpp
+++ Hash Table/4.SingleNumber.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<unordered_set>
+#include<unordered_map>
 #include<vector>
 
 using namespace std;
 
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
+    int singleNumberN_SET(vector<int>& nums) {
         unordered_set<int> single;
         for (int it : nums)
         {   
@@ -16,6 +17,24 @@ public:
                 single.insert(it);
         }
         return *single.begin();
+    }
+
+    int singleNumberN_MAP(vector<int>& nums) {
+        unordered_map<int, int> single;
+        for (int it : nums)
+            single[it] = single[it]+1;
+
+        for (int it : nums)
+            if(single[it] == 1)
+                return it;
+        return 0;
+    }
+
+    int singleNumberBitWise(vector<int>& nums) {
+        int a = 0;
+        for (int it : nums)
+            a ^= it;
+        return a;
     }
 };
 
@@ -28,5 +47,5 @@ int main(){
     vector<int> n = {4,1,2,1,2};
 
     Solution s;
-    cout << s.singleNumber(n);
+    cout << s.singleNumberN(n);
 }
