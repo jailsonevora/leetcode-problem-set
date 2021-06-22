e725808846b6cb43b8325e5e9103087c68d53c5e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-22 13:06:39 -0100

    main

--- Hash Table/5.IntersectionofTwoArrays.cpp
+++ Hash Table/5.IntersectionofTwoArrays.cpp
@@ -2,9 +2,24 @@
 #include<unordered_set>
 #include<vector>
 
+using namespace std;
+
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    vector<int> n1 = {1,2,2,1};
+    vector<int> n2 = {2,2};
+
+    Solution s;
+    s.intersection(n1, n2); 
+}
