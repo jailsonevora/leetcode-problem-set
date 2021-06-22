9672811956b9536d11e749600874c96e8d147fbd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-22 15:10:45 -0100

    Happy number

--- Hash Table/6.HappyNumber.cpp
+++ Hash Table/6.HappyNumber.cpp
@@ -1,6 +1,25 @@
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+
+using namespace std;
+
 class Solution {
 public:
     bool isHappy(int n) {
         
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    int n;
+    cin >> n;
+
+    Solution s;
+    s.isHappy(n); 
+}
