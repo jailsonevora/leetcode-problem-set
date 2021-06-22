81a4c001aaf9698a90a560abab1529d37083c75c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-22 16:24:15 -0100

    Happy number

--- Hash Table/6.HappyNumber.cpp
+++ Hash Table/6.HappyNumber.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<unordered_set>
 #include<vector>
+#include<cmath>
 
 using namespace std;
 
@@ -8,6 +9,17 @@ class Solution {
 public:
     bool isHappy(int n) {
         
+        while (n != 1)
+        {   
+            int w = 0;     
+            while (n)
+            {
+                w += (pow((n%10),2));
+                n/=10;
+            }
+            n = w;
+        }
+        return true;
     }
 };
 
