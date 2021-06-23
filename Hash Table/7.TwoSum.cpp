68738331ecb1c2b97b3f4f23c525fd3362499502
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-23 10:01:18 -0100

    Two sum

--- Hash Table/7.TwoSum.cpp
+++ Hash Table/7.TwoSum.cpp
@@ -2,9 +2,24 @@
 #include<unordered_map>
 #include<vector>
 
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    vector<int> n1 = {2,4,3,2};
+    vector<int> n2 = {2,4,3,2};
+
+    Solution s;
+    s.twoSum(n1,n2); 
+}
