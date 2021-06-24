a3ab5002639acedf7e1af8c28202d48e53849307
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-24 15:54:49 -0100

    Isomorphic Strings

--- Hash Table/8.IsomorphicStrings.cpp
+++ Hash Table/8.IsomorphicStrings.cpp
@@ -12,8 +12,9 @@ public:
         for (int i = 0; i < s.size(); i++){
             if(mp.count(s[i]) && mp[s[i]] != t[i] )
                 return false;
-            else
-                mp.insert(make_pair(s[i],t[i]));
+            else if(mp.count(t[i]))
+                return false;           
+            mp.insert(make_pair(s[i],t[i]));
         }
         return true;        
     }
@@ -26,8 +27,10 @@ int main(){
     cout.tie();
 
     //string s = "egg", t = "add";
-    //string s = "foo", t = "bar";
-    string s = "paper", t = "title";
+    string s = "foo", t = "bar";
+    //string s = "paper", t = "title";
+    //string s = "badc", t = "baba";
+
  
     Solution sl;
     sl.isIsomorphic(s,t);
