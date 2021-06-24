fdd888b3744e601369bc3e268643d1369d7ac9d4
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-24 16:23:43 -0100

    Isomorphic Strings

--- Hash Table/8.IsomorphicStrings.cpp
+++ Hash Table/8.IsomorphicStrings.cpp
@@ -6,15 +6,36 @@ using namespace std;
 
 class Solution {
 public:
+
+    bool isIsomorphic(string s, string t) {
+        
+        unordered_map<char,char> mps{0}, mpt{0};
+
+        for (int i = 0; i < s.size(); i++){
+            if( mps[s[i]] != mpt[t[i]])
+                return false;
+            mps[s[i]] = i+1;
+            mpt[t[i]] = i+1;
+        }
+        return true;        
+    }
+
     bool isIsomorphic(string s, string t) {
         unordered_map<char,char> mp;
 
         for (int i = 0; i < s.size(); i++){
-            if(mp.count(s[i]) && mp[s[i]] != t[i] )
-                return false;
-            else if(mp.count(t[i]))
-                return false;           
-            mp.insert(make_pair(s[i],t[i]));
+            if( mp.count(s[i]) ){
+                if(mp[s[i]] != t[i])
+                    return false;
+            }
+            else{ 
+                for (unordered_map<char,char>::iterator it=mp.begin(); it!=mp.end(); ++it)
+                {
+                    if(it->second == t[i])
+                        return false;
+                }                      
+                mp.insert(make_pair(s[i],t[i]));
+            }
         }
         return true;        
     }
@@ -27,8 +48,8 @@ int main(){
     cout.tie();
 
     //string s = "egg", t = "add";
-    string s = "foo", t = "bar";
-    //string s = "paper", t = "title";
+    //string s = "foo", t = "bar";
+    string s = "paper", t = "title";
     //string s = "badc", t = "baba";
 
  
