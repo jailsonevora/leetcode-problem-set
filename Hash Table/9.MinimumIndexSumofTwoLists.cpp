7ce420a592e9fe71afe87c0056b3f4a9969a5400
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-28 15:41:01 -0100

    Minimum Index Sum of Two Lists

--- Hash Table/9.MinimumIndexSumofTwoLists.cpp
+++ Hash Table/9.MinimumIndexSumofTwoLists.cpp
@@ -24,16 +24,41 @@ public:
         for (int i = 0; i < list2.size(); i++)
             if(mp.count(list2[i]))
                 v.push_back(make_pair(list2[i], mp[list2[i]] + i));
-
-        pair<string,int> min = *min_element(v.begin(), v.end(), [](const auto& lhs, const auto& rhs) {
+        
+        pair<string,int> min = *min_element(v.begin(), v.end(), 
+            [](const pair<string,int>& lhs, const pair<string,int>& rhs) {
             return lhs.second < rhs.second;    
         });
-        
         vector<string> result;
-        for (auto& rlt : v){
+        for (pair<string,int>& rlt : v)
             if(min.second == rlt.second)
                 result.push_back(rlt.first);
-        }
+        return result;
+    }
+
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+
+        unordered_map<string,int> mp;
+
+        for (int i = 0; i < list1.size(); i++)
+            mp.insert(make_pair(list1[i],i));
+
+        vector<string> result;
+        int min=INT_MAX, sum =0;
+
+        for (int i = 0; i < list2.size(); i++){
+            if(mp.count(list2[i]))
+            {
+                sum = mp[list2[i]] + i;
+                if(sum<min){
+                    result.clear();
+                    result.push_back(list2[i]);
+                    min = sum;
+                }           
+                else if(sum == min)
+                    result.push_back(list2[i]);
+            } 
+        }        
         return result;
     }
 };
@@ -45,9 +70,9 @@ int main(){
     cout.tie();
 
     //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
-    vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KFC","Shogun","Burger King"};
+    //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KFC","Shogun","Burger King"};
     //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KFC","Burger King","Tapioca Express","Shogun"};
-    //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KNN","KFC","Burger King","Tapioca Express","Shogun"};
+    vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KNN","KFC","Burger King","Tapioca Express","Shogun"};
     //vector<string> l1 = {"KFC"}, l2 = {"KFC"};
 
     Solution sl;
