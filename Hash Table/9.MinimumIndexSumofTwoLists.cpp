c00ec9924011684a24ef316cade2e7269f08abba
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-28 10:01:38 -0100

    Minimum Index Sum of Two Lists

--- Hash Table/9.MinimumIndexSumofTwoLists.cpp
+++ Hash Table/9.MinimumIndexSumofTwoLists.cpp
@@ -31,8 +31,8 @@ int main(){
     //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
     //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KFC","Shogun","Burger King"};
     //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KFC","Burger King","Tapioca Express","Shogun"};
-    //vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KNN","KFC","Burger King","Tapioca Express","Shogun"};
-    vector<string> l1 = {"KFC"}, l2 = {"KFC"};
+    vector<string> l1 = {"Shogun","Tapioca Express","Burger King","KFC"}, l2 = {"KNN","KFC","Burger King","Tapioca Express","Shogun"};
+    //vector<string> l1 = {"KFC"}, l2 = {"KFC"};
 
     Solution sl;
     sl.findRestaurant(l1,l2);
