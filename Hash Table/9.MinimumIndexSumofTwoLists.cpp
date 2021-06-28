dfeec3deb9ff6a6fce0d151bf81461b0dc579138
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-28 09:57:33 -0100

    Minimum Index Sum of Two Lists

--- Hash Table/9.MinimumIndexSumofTwoLists.cpp
+++ Hash Table/9.MinimumIndexSumofTwoLists.cpp
@@ -8,6 +8,15 @@ using namespace std;
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+
+        unordered_map<string,int> mp;
+
+        for (int i = 0; i < list1.size(); i++)
+            mp.insert(make_pair(list1[i],i));
+
+        for (int i = 0; i < list2.size(); i++)
+            if(mp.count(list2[i]))
+                mp[list2[i]] = mp[list2[i]];
         
     }
 };
