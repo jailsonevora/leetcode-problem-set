c0a26c87e27dcce733e7f2c3590dae673cca3983
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-28 16:48:12 -0100

    First Unique Character in a String

--- Hash Table/10.FirstUniqueCharacterInAString.cpp
+++ Hash Table/10.FirstUniqueCharacterInAString.cpp
@@ -3,9 +3,23 @@
 #include<vector>
 #include<algorithm>
 
+using namespace std;
+
 class Solution {
 public:
     int firstUniqChar(string s) {
         
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    string s = "leetcode";
+
+    Solution sl;
+    sl.firstUniqChar(s);
+}
