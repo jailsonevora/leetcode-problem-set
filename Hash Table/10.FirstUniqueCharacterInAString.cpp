0290b5488886d817ed5e8e68fb4063682cb297ba
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-28 23:41:14 -0100

    done

--- Hash Table/10.FirstUniqueCharacterInAString.cpp
+++ Hash Table/10.FirstUniqueCharacterInAString.cpp
@@ -8,11 +8,36 @@ using namespace std;
 class Solution {
 public:
     int firstUniqChar(string s) {
+        
         unordered_map<char,int> ch;
         for (int i = 0; i < s.size(); i++)
                 ch[s[i]]  = ch[s[i]] + 1;
         
         for (int i = 0; i < s.size(); i++)
+            if(ch[s[i]] == 1)
+                return i;
+        return -1;
+    }
+
+    int firstUniqChar(string s) {
+        
+        unordered_map<char,int> ch;
+        for (int i = 0; i < s.size(); i++)
+                ch[s[i]]++;
+        
+        for (int i = 0; i < s.size(); i++)
+            if(ch[s[i]] == 1)
+                return i;
+        return -1;
+    }
+
+    int firstUniqChar(string s) {
+        
+        int ch[256] = {0};
+        for(int i = 0; i < s.size(); i++)
+            ch[s[i]]++;
+
+        for(int i = 0; i < s.size(); i++)
             if(ch[s[i]] == 1)
                 return i;
         return -1;
