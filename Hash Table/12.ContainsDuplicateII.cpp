5ffedae52bf57f2b9328fc4db833ec462b698e68
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-29 15:35:29 -0100

    Group Anagrams

--- Hash Table/12.ContainsDuplicateII.cpp
+++ Hash Table/12.ContainsDuplicateII.cpp
@@ -8,6 +8,33 @@ using namespace std;
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
+
+        unordered_map<int,int> mp;
+
+        for (int i = 0; i < nums.size(); i++){
+            if (mp.count(nums[i]) && i - mp[nums[i]] <= k )
+                    return true;
+            mp[nums[i]] = i; 
+        }
+        return false;
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    // vector<int> nums = {1,2,3,1};
+    // int k = 3;
+
+    vector<int> nums = {1,0,1,1};
+    int k = 1;
+
+    // vector<int> nums = {1,2,3,1,2,3};
+    // int k = 2;
+
+    Solution sl;
+    sl.containsNearbyDuplicate(nums, k);
+}
