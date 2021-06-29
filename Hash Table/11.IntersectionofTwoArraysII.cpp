e813152a4628ae3c604dadeaa39e549b17676e1d
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-29 09:24:20 -0100

    Intersection of Two Arrays II

--- Hash Table/11.IntersectionofTwoArraysII.cpp
+++ Hash Table/11.IntersectionofTwoArraysII.cpp
@@ -1,3 +1,10 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
