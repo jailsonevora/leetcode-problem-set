9594895d047b76365ad31dd8d7b1f87f4f8c5947
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-29 20:41:58 -0100

    Group Anagrams

--- Hash Table/13.GroupAnagrams.cpp
+++ Hash Table/13.GroupAnagrams.cpp
@@ -8,7 +8,19 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, vector<string>> mp;
+
+        for (int i = 0; i < strs.size(); i++)
+        {
+            string sortedStr = strs[i];
+            sort(sortedStr.begin(), sortedStr.end());
+            mp[strs[i]].push_back(sortedStr);
+        }
         
+        vector<vector<string>> result;
+        for(auto itr = mp.begin(); itr != mp.end(); itr++)
+            result.push_back(itr->second);        
     }
 };
 
@@ -19,9 +31,9 @@ int main(){
     cin.tie();
     cout.tie();
     
-    //vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
     //vector<string> strs = {""};
-    vector<string> strs = {"a"};
+    //vector<string> strs = {"a"};
 
 
     Solution sl;
