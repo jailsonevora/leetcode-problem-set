762229c5ae84eb10a438890bf76c05856be02b39
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-29 11:21:11 -0100

    Contains Duplicate II

---
+++ Hash Table/12.ContainsDuplicateII.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+    }
+};
