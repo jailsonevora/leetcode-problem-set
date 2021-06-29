fb711cd0a0bca4eff25650e0e620703e9ad41efc
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-29 09:27:11 -0100

    Intersection of Two Arrays II

--- Hash Table/11.IntersectionofTwoArraysII.cpp
+++ Hash Table/11.IntersectionofTwoArraysII.cpp
@@ -11,3 +11,15 @@ public:
         
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+    
+    vector<int> nums1 = {1,2,2,1}, nums2 = {2,2};
+
+    Solution sl;
+    sl.intersect(nums1,nums2);
+}
