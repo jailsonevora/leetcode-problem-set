49740dcf42c57d08f774779cec92f17fabdab32a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-30 14:44:11 -0100

    Find Duplicate Subtrees

--- Hash Table/14.ValidSudoku.cpp
+++ Hash Table/14.ValidSudoku.cpp
@@ -11,12 +11,12 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
 
         unordered_set<string> se;
-        for (int i = 0; i < board.size(); ++i){
-            for (int j = 0; j < board.size(); ++j){
-                if(board[i][j] != '.'){
-                    if(!se.insert(board[i][j] +" in row "+i).second ||
-                      !se.insert(board[i][j] +" in col "+j).second ||
-                      !se.insert(board[i][j] +" in box "+i/3+" "+j/3).second ||
+        for (int i = 0; i < board.size(); ++i)
+            for (int j = 0; j < board.size(); ++j)
+                if(board[i][j] != '.')
+                    if( !se.insert(std::string(1,board[i][j]).append(" in row ").append(1,i)).second ||
+                        !se.insert(std::string(1,board[i][j]).append(" in col ").append(1,j)).second ||
+                        !se.insert(std::string(1,board[i][j]).append(" in box ").append(1,(i/3)).append(1,(j/3)) ).second
                       )
                         return false;
         return true;        
