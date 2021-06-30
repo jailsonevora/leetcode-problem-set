29fcf4041cb29af3e93f37923c699f157d0650c9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-06-30 11:19:52 -0100

    Valid Sudoku

--- Hash Table/14.ValidSudoku.cpp
+++ Hash Table/14.ValidSudoku.cpp
@@ -1,3 +1,10 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
