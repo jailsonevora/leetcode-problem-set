b223b9752183cddbc32aa10b236a5899a9fa5de6
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-01 09:38:13 -0100

    Jewels and Stones

--- Hash Table/16.JewelsandStones.cpp
+++ Hash Table/16.JewelsandStones.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<unordered_map>
-#include<unordered_set>
 #include<vector>
 #include<algorithm>
 
@@ -9,7 +8,15 @@ using namespace std;
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        
+
+        unordered_map<char,int> mp;
+
+        for (int i = 0; i < jewels.size(); i++)
+            mp.insert(make_pair(jewels[i],0));
+
+        for (int i = 0; i < stones.size(); i++)
+            mp[stones[i]]++;        
+        return 0;
     }
 };
 
@@ -18,4 +25,6 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie();
     cout.tie();
+
+    
 }
