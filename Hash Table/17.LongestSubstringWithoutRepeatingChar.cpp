8473775f8a7f6fa2ec3e98030fe7d370a517260b
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-01 09:54:15 -0100

    Longest Substring Without Repeating Characters

---
+++ Hash Table/17.LongestSubstringWithoutRepeatingChar.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+    }
+};
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    string s = {"abcabcbb"};
+    Solution s;
+    s.lengthOfLongestSubstring(s);
+
+}
