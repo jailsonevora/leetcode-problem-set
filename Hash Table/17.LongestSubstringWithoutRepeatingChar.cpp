fb07e928b61fc5627bff3512307e3675120a1310
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-01 15:28:53 -0100

    Longest Substring Without Repeating Characters
    Solution

--- Hash Table/17.LongestSubstringWithoutRepeatingChar.cpp
+++ Hash Table/17.LongestSubstringWithoutRepeatingChar.cpp
@@ -7,14 +7,38 @@ using namespace std;
 
 class Solution {
 public:
-    
     int lengthOfLongestSubstring(string s) {
 
         unordered_set<string> st;
         string lgstStr = {};
 
+        for (int i = 0; i < s.size(); i++){
+            string temp = {s[i]};
+            for (int j = i+1; j < s.size(); j++){
+                if(temp.find(s[j]) == string::npos)
+                    temp += std::string(1,s[j]);
+                else{
+                    if(temp.size() > lgstStr.size())
+                        lgstStr = temp;
+                    i=j;
+                    temp = s[j];
+                    continue;
+                }                    
+            }
+        }
+        return lgstStr.size();
+    }
+
+    int lengthOfLongestSubstring(string s) {
+
+        unordered_set<string> st;
+        string lgstStr = {};
+
+        if(s.size() == 1)
+            lgstStr = s.size();
+
         string temp = {s[0]};
-        for (int i = 0, j = i+1; j < s.size(); ++i, j++){
+        for (int i = 0, j = i+1; i < s.size(); ++i, j++){
             if(temp.find(s[j]) == string::npos)
                 temp += std::string(1,s[j]);
             else{
