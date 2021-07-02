71540d36c1972444cdacc07b6966bca2f6feaef9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-02 11:07:12 -0100

    done

--- Hash Table/18.4SumII.cpp
+++ Hash Table/18.4SumII.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 class Solution {
 public:
+    //with map
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         
         unordered_map<int,int> mp;
@@ -17,6 +18,19 @@ public:
         for(auto c:nums3)
             for(auto d:nums4)
                 ans+=mp[0-c-d];
+        return ans;
+    }
+    // with array
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {        
+        
+        int mp[1024] = {0}, ans=0;
+        
+        for(int i = 0; i < nums1.size(); i++)
+            for(int j = 0; j < nums2.size(); j++)
+                mp[nums1[i]+nums2[j]]++;
+        for(int i = 0; i < nums3.size(); i++)
+            for(int j = 0; j < nums4.size(); j++)
+                ans+=mp[0-nums3[i]-nums4[j]];
         return ans;
     }
 };
