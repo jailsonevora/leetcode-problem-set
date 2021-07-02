776a0638ded0118bed3d4b0051c627ad4e6b9af5
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-02 15:12:57 -0100

    Top K Frequent Elements

--- Hash Table/19.TopKFrequentElements.cpp
+++ Hash Table/19.TopKFrequentElements.cpp
@@ -1,16 +1,40 @@
 #include<iostream>
 #include<unordered_map>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
 class Solution {
+    struct node
+    {
+        int key;
+        int val;
+        node(int k, int v) : key(k), val(v) {}
+    };
+
+    static int cmp(node const& a, node const& b)
+    {
+        return a.val < b.val;
+    }    
+    
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
         
+        unordered_map<int, int> mp;
+
         for(auto it: nums)
             mp[it]++;
+
+        priority
+        
+        vector<int> ans;
+        for (int i = 0; i < k; ++i)
+        {   
+            v.push_back(mp)
+        }
+        
+        
 
         return vector<int>{};
     }
