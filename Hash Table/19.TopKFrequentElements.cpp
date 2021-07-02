3c1c401ea82ff70dd7cea7ecda0b37713729f13a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-02 11:15:31 -0100

    Top K Frequent Elements

--- Hash Table/19.TopKFrequentElements.cpp
+++ Hash Table/19.TopKFrequentElements.cpp
@@ -2,9 +2,24 @@
 #include<unordered_map>
 #include<vector>
 
+using namespace std;
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    vector<int> n = {1,1,1,2,2,3};
+    int k = 2;
+ 
+    Solution s;
+    s.topKFrequent(n,k);
+}
