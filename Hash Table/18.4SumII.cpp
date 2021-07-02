3c300fff77e5321c4398a60e5746123a859223ce
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-02 10:40:07 -0100

    4Sum II

--- Hash Table/18.4SumII.cpp
+++ Hash Table/18.4SumII.cpp
@@ -8,5 +8,27 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         
+        unordered_map<int,int> mp;
+        int ans=0;
+        
+        for(auto a: nums1)
+            for(auto b:nums2)
+                mp[a+b]++;
+        for(auto c:nums3)
+            for(auto d:nums4)
+                ans+=mp[0-c-d];
+        return ans;
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    vector<int> n1 = {1,2}, n2 = {-2,-1}, n3 = {-1,2}, n4 = {0,2};
+ 
+    Solution s;
+    s.fourSumCount(n1,n2,n3,n4);
+}
