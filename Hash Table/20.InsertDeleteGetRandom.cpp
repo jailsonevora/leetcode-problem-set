466cf3279374ffe76a200b5bc66bbafa18769f06
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-02 23:57:25 -0100

    Hash table done

--- Hash Table/20.InsertDeleteGetRandom.cpp
+++ Hash Table/20.InsertDeleteGetRandom.cpp
@@ -2,35 +2,90 @@
 #include<vector>
 #include<algorithm>
 #include<cmath>
+#include<unordered_set>
+#include<unordered_map>
 
 using namespace std;
 
-
 class RandomizedSet {
 public:
-    vector<bool> hashSet;
+    unordered_set<int> hashSet;
     /** Initialize your data structure here. */
     RandomizedSet() {
-        hashSet = vector<bool>(1000001, false);
+        hashSet.clear();
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        hashSet[rand()] = val;
+        return hashSet.insert(val).second;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        auto it = find(hashSet.begin(), hashSet.end(), val);
-        if (it != hashSet.end())
-            hashSet.erase(it);  
+        return hashSet.erase(val);          
     }
     
     /** Get a random element from the set. */
     int getRandom() {
-        return hashSet[rand()];
+        auto iter = hashSet.begin();
+        advance(iter, rand() % hashSet.size());
+        return *iter;
     }
 };
+
+class RandomizedSet {    
+    unordered_map<int,int> mp;
+    vector<int> vec;
+
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        vec.clear();
+        mp.clear();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(!mp.count(val)){            
+            mp[val] = vec.size();
+            vec.push_back(val);
+            return true;
+        }
+        return false;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if (mp.count(val)){
+            vec[mp[val]]=vec.back();
+            mp[vec.back()]=mp[val];
+            vec.pop_back();
+            mp.erase(val);
+            return true;
+        }
+        return false;            
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return vec[rand()%vec.size()];
+    }
+};
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    int val = 10;
+    cin >> val; 
+
+    RandomizedSet* obj = new RandomizedSet();
+    obj->insert(val);
+    obj->remove(val);
+    cout << obj->getRandom();
+}
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
