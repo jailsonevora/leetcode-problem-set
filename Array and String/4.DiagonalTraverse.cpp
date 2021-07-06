18fce841d6d6b18ad625fe61fb820ae53ddf9c37
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-06 13:09:43 -0100

    Diagonal Traverse

--- Array and String/4.DiagonalTraverse.cpp
+++ Array and String/4.DiagonalTraverse.cpp
@@ -12,3 +12,18 @@ public:
         
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+    vector<vector<int>> mat = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    sl.findDiagonalOrder(mat);
+}
