03ddd6ad4eae9fbd63872143724825cbe7c32e45
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-07 21:16:23 -0100

    Implement strStr()

---
+++ Array and String/8.ImplementstrStr.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+
+    sl.strStr("11","1");
+}
