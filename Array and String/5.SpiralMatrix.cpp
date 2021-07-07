29b2f691231640702028657794aae9a9260dd715
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-07 11:01:03 -0100

    Spiral Matrix

--- Array and String/5.SpiralMatrix.cpp
+++ Array and String/5.SpiralMatrix.cpp
@@ -10,26 +10,34 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> v;
-        bool goingUp = true;
-        int i = 0;
+        int i, direction = 0; //0 left to right, 1 top to down, 2 right to left, 3 down to top
         
-        for (int r = 0, c = 0; r < matrix.size() && c < matrix[0].size();){
-            if(c == matrix[0].size()-1 && (r != matrix.size()-1)){
-                ++r;                
-                v.push_back(matrix[r][c]);
+        for (int top = 0, down = matrix.size()-1, left = 0, right = matrix[0].size()-1; top <= down && left <= right;){
+            //0 left to right
+            if(direction == 0){
+                for (i = left; i <= right; i++)
+                    v.push_back(matrix[top][i]);
+                top++;
             }
-            else if(c > 0 && r == matrix[0].size()-1){
-                --c;
-                v.push_back(matrix[r][c]);
+            //1 top to down
+            else if(direction==1){
+                for (i = top; i <= down; i++)
+                    v.push_back(matrix[i][right]);
+                right--;
             }
-            else if(c == 0 && r == matrix[0].size()-1){
-                --r;
-                v.push_back(matrix[r][c]);
+            //2 right to left
+            else if(direction==2){
+                for (i = right; i >= left; i--)
+                    v.push_back(matrix[down][i]);
+                down--;
             }
-            else{
-                v.push_back(matrix[r][c]);
-                c++;
+            //3 down to top
+            else if(direction==3){
+                for (i = down; i >= top; i--)
+                    v.push_back(matrix[i][left]);
+                left++;
             }
+            direction = (direction+1)%4;
         }
         return v;
     }
