3cc6c3899c28439d19c70b753f24c76641c756ef
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-07 15:34:00 -0100

    Add binary

--- Array and String/7.AddBinary.cpp
+++ Array and String/7.AddBinary.cpp
@@ -2,14 +2,28 @@
 #include<vector>
 #include<algorithm>
 #include<cmath>
-#include <numeric>
+#include<numeric>
+#include<string>
+#include<bitset>
 
 using namespace std;
 
 class Solution {
 public:
     string addBinary(string a, string b) {
-        
+        string ans = "";
+        int sum = 0;
+
+        for (int i = a.size()-1, j = b.size()-1; i >= 0 || j >= 0 || sum==1; --i, --j)
+        {
+            sum +=( (i >= 0) ? a[i] - '0': 0);
+            sum +=( (j >= 0) ? b[j] - '0': 0);
+
+            ans = char(sum % 2 + '0') + ans;
+
+            sum/=2; 
+        }
+        return ans;
     }
 };
 
