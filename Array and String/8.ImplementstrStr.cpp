85f4e1da9d830eb13c3db6c8073ed0088aeadcc0
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-08 10:58:00 -0100

    Implement strStr()

--- Array and String/8.ImplementstrStr.cpp
+++ Array and String/8.ImplementstrStr.cpp
@@ -28,7 +28,7 @@ public:
         if(!needle.size())
             return 0;
 
-        //if(haystack.size() < needle.size()) return -1;
+        if(haystack.size() < needle.size()) return -1;
 
         for (int i = 0; i <= haystack.size()-needle.size(); i++)
             if(!mp.count(haystack.substr(i,needle.size())))
