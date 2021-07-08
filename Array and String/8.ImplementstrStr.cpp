226b6a775f0d567052da9a48e7914b66c0e47935
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-08 10:57:06 -0100

    Implement strStr()

--- Array and String/8.ImplementstrStr.cpp
+++ Array and String/8.ImplementstrStr.cpp
@@ -10,14 +10,33 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        
+
+        if(haystack.size() < needle.size()) return -1;
+
+        for (int i = 0; i <= haystack.size() - needle.size(); i++)
+            if (string(haystack.begin()+i,haystack.begin()+i+needle.size()) == needle)
+                return i;
+        return -1;
+    }
+    
+    int strStr(string haystack, string needle) {
+
+        unordered_map<string, int> mp;
+
         if(!needle.size())
             return 0;
 
-        for (int i = 0; i < haystack.size() - needle.size(); i++)
-            if (haystack.substr(i,i+needle.size()) == needle)
-                return i;
-        return -1;
+        //if(haystack.size() < needle.size()) return -1;
+
+        for (int i = 0; i <= haystack.size()-needle.size(); i++)
+            if(!mp.count(haystack.substr(i,needle.size())))
+                mp[haystack.substr(i,needle.size())] = i;
+
+        if(mp.find(needle) != mp.end())
+            return mp[needle];
+        else
+            return -1;
+
     }
 };
 
@@ -29,7 +48,7 @@ int main(){
     Solution sl;
 
     //cout << sl.strStr("","");
-    //cout << sl.strStr("hello","ll");
+    cout << sl.strStr("hello","ll");
     //cout << sl.strStr("a","a");
-    cout << sl.strStr("aaa","a");
+    //cout << sl.strStr("aaa","a");
 }
