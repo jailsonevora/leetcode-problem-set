e0939872b18880d00ad9d97c4da54edd55e3a540
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-08 15:55:00 -0100

    heap sort

--- Array and String/9.LongestCommonPrefix.cpp
+++ Array and String/9.LongestCommonPrefix.cpp
@@ -4,20 +4,43 @@
 #include<cmath>
 #include<numeric>
 #include<unordered_map>
+#include<queue>
 
 using namespace std;
 
 class Solution {
 public:
+    struct node
+    {
+        string key;
+        int val;
+        node(string k, int v) : key(k), val(v) {}
+    };
+
+    struct compare 
+    { 
+        bool operator()(node const& a, node const& b)
+        {
+            a.val < b.val && (a.key.compare(b.key) < 0);
+        } 
+    }; 
     string longestCommonPrefix(vector<string>& strs) {
 
-        unordered_map<string,int> mp;
+        unordered_map<string,int> mp;       
         
-        for (int i = 0; i < strs.size(); i++){
-            for (int j = 1; j <= strs[i].size(); ++j)
+        for (int i = 0; i < strs.size() && !strs[i].empty(); i++){
+            for (int j = 1; j <= strs[i].size() && !strs[i].empty(); ++j)
                 mp[strs[i].substr(0,j)]++;
         }
-        mp.size();
+        priority_queue<node,vector<node>,compare> heap;
+        for(auto it: mp)
+            heap.push(node(it.first,it.second));
+        
+        if(heap.empty())
+            return "";
+        
+        string ans = (heap.top().val == strs.size()) ? heap.top().key : "";
+        return ans;
     }
 };
 
@@ -28,6 +51,10 @@ int main(){
 
     Solution sl;
 
-    vector<string> v = {"flower","flow","flight"};
+    //vector<string> v = {"flower","flow","flight"};
+    //vector<string> v = {"dog","racecar","car"};
+    //vector<string> v = {""};
+    vector<string> v = {"flower","flawer","flvwer","flower"};
+
     cout << sl.longestCommonPrefix(v);
 }
