0f5b8471a2ef3915597a1df0797ebb696e6a0d31
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-08 12:54:58 -0100

    Longest Common Prefix

--- Array and String/9.LongestCommonPrefix.cpp
+++ Array and String/9.LongestCommonPrefix.cpp
@@ -10,6 +10,24 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+
+        unordered_map<string,int> mp;
         
+        for (int i = 0; i < strs.size(); i++){
+            for (int j = 1; j <= strs[i].size(); ++j)
+                mp[strs[i].substr(0,j)]++;
+        }
+        mp.size();
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+
+    vector<string> v = {"flower","flow","flight"};
+    cout << sl.longestCommonPrefix(v);
+}
