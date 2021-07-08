3f36d00d528ba4eb645af4bfebff677595ac6e27
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-08 11:10:02 -0100

    Implement strStr()

--- Array and String/8.ImplementstrStr.cpp
+++ Array and String/8.ImplementstrStr.cpp
@@ -9,17 +9,6 @@ using namespace std;
 
 class Solution {
 public:
-    //with string
-    // int strStr(string haystack, string needle) {
-
-    //     if(haystack.size() < needle.size()) return -1;
-
-    //     for (int i = 0; i <= haystack.size() - needle.size(); i++)
-    //         if (string(haystack.begin()+i,haystack.begin()+i+needle.size()) == needle)
-    //             return i;
-    //     return -1;
-    // }
-
     //with substring
     int strStr(string haystack, string needle) {
 
@@ -32,26 +21,37 @@ public:
         return -1;
     }
 
+    //with string
+    int strStr(string haystack, string needle) {
+
+        if(haystack.size() < needle.size()) return -1;
+
+        for (int i = 0; i <= haystack.size() - needle.size(); i++)
+            if (string(haystack.begin()+i,haystack.begin()+i+needle.size()) == needle)
+                return i;
+        return -1;
+    }    
+
     //with map
-    // int strStr(string haystack, string needle) {
+    int strStr(string haystack, string needle) {
 
-    //     unordered_map<string, int> mp;
+        unordered_map<string, int> mp;
 
-    //     if(!needle.size())
-    //         return 0;
+        if(!needle.size())
+            return 0;
 
-    //     if(haystack.size() < needle.size()) return -1;
+        if(haystack.size() < needle.size()) return -1;
 
-    //     for (int i = 0; i <= haystack.size()-needle.size(); i++)
-    //         if(!mp.count(haystack.substr(i,needle.size())))
-    //             mp[haystack.substr(i,needle.size())] = i;
+        for (int i = 0; i <= haystack.size()-needle.size(); i++)
+            if(!mp.count(haystack.substr(i,needle.size())))
+                mp[haystack.substr(i,needle.size())] = i;
 
-    //     if(mp.find(needle) != mp.end())
-    //         return mp[needle];
-    //     else
-    //         return -1;
+        if(mp.find(needle) != mp.end())
+            return mp[needle];
+        else
+            return -1;
 
-    // }
+    }
 };
 
 int main(){
