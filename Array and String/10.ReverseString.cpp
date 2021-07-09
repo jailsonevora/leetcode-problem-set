ddfa23ff0d096a34cfde86af7068e1b9955b4160
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-09 14:49:57 -0100

    Two Pointers

--- Array and String/10.ReverseString.cpp
+++ Array and String/10.ReverseString.cpp
@@ -10,10 +10,11 @@ using namespace std;
 
 class Solution {
 public:
-    // void reverseString(vector<char>& s) {
-    //     for(int i = 0, j = s.size()-1; i < j; ++i,--j)
-    //         std::swap(s[i],s[j]);
-    // }
+    // Two Pointers
+    void reverseString(vector<char>& s) {
+        for(int i = 0, j = s.size()-1; i < j; ++i,--j)
+            std::swap(s[i],s[j]);
+    }
 
     // with built-in
     void reverseString(vector<char>& s) {
