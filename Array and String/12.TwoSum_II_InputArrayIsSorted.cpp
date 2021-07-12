3542f401526c68610d7dabb1c403dfbdb1cb72d7
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-12 12:06:05 -0100

    Two Sum II - Input array is sorted

--- Array and String/12.TwoSum_II_InputArrayIsSorted.cpp
+++ Array and String/12.TwoSum_II_InputArrayIsSorted.cpp
@@ -15,3 +15,20 @@ public:
     }
 };
 
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+
+    int target = 9;
+    vector<int> v = {2,7,11,15};
+    //int target = 6; 
+    //vector<int> v = {2,3,4};
+    //int target = -1; 
+    //vector<int> v = {-1,0};
+
+    for(auto v :sl.twoSum(v,target))
+        cout << v << "\n";
+}
