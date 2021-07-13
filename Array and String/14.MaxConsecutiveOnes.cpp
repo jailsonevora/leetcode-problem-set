cce0c36403c3c6571d1e8e2ef9e0c7184e6cc826
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-13 11:58:56 -0100

    Max Consecutive Ones

--- Array and String/14.MaxConsecutiveOnes.cpp
+++ Array and String/14.MaxConsecutiveOnes.cpp
@@ -3,16 +3,23 @@
 #include<algorithm>
 #include<cmath>
 #include<numeric>
-#include<queue>
+#include<cmath>
 
 using namespace std;
 
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-
-        for
-        
+        int ptr = 0, ans = 0;
+        for(int i = 0; i < nums.size();++i){
+            if(nums[i] == 1){
+                ptr++;
+                ans = max(ans,ptr);
+            }
+            else
+                ptr=0;
+        }
+        return ans;
     }
 };
 
@@ -22,7 +29,7 @@ int main(){
     cout.tie();
 
     Solution sl;
-    
+
     vector<int> v = {1,1,0,1,1,1};
 
    cout << sl.findMaxConsecutiveOnes(v) << "\n";
