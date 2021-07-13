0c53a84cdc6ed1dc5202512f564a411adeae3161
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-13 10:31:16 -0100

    Remove Element

--- Array and String/13.RemoveElement.cpp
+++ Array and String/13.RemoveElement.cpp
@@ -14,3 +14,18 @@ public:
         
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+
+    // int target = 9;
+    // vector<int> v = {2,7,11,15};
+    int val = 2; 
+    vector<int> v = {0,1,2,2,3,0,4,2};
+
+   cout << sl.removeElement(v,val) << "\n";
+}
