c72ab952950aabe1a666658093645b5a40fe0164
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-14 15:36:23 -0100

    Minimum Size Subarray Sum

--- Array and String/15.MinimumSizeSubarraySum.cpp
+++ Array and String/15.MinimumSizeSubarraySum.cpp
@@ -10,7 +10,21 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        
+        int ans = INT_MAX;
+
+        int left = 0, sum = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            while (sum >= target)
+            {
+                ans = min(ans,i+1-left);
+                sum-=nums[left];
+                left++;
+            }            
+        }
+        return (ans != INT_MAX) ? ans : 0;
     }
 };
 
@@ -22,7 +36,7 @@ int main(){
     Solution sl;
 
     int target = 7;
-    vector<int> nums = {1,1,0,1,1,1};
+    vector<int> nums = {2,1,0,1,1,1};
 
    cout << sl.minSubArrayLen(target, nums) << "\n";
 }
