6a2d659f4db02864c7752baaaf4737f1d6812435
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-14 14:53:28 -0100

    Minimum Size Subarray Sum

--- Array and String/15.MinimumSizeSubarraySum.cpp
+++ Array and String/15.MinimumSizeSubarraySum.cpp
@@ -13,3 +13,16 @@ public:
         
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+
+    int target = 7;
+    vector<int> nums = {1,1,0,1,1,1};
+
+   cout << sl.minSubArrayLen(target, nums) << "\n";
+}
