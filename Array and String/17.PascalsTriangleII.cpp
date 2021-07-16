17578a3747d4d24c250f568ed94e8bfc3edf8bac
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-16 10:50:47 -0100

    Pascal's Triangle II

--- Array and String/17.PascalsTriangleII.cpp
+++ Array and String/17.PascalsTriangleII.cpp
@@ -9,23 +9,30 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-        vector<int> ans;
-        vector<vector<int>> result(34);
-        for (int r = 0; r < 34; r++){
-            result[r].resize(r+1);
-            result[r][0] = result[r][r] = 1;
-            if(r == rowIndex)
-                    ans.push_back(result[r][0]);
-            for (int c = 1; c < r; ++c){
-                result[r][c] = result[r-1][c-1] + result[r-1][c];
-                if(r == rowIndex)
-                    ans.push_back(result[r][c]);
-            }
-            if(r == rowIndex && r !=0)
-                    ans.push_back(result[r][r]);
-        }
-        return ans;  
+    // vector<int> getRow(int rowIndex) {
+    //     vector<int> ans;
+    //     vector<vector<int>> result(34);
+    //     for (int r = 0; r < 34; r++){
+    //         result[r].resize(r+1);
+    //         result[r][0] = result[r][r] = 1;
+    //         if(r == rowIndex)
+    //                 ans.push_back(result[r][0]);
+    //         for (int c = 1; c < r; ++c){
+    //             result[r][c] = result[r-1][c-1] + result[r-1][c];
+    //             if(r == rowIndex)
+    //                 ans.push_back(result[r][c]);
+    //         }
+    //         if(r == rowIndex && r !=0)
+    //                 ans.push_back(result[r][r]);
+    //     }
+    //     return ans;  
+    // }
+
+    vector<int> getRow(int r) {
+        vector<int> res(r+1, 1); 
+        for(int i=1; i<r; i++)
+            res[i] = (res[i-1]*1LL*(r-i+1))/i;
+        return res;
     }
 };
 
@@ -38,8 +45,8 @@ int main(){
     Solution sl;
 
    
-    //int rowIndex = 3;
-    int rowIndex = 0;
+    int rowIndex = 3;
+    //int rowIndex = 0;
     //int rowIndex = 1;
 
    sl.getRow(rowIndex);
