44b9719b62135d317a9d9a27a52a41663f15b738
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-16 10:10:00 -0100

    Pascal's Triangle II

--- Array and String/17.PascalsTriangleII.cpp
+++ Array and String/17.PascalsTriangleII.cpp
@@ -13,3 +13,19 @@ public:
         
     }
 };
+
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+
+    // int k = 2;
+    // vector<int> nums = {-1,-100,3,99};
+    int rowIndex = 3;
+    vector<int> nums = {1,3,3,1};
+
+   sl.getRow(rowIndex);
+}
