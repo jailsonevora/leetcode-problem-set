f197cfb92d3bf9564655f7f761e4b0bb9a44dabd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-19 14:34:43 -0100

    Reverse Words in a String III

--- Array and String/19.ReverseWordsInAStringIII.cpp
+++ Array and String/19.ReverseWordsInAStringIII.cpp
@@ -10,7 +10,15 @@ using namespace std;
 class Solution {
 public:
     string reverseWords(string s) {
-        
+        int prevToken = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                reverse(s.begin() + prevToken, s.begin() + i);
+                prevToken = i+1;
+            }
+        }
+        reverse(s.begin() + prevToken, s.end());
+        return s;
     }
 };
 
@@ -20,8 +28,10 @@ int main(){
     cout.tie();
 
     Solution sl;   
-    string s = "Let's take LeetCode contest";
-    // string s = "God Ding";
+    //string s = "Let's take LeetCode contest";
+    string s = "God Ding";
 
    sl.reverseWords(s);
 }
+
+95.24%
