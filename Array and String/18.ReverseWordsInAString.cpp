f5fd77b582683b89ac47c9f2e4ea04916ea984fd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-07-19 13:24:30 -0100

    Reverse Words in a String

--- Array and String/18.ReverseWordsInAString.cpp
+++ Array and String/18.ReverseWordsInAString.cpp
@@ -31,6 +31,36 @@ public:
         }
         return ans;
     }
+
+    string reverseWords(string s) {
+        if (s.empty())  return s;
+        
+        while(s.back() == ' ')                      //remove the space at end, after reversing the string, there is no space at beginning
+            s.pop_back();
+        reverse(s.begin(), s.end());                
+        s.push_back(' ');                           //add a space at the end, space is used to identify a complete word.
+        
+        string::iterator start = s.begin();
+        for (string::iterator itr = s.begin(); itr != s.end(); itr++){                  //reverse each single word
+            if (*itr == ' '){
+                reverse(start, itr);
+                start = next(itr);                  //do not include space
+            }
+        }
+    
+        while(s.back() == ' ')                      //remove all extra spaces
+            s.pop_back();  
+        int i=0;
+        while(i<s.size()){
+            if (s[i] == ' ' && s[i+1] == ' '){
+                s.erase(i+1, 1);
+                continue;
+            }
+            i++;
+        }
+        
+        return s;
+}
 };
 
 int main(){
