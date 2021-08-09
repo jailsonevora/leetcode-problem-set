62d4cc92edaeeaefc9b1f1a678c35cf3ac34db9c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-09 14:40:58 -0100

    Number of Islands

--- Queue & Stack/2.NumberofIslands.cpp
+++ Queue & Stack/2.NumberofIslands.cpp
@@ -5,7 +5,32 @@ using namespace std;
 
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
+
+    void islandBoundary(vector<vector<char>> &grid, int x, int y, int r, int c){
+        if (x < 0 || x >= r || y < 0 || y >= c || grid[x][y] != '1')
+            return;
         
+        grid[x][y] = '2';
+        
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+
+        int rows = grid.size();
+        if(rows == 0)
+            return 0;
+
+        int cols = grid[0].size();
+
+        int ans = 0;
+
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                if (grid[r][c] == '1')
+                {   
+                    islandBoundary(grid, r,c, rows, cols);
+                    ans++;
+                }
+        return ans;        
     }
 };
