ce759518861e2e132738216480d006bd7647eca7
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-11 13:18:04 -0100

    Daily Temperatures

--- Queue & Stack/7.DailyTemperatures.cpp
+++ Queue & Stack/7.DailyTemperatures.cpp
@@ -2,9 +2,28 @@
 #include<vector>
 #include<stack>
 
+unsing namespace std;
+
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+    //string s = "()";
+    //string s = "()[]{}";
+    //string s = "(]";
+    //string s = "([)]";
+    //string s = "{[]}";
+    vector<int> s; 
+    //{73,74,75,71,69,72,76,73};
+    
+    sl.dailyTemperatures(s);
+}
