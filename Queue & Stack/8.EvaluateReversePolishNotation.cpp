103a31618eb60341cfd6010c157b0e157f1c2655
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-11 15:27:43 -0100

    Evaluate Reverse Polish Notation

--- Queue & Stack/8.EvaluateReversePolishNotation.cpp
+++ Queue & Stack/8.EvaluateReversePolishNotation.cpp
@@ -11,3 +11,16 @@ public:
         
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+    vector<string> tokens  {"2","1","+","3","*"};
+    //vector<int> s {30,40,50,60};
+    // vector<int> s {30,60,90};
+
+    cout << sl.evalRPN(tokens);
+}
