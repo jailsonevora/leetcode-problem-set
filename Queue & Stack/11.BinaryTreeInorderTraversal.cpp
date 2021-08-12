6465315ccf44bc672776e4c461f475b1cc9d4761
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-12 13:54:35 -0100

    Binary Tree Inorder Traversal

--- Queue & Stack/11.BinaryTreeInorderTraversal.cpp
+++ Queue & Stack/11.BinaryTreeInorderTraversal.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<set>
 #include<stack>
+#include<vector>
 
 using namespace std;
 
@@ -18,8 +19,73 @@ struct TreeNode {
 };
 
 class Solution {
+    void DFS_Recursive(TreeNode* root, vector<int> &ans){
+
+        if(root->left)
+            DFS_Recursive(root, ans);
+
+        ans.push_back(root->val);
+
+        if(root->right)
+            DFS_Recursive(root, ans);
+
+    }
+
+    // void DFS_Iteratively(TreeNode* root){
+
+    //     stack<TreeNode*> stck;
+    //     set<TreeNode*> visited;
+    //     vector<int> ans;
+
+    //     stck.push(root);
+
+    //     while (!stck.empty())
+    //     {
+    //         TreeNode* cur = stck.top();
+    //         stck.pop();
+            
+    //         for (TreeNode* next: cur->left)
+    //         {
+    //             if(!visited.count(next)){
+    //                 visited.insert(next);
+    //                 stck.push(next);
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         
+        stack<TreeNode*> stck;
+        set<TreeNode*> visited;
+        vector<int> ans;
+
+        if(!root)
+            return ans;
+
+        TreeNode* cur = root;
+        while (!stck.empty() || cur)
+        {
+            while (cur)
+            {
+                if(!visited.count(cur)){
+                    visited.insert(cur);
+                    stck.push(cur);
+                    cur = cur->left;
+                }
+            }
+            cur = stck.top();
+            stck.pop();
+            ans.push_back(cur->val);
+            cur = cur->right;
+        }
+        return ans;
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        DFS_Recursive(root, ans);        
+        return ans;
     }
 };
