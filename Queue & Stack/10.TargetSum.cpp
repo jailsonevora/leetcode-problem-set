e4dd523b2422679d0e948c694fbeb326d9ff004c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-12 12:41:35 -0100

    Target Sum

--- Queue & Stack/10.TargetSum.cpp
+++ Queue & Stack/10.TargetSum.cpp
@@ -5,20 +5,31 @@
 using namespace std;
 
 class Solution {
+    void backtrack(vector<int>& nums, int target, int sum, int pos, int& count){
+        if(pos == nums.size()){
+            if(sum == target) count++;
+            return;
+        }
+        backtrack(nums, target, sum + nums[pos], pos + 1, count);
+        backtrack(nums, target, sum - nums[pos], pos + 1, count);
+    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int head = 0, tail = nums.size()-1;
-        for (; head < tail; )
-        {   
-            int sum = nums[head]+nums[tail];      
-            if (sum > target)
-                tail--;
-            else if (sum < target)
-                head++;
-            else
-                return vector<int>{head+1, tail+1}.size();
-            
-        }
-        return vector<int>{head, tail}.size();
+        int count = 0;
+        backtrack(nums, target, 0, 0, count);
+        return count;
     }
 };
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    vector<int> nums = {1,1,1,1,1};
+    int target = 3;
+ 
+    Solution sl;
+    cout << sl.findTargetSumWays(nums,target);
+}
