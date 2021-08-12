7fa22c1aa13a9d244214c65307e1ad3e0f7f31e3
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-12 12:42:49 -0100

    Target Sum

--- Queue & Stack/10.TargetSum.cpp
+++ Queue & Stack/10.TargetSum.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Backtracking / Recursion
 class Solution {
     void backtrack(vector<int>& nums, int target, int sum, int pos, int& count){
         if(pos == nums.size()){
@@ -18,6 +19,34 @@ public:
         int count = 0;
         backtrack(nums, target, 0, 0, count);
         return count;
+    }
+};
+
+//Dynamic Programing
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = 0;
+
+        for(auto x: nums) sum += x;
+
+        if(sum < S || -sum > S) return 0;
+
+        vector<int>cur(2 * sum + 1);
+        vector<int>next(2 * sum + 1);
+        
+        cur[sum] = 1;
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < 2 * sum + 1; j++){
+                if(cur[j] != 0){
+                    next[j + nums[i]] += cur[j];
+                    next[j - nums[i]] += cur[j];
+                    cur[j] = 0;
+                }
+            }
+            swap(cur, next);
+        }
+        return cur[sum + S];
     }
 };
 
