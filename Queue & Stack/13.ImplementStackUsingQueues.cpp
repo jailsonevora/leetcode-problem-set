ae0b1a7b725ea200cacd8deb3befa02f903d43b9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-13 14:43:06 -0100

    Implement Stack using Queues

--- Queue & Stack/13.ImplementStackUsingQueues.cpp
+++ Queue & Stack/13.ImplementStackUsingQueues.cpp
@@ -1,35 +1,46 @@
 #include<iostream>
 #include<set>
-#include<stack>
+#include<queue>
 #include<vector>
 
 using namespace std;
 
 class MyStack {
+    queue<int> q1, q2;
 public:
     /** Initialize your data structure here. */
     MyStack() {
-        
     }
     
     /** Push element x onto stack. */
     void push(int x) {
-        
+
+        q2.push(x);
+        // Push all the remaining
+        // elements in q1 to q2.
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        swap(q1,q2);
     }
     
     /** Removes the element on top of the stack and returns that element. */
     int pop() {
-        
+        int top = q1.front();
+        q1.pop();
+        return top;
     }
     
     /** Get the top element. */
     int top() {
-        
+        return q1.front();
     }
     
     /** Returns whether the stack is empty. */
     bool empty() {
-        
+        return q1.empty();
     }
 };
 
