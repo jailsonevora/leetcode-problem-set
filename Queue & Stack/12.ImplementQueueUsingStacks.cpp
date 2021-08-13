7cff6f914f171d3b1377b464d56f20c3d28afa44
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-13 12:58:06 -0100

    Implement Queue using Stacks

--- Queue & Stack/12.ImplementQueueUsingStacks.cpp
+++ Queue & Stack/12.ImplementQueueUsingStacks.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 class MyQueue {
+    stack<int> s1, s2;
 public:
     /** Initialize your data structure here. */
     MyQueue() {
@@ -15,21 +16,41 @@ public:
     /** Push element x to the back of queue. */
     void push(int x) {
         
+        // Move all elements from s1 to s2
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        s1.push(x);
+
+        // Push everything back to s1
+        while (!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        
+        int tp;
+        if(!empty()){
+            tp = s1.top();
+            s1.pop();
+        }
+        return tp;
     }
     
     /** Get the front element. */
     int peek() {
-        
+        return s1.top();
     }
     
     /** Returns whether the queue is empty. */
     bool empty() {
-        
+        bool r;
+        (s1.empty()) ? r = true : r = false;
+        return r;
     }
 };
 
