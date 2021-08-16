78394d133d7cbbd237cb60b75f0bde05bb446883
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-16 11:15:26 -0100

    Flood Fill

--- Queue & Stack/15.FloodFill.cpp
+++ Queue & Stack/15.FloodFill.cpp
@@ -7,16 +7,19 @@ using namespace std;
 
 class Solution {
     void DFS(vector<vector<int>> &image, int sr, int sc, int newColor, int r, int c, int source){
+        
         if (sr < 0 || sr >= r || sc < 0 || sc >= c)
             return;
+        else if(image[sr][sc] != source)
+            return;
         
-        image[sr][sc] = '2';
+        image[sr][sc] = newColor;
 
         //recursive call to 4 adjacent directions
-        DFS(image, sr - 1, sc, newColor, r, c, source);
-        DFS(image, sr + 1, sc, newColor, r, c, source);
-        DFS(image, sr, sc + 1, newColor,r, c, source);
-        DFS(image, sr, sc - 1, newColor,r, c, source);
+        DFS(image, sr - 1, sc, newColor, r, c, source); //top
+        DFS(image, sr + 1, sc, newColor, r, c, source); //down
+        DFS(image, sr, sc + 1, newColor,r, c, source); //left
+        DFS(image, sr, sc - 1, newColor,r, c, source); //right
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
