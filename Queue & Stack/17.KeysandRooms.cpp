f88f4d4da3c5e8af10f0fe0827eb02d21211fe5a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-16 12:20:41 -0100

    Keys and Rooms

--- Queue & Stack/17.KeysandRooms.cpp
+++ Queue & Stack/17.KeysandRooms.cpp
@@ -8,7 +8,30 @@ using namespace std;
 
 class Solution {
 public:
+    //DFS with stack
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        
+
+        bool seen[rooms.size()];
+        seen[0] = true;
+
+        stack<int> stk;
+        stk.push(0);
+
+        while (!stk.empty())
+        {
+            auto& keys = rooms[stk.top()];
+            stk.pop();
+
+            for (int newKey :keys)
+                if(!seen[newKey])
+                    seen[newKey] = true, 
+                        stk.push(newKey);
+        }
+
+        for (bool visited : seen)
+            if(!visited)
+                return false;
+
+        return true;
     }
 };
