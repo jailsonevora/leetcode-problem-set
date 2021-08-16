c00c46c2eabf6b26366359236b9ad727cac9dfc5
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-16 11:43:47 -0100

    01 Matrix

--- Queue & Stack/16._01Matrix.cpp
+++ Queue & Stack/16._01Matrix.cpp
@@ -6,8 +6,29 @@
 using namespace std;
 
 class Solution {
+    void DFS(vector<vector<int>> &grid, int x, int y, int r, int c){
+        if (x < 0 || x >= r || y < 0 || y >= c || grid[x][y] != 1)
+            return;
+        
+        grid[x][y] +=1 ;
+
+        //recursive call to 4 adjacent directions
+        DFS(grid, x+1, y, r, c);
+        DFS(grid, x, y+1, r, c);
+        DFS(grid, x-1, y, r, c);
+        DFS(grid, x, y-1, r, c);
+    }
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
+        int rows = mat.size();
+        int cols = mat[0].size();
+
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                if (mat[r][c] == 1)
+                {   
+                    DFS(mat, r,c, rows, cols);
+                }
+        return mat;    
     }
 };
