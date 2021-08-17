622db0e901cf16b8328a05e8aaf9ab0780427d02
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-17 14:31:40 -0100

    Binary Tree Preorder Traversal

--- Tree/1.BinaryTreePreorderTraversal.cpp
+++ Tree/1.BinaryTreePreorderTraversal.cpp
@@ -16,4 +16,19 @@ using namespace std;
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
 
+ class Solution {
+     void DFS_Recursive(TreeNode* root, vector<int>&ans){
+        if (!root)
+            return;
+        
+
+     }
+public:
+    vector<int> preorderTraversal(TreeNode* root) { 
+        vector<int> ans;     
+        DFS_Recursive(root, ans);
+        return ans;
+    }
+};
+
 
