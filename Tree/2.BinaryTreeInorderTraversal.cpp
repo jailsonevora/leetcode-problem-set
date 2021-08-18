77edc3399119153c903f1f708b5b8b322b55bcf6
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-18 21:40:16 -0100

    Binary Tree Inorder Traversal

--- Tree/2.BinaryTreeInorderTraversal.cpp
+++ Tree/2.BinaryTreeInorderTraversal.cpp
@@ -18,6 +18,20 @@ using namespace std;
  };
 
  class Solution {
+    void DFS_Recursive_InorderTraversal(TreeNode* root, vector<int>&ans){
+
+        if (!root)
+            return;
+
+        if (root->left)
+            DFS_Recursive_InorderTraversal(root->left, ans);
+
+        ans.push_back(root->val);
+
+        if (root->right)
+            DFS_Recursive_InorderTraversal(root->right, ans);
+
+    }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         
