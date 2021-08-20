f87b794338ad04ac5e974370bf94624ddd13a920
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-20 16:07:20 -0100

    Maximum Depth of Binary Tree

--- Tree/5.MaximumDepthofBinaryTree.cpp
+++ Tree/5.MaximumDepthofBinaryTree.cpp
@@ -27,4 +27,32 @@ class Solution {
         
         return std::max(left,right)+1;
     }
+
+    int BFS_levelOrder(TreeNode* root) {
+        queue<TreeNode*> queue;
+        int ans;
+
+        if(!root)
+            return 0;
+
+        queue.push(root);
+
+        while (!queue.empty())
+        {
+            int size = queue.size();
+            for (int i = 0; i < size; ++i)
+            {        
+                TreeNode* curr = queue.front();
+                queue.pop();              
+                
+                if (curr->left)
+                    queue.push(curr->left);
+                
+                if (curr->right)
+                    queue.push(curr->right);
+            }
+            ans++;
+        }
+        return ans;
+    }
 };
