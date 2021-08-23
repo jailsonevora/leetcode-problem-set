8fb057bf657193e16e809bd46324905289a2cd62
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-23 12:33:04 -0100

    Symmetric Tree

--- Tree/6.SymmetricTree.cpp
+++ Tree/6.SymmetricTree.cpp
@@ -20,19 +20,16 @@ using namespace std;
  class Solution {
     bool DFS_Recursive_PreOrderTraversal(TreeNode* rootL, TreeNode* rootR){
 
-        if (!rootL || !rootR)
-            return false;
-        
         if (!rootL && !rootR)
             return true;
-
-        if(rootL->val != rootR->val)
+        
+        if (!rootL || !rootR)
             return false;
 
         return ( 
-            rootL->val == rootR->val 
-            && DFS_Recursive_PreOrderTraversal(rootL->left,rootR->right) 
-            && DFS_Recursive_PreOrderTraversal(rootL->right,rootR->left) 
+                rootL->val == rootR->val 
+                && DFS_Recursive_PreOrderTraversal(rootL->left,rootR->right) 
+                && DFS_Recursive_PreOrderTraversal(rootL->right,rootR->left) 
             );
     }
 public:
