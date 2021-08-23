310fa7fe3fa0bfec4da5ad2b0ab7c9bfe100d9d4
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-23 15:09:54 -0100

    Path Sum

--- Tree/7.PathSum.cpp
+++ Tree/7.PathSum.cpp
@@ -44,4 +44,18 @@ public:
 
         return ans;
     }
+
+    //botton-up aproach
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        if(!root) 
+            return false;
+        
+        targetSum -= root->val;
+        
+        if(!root->left && !root->right)
+            return targetSum == 0;
+
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    }
 };
