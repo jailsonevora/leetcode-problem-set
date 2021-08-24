0c3c35b0fe65257cdbc902e5ab3dc1358cc3d0eb
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-24 11:57:13 -0100

    Construct Binary Tree from Inorder and Postorder Traversal

--- Tree/8.ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
+++ Tree/8.ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -25,7 +25,8 @@ using namespace std;
         TreeNode *root = new TreeNode(postorder[p2-1]);
         auto it = find(inorder.begin() + i1, inorder.begin() + i2, postorder[p2-1]);
         int diff = it - inorder.begin() - i1;
-        root->left = buildTree(inorder, 0, n, postorder, 0 n);
+        root->left = buildTree(inorder, i1, i1 + diff, postorder, p1, p1 + diff);
+        root->right = buildTree(inorder, i1 + diff + 1, i2, postorder, p1 + diff,  n);
     }
 
 public:
