503f531ed22a5c8e85394f6d69b1296eb00f6b33
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-24 13:44:56 -0100

    Construct Binary Tree from Preorder and Inorder Traversal

--- Tree/9.ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
+++ Tree/9.ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -19,15 +19,21 @@ using namespace std;
 
 class Solution {
     TreeNode* buildTree(vector<int>& preorder, int p1, int p2, vector<int>& inorder, int i1, int i2) {
-        if(p1 >= p2 || i1 >= i2)
+        if(p1 > p2)
             return nullptr;
         
-        TreeNode *root = new TreeNode(preorder[p1 - 1]);
-        auto it = find(inorder.begin() + i1, inorder.begin() + i2, inorder[i2-1]);
-        int diff = it - inorder.begin() - i1;
+        TreeNode *root = new TreeNode(preorder[p1]);
+        int i = i1;
+        while (i <= i2) {
+            if (inorder[i] == root->val) break;
+            i ++;
+        }
+
+        int diff = i - i1 - 1;
+        p1++;
         
         root->left = buildTree(preorder, p1, p1 + diff, inorder, i1, i1 + diff);
-        root->right = buildTree(preorder, p1 + diff + 1, p2, inorder, it + 1,  i2);
+        root->right = buildTree(preorder, p1 + diff + 1, p2, inorder, i + 1,  i2);
         
         return root;
     }
@@ -35,7 +41,8 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         if(!inorder.size())
             return nullptr;
-        return buildTree(preorder, 0, preorder.size(), inorder, 0, inorder.size());
+        return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
     }
 };
+
 
