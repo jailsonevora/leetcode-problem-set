c773010b1469f22f2b97b36840ccf322df0fcfe7
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-25 11:27:49 -0100

    Populating Next Right Pointers in Each Node II

--- Tree/11.PopulatingNextRightPointersInEachNodeII.cpp
+++ Tree/11.PopulatingNextRightPointersInEachNodeII.cpp
@@ -22,3 +22,61 @@ public:
     Node(int _val, Node* _left, Node* _right, Node* _next)
         : val(_val), left(_left), right(_right), next(_next) {}
 };
+
+
+class Solution {
+    void connectRecursive(Node *root)
+    {
+        if (!root)
+            return;
+        
+        if (root->left)
+            root->left->next = root->right;
+
+        if (root->right)
+            root->right->next = root->next ? root->next->left : NULL;
+        
+        connectRecursive(root->left);
+        connectRecursive(root->right);
+    }
+public:
+    Node* connect(Node* root) {
+
+        if(!root)
+            return nullptr;
+
+        queue<Node*> queue;
+        queue.push(root);
+
+        while (!queue.empty())
+        {
+            int size = queue.size();
+            while (size--)
+            {
+                Node* curr = queue.front();
+                queue.pop();
+
+                if(!size)
+                    curr->next = nullptr;
+                else
+                    curr->next = queue.front();
+
+                if(curr->left)
+                    queue.push(curr->left);
+                
+                if (curr->right)
+                    queue.push(curr->right);
+            }            
+        }
+        return root;        
+    }
+
+    // recursive top-down
+    Node* connect(Node* root) {
+
+        if(!root)
+            return nullptr;
+        connectRecursive(root);        
+        return root;        
+    }
+};
