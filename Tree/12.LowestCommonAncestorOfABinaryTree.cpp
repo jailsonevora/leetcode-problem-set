c52057c258d822f7d9e909d658843f7cc7b45c6c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-25 14:32:57 -0100

    Lowest Common Ancestor of a Binary Tree

--- Tree/12.LowestCommonAncestorOfABinaryTree.cpp
+++ Tree/12.LowestCommonAncestorOfABinaryTree.cpp
@@ -17,6 +17,11 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if(p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else if(p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+        else
+            return root;
     }
 };
