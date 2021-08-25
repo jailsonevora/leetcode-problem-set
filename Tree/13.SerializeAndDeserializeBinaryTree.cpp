23c2c9ac331c65c733d4f090d5232aa4a669c9ea
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-25 14:57:50 -0100

    Serialize and Deserialize Binary Tree

---
+++ Tree/13.SerializeAndDeserializeBinaryTree.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+#include<unordered_set>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.*/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
