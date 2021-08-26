8f90c4f71c24be168a0ed4c12603f0454eac80fe
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-26 14:54:38 -0100

    Fibonacci Number

--- Recursion1/6.FibonacciNumber.cpp
+++ Recursion1/6.FibonacciNumber.cpp
@@ -10,6 +10,11 @@ using namespace std;
 class Solution {
 public:
     int fib(int n) {
-        
+
+        if(n < 2)
+            return n;
+
+        return fib(n-1) + fib(n-2);       
     }
+    
 };
