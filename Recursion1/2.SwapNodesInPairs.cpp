0de5597c1bc959e61ff8cfa8eff90c3290213154
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-26 11:04:47 -0100

    Swap Nodes in Pairs

--- Recursion1/2.SwapNodesInPairs.cpp
+++ Recursion1/2.SwapNodesInPairs.cpp
@@ -12,3 +12,22 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head)
+            return head;
+
+        ListNode* first = head;
+        ListNode* second = first->next;
+
+        if (!second)
+            return first;
+
+        ListNode* third = swapPairs(second->next);       
+                    
+        second->next = first;
+        first->next = third;
+    }
+};
