ae9a44c537e4ca2955cf46e6d7b4887c9d289719
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-27 11:32:49 -0100

    Maximum Depth of Binary Tree

--- Recursion1/8.MaximumDepthOfBinaryTree.cpp
+++ Recursion1/8.MaximumDepthOfBinaryTree.cpp
@@ -9,3 +9,21 @@
 #include <string.h>
 
 using namespace std;
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        // BFS-LevelOrder aproach Iteratively
+        //BFS_Iteratively_levelOrder_maxDepth(root);
+        
+        // Bottom-Up aproach recrusively
+        if(!root)
+            return 0;        
+        
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        
+        return std::max(left,right)+1;
+    }
+};
