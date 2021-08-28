b2a81a125d09675d5107cccd713896c58aaed755
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-28 00:26:13 -0100

    pow

--- Recursion1/9.Pow.cpp
+++ Recursion1/9.Pow.cpp
@@ -12,3 +12,11 @@ using namespace std;
 
 class Solution {
 public:
+    double myPow(double x, int n) {
+        if(n==0)
+            return 1;
+        int temp = abs(n);
+        double ans = myPow(x*x, temp/2) * (temp%2 ? x : 1);
+        return (n>0 ? ans : 1/ans);
+    }
+};
