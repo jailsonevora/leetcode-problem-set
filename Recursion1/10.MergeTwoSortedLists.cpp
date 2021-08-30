98e95866d2ecc3c5b6869addc4813079853b37c1
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-30 09:30:05 -0100

    Merge Two Sorted Lists

--- Recursion1/10.MergeTwoSortedLists.cpp
+++ Recursion1/10.MergeTwoSortedLists.cpp
@@ -63,3 +63,18 @@ public:
             curr->next->next = NULL;
         }
     }
+};
+
+int main(){
+    
+    Solution l1, l2;
+    l1.pushBack(1);
+    l1.pushBack(2);
+    l1.pushBack(4);
+
+    l2.pushBack(1);
+    l2.pushBack(3);
+    l2.pushBack(4);
+
+    l1.mergeTwoLists(l1.head, l2.head);
+}
