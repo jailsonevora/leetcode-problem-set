277288a2f94ed61794f8249cd6a08c4aa3637fcb
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-30 11:30:20 -0100

    Sort an Array

--- Recursion2/1.SortAnArray.cpp
+++ Recursion2/1.SortAnArray.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
 class Solution {
+    void mergesort(vector<int>& nums,vector<int>& tmp, int leftStart, int rightEnd){
+        if(leftStart >= right)
+            return;
+
+        int middle = (leftStart + rightEnd) / 2;
+        mergesort(nums, tmp, leftStart, middle);
+        mergesort(nums, tmp, middle + 1, rightEnd);
+        mergeHalves(nums, tmp, leftStart, rightEnd);
+    }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        
+        mergesort(vector<int>& nums,vector<int>& tmp, int leftStart, int rightEnd);
     }
 };
