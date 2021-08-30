6521597efb5d0f79c614ed9ea88735c0df151524
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-30 11:52:56 -0100

    Sort an Array

--- Recursion2/1.SortAnArray.cpp
+++ Recursion2/1.SortAnArray.cpp
@@ -5,9 +5,44 @@
 using namespace std;
 
 class Solution {
-    void mergeHalves(vector<int>& nums,vector<int>& tmp, int leftStart, int rightEnd){
+    // vector<int> mergeHalves(vector<int>& nums, int i, vector<int>& tmp, int j){
+    //     if(!nums.size())
+    //         return tmp;
+    //     if(!tmp.size())
+    //         return nums;
 
-    }
+    //     if(nums[i] >= tmp[j])
+    //         tmp[j+1] = mergeHalves(nums, i, tmp, j + 1);
+    //     else
+    //     {
+    //         nums[i+1]=mergeHalves(nums,i + 1, tmp, i);
+    //         tmp=nums;
+    //     }
+    //     return tmp;
+    // }
+    void mergeHalves(vector<int>& nums,vector<int>& tmp, int leftStart, int rightEnd){
+        int leftEnd = (rightEnd + leftStart) / 2;
+        int rightStart = leftEnd + 1;
+        int size = rightStart - leftStart + 1;
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        while (left <= leftEnd && right <= rightEnd)
+        {
+            if(nums[left] <= nums[right]){
+                tmp[index] = nums[left];
+                left++;
+            }else{
+                tmp[index] = nums[right];
+                right++;
+            }
+            index++;
+        }
+        
+    } 
+
     void mergesort(vector<int>& nums,vector<int>& tmp, int leftStart, int rightEnd){
         if(leftStart >= rightEnd)
             return;
