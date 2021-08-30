c6676583d794f3337758fb22cfbb3edbeb8a4444
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-30 10:25:13 -0100

    Unique Binary Search Trees II

--- Recursion1/12.UniqueBinarySearchTreesII.cpp
+++ Recursion1/12.UniqueBinarySearchTreesII.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<map>
 
 using namespace std;
 
@@ -11,4 +13,41 @@ struct TreeNode {
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+     //with dp using ordered_map
+    vector<TreeNode*> help(int start,int end,map<pair<int,int>,vector<TreeNode*>> &dp){
+        if(start>end){
+            //return NULL;
+           vector<TreeNode*> ans;
+            ans.push_back(NULL);
+            return ans;
+        }
+        
+        if(dp.count({start,end}))
+            return dp[{start,end}];
+         
+        vector<TreeNode*> ans;
+        for(int i=start;i<=end;i++){
+            //TreeNode* root = new TreeNode(i);
+            vector<TreeNode*> left = help(start,i-1,dp);
+            vector<TreeNode*> right = help(i+1,end,dp);
+            for(TreeNode* l:left){
+                for(TreeNode* r:right){
+                    TreeNode* root = new TreeNode(i);
+                    root->left = l;
+                    root->right = r;
+                    ans.push_back(root);
+                }
+            }
+        }
+        
+        dp[{start,end}] = ans;
+        return ans;
+    }
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        
+    }
 };
