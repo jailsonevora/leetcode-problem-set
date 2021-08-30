97e64b8d48ac1d0bd3810ae65403381997f96b9a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-30 10:02:56 -0100

    K-th Symbol in Grammar

--- Recursion1/11.K-thSymbolInGrammar.cpp
+++ Recursion1/11.K-thSymbolInGrammar.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        
+        if(n==1 and k==1)
+            return 0;
+        int mid = pow(2,n-1)/2;
+        if(k<=mid)
+            return kthGrammar(n-1,k);
+        return !kthGrammar(n-1,k-mid);
     }
 };
