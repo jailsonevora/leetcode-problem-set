17b41164cc3f14432c3346e6090cce9faba2237c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-30 09:29:45 -0100

    Merge Two Sorted Lists

--- Recursion1/10.MergeTwoSortedLists.cpp
+++ Recursion1/10.MergeTwoSortedLists.cpp
@@ -20,3 +20,46 @@ struct ListNode {
 
 class Solution {
 public:
+
+    ListNode *head = NULL;
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+        ListNode *headL1 = l1, *headL2 = l2, *headTemp = NULL;
+
+        while (headL1 && headL2)
+        {
+            if (headL1->val <= headL2->val)
+                pushBack(headTemp, headL1);
+            else
+                pushBack(headTemp, headL2);
+        }
+
+        while (headL1)
+            pushBack(headTemp, headL1);
+
+        while (headL2)
+            pushBack(headTemp, headL2);
+        
+        return headTemp;
+    }
+
+    void pushBack(ListNode *&head, ListNode *&tempNode)
+    {
+        ListNode* curr = head;
+
+        if (!head){  
+            head = tempNode;
+            tempNode = tempNode->next;
+            head->next = NULL;
+        } 
+        else{
+
+            while(curr && curr->next) 
+                curr = curr->next;
+            curr->next = tempNode;
+
+            tempNode = tempNode->next;
+            curr->next->next = NULL;
+        }
+    }
