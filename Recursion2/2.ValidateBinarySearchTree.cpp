a35701ca93aea35bedd0ef224692050a3b322438
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-31 09:23:44 -0100

    Search a 2D Matrix II

--- Recursion2/2.ValidateBinarySearchTree.cpp
+++ Recursion2/2.ValidateBinarySearchTree.cpp
@@ -15,8 +15,20 @@ using namespace std;
 };
 
 class Solution {
+    TreeNode* prev = nullptr;
 public:
     bool isValidBST(TreeNode* root) {
         
+        if(!root)
+            return true;
+
+        if(!isValidBST(root->left))
+            return false;
+        
+        if (prev && root->val <= prev->val)
+            return false;
+            
+        prev = root;
+        return isValidBST(root->right);
     }
 };
