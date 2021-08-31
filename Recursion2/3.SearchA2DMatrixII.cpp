7b3cc2c187a129adca2254f0f081592d7e3d9121
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-31 09:33:48 -0100

    Search a 2D Matrix II

--- Recursion2/3.SearchA2DMatrixII.cpp
+++ Recursion2/3.SearchA2DMatrixII.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int row = 0, col = matrix[0].size();
         
     }
 };
