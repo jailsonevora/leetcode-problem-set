cafa0416867479f30b44a034cecbf6ab851de977
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-31 09:38:49 -0100

    Search a 2D Matrix II

--- Recursion2/3.SearchA2DMatrixII.cpp
+++ Recursion2/3.SearchA2DMatrixII.cpp
@@ -21,3 +21,20 @@ public:
         return false;        
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+    vector<vector<int>> mat = {
+        {1,4,7,11,15},
+        {2,5,8,12,19},
+        {3,6,9,16,22},
+        {10,13,14,17,24},
+        {18,21,23,26,30}
+    };
+
+    sl.searchMatrix(mat, 5);
+}
