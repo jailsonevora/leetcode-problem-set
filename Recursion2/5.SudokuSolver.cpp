9ea7b0b2749c794da549d3f3aa9e38fbcee97a55
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-08-31 12:55:39 -0100

    Sudoku Solver

--- Recursion2/5.SudokuSolver.cpp
+++ Recursion2/5.SudokuSolver.cpp
@@ -10,3 +10,21 @@ public:
         
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    Solution sl;
+    vector<vector<int>> board = {
+        {1,4,7,11,15},
+        {2,5,8,12,19},
+        {3,6,9,16,22},
+        {10,13,14,17,24},
+        {18,21,23,26,30}
+    };
+
+    // recursive
+    sl.solveSudoku(board);
+}
