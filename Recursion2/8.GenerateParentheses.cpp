d60623533b9c6b1eeaa5ad1678585d6b800a7186
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-01 09:41:37 -0100

    Generate Parentheses

--- Recursion2/8.GenerateParentheses.cpp
+++ Recursion2/8.GenerateParentheses.cpp
@@ -4,3 +4,24 @@
 #include<unordered_set>
 
 using namespace std;
+
+class Solution {
+    void backtracking(vector<string> &ans, string cur, int openParentheses, int closeParentheses, int n){
+        if(cur.size() == n * 2){
+            ans.push_back(cur);
+            return;
+        }
+
+        if(openParentheses < n)
+            backtracking(ans, cur+"(", openParentheses+1, closeParentheses, n);
+
+        if(closeParentheses < openParentheses)
+            backtracking(ans, cur+")", openParentheses, closeParentheses+1, n);
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        backtracking(ans,"", 0, 0, n);
+        return ans;
+    }
+};
