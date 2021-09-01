05114f8a4750b03d096fbe8735162f9032da8268
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-01 15:22:45 -0100

    Binary Search

--- Binary Search/1.BinarySearch.cpp
+++ Binary Search/1.BinarySearch.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -7,7 +8,28 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        int tail = n
+        //sort(nums.begin(), nums.end());
+
+        int head = 0, tail = nums.size() - 1;
+        
+
+        while(head < tail)
+        {
+            int midle = head + (tail + head) / 2;
+        }
+        
+
         
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    vector<int> v = {-1,0,3,5,9,12};
+
+    Solution sl;
+    sl.search(v,9);
+}
