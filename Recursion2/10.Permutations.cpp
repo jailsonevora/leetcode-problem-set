785da4366d8640605f4400b320025d46541ad349
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-01 13:15:15 -0100

    Permutations

--- Recursion2/10.Permutations.cpp
+++ Recursion2/10.Permutations.cpp
@@ -6,8 +6,20 @@
 using namespace std;
 
 class Solution {
+    void permute(vector<int> nums, int i, vector<vector<int>>& perms) {
+        if (i == nums.size()) {
+            perms.push_back(nums);
+        } else {
+            for (int j = i; j < nums.size(); j++) {
+                swap(nums[i], nums[j]);
+                permute(nums, i + 1, perms);
+            }
+        }
+    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        
-    }
+        vector<vector<int>> perms;
+        permute(nums, 0, perms);
+        return perms;
+    }    
 };
