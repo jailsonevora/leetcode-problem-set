5d26eafd089bed67161769ea8e7fc08479c70e76
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-03 15:48:26 -0100

    Balanced Binary Tree

--- Binary Search Tree/7.BalancedBinaryTree.cpp
+++ Binary Search Tree/7.BalancedBinaryTree.cpp
@@ -15,3 +15,23 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+class Solution {
+    bool ans = true;
+    int height(TreeNode*root){
+        
+        if(!root)
+            return 0;
+        
+        int l = height(root->left);
+        int r = height(root->right);
+        ans = ans & abs(l-r) < 2;
+        
+        return max(l,r) + 1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        height(root);
+        return ans;
+    }
+};
