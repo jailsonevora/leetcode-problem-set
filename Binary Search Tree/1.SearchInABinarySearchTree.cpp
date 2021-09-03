2793385385b0b1aaee9ae1f05f50a944eb0755fa
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-03 10:40:50 -0100

    Search in a Binary Search Tree

--- Binary Search Tree/1.SearchInABinarySearchTree.cpp
+++ Binary Search Tree/1.SearchInABinarySearchTree.cpp
@@ -16,3 +16,18 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root)
+            return root;
+
+        if(root->val == val)
+            return root;
+        else if(root->val > val)
+            return searchBST(root->left, val);
+        else
+            return searchBST(root->right, val);        
+    }
+}; 
