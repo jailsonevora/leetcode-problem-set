05166e51f4bdb8600455630278049f4a7d7e9a32
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-03 14:50:14 -0100

    Kth Largest Element in a Stream

--- Binary Search Tree/5.KthLargestElementInAStream.cpp
+++ Binary Search Tree/5.KthLargestElementInAStream.cpp
@@ -4,3 +4,20 @@
 #include<unordered_set>
 
 using namespace std;
+
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) {
+        
+    }
+    
+    int add(int val) {
+        
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
