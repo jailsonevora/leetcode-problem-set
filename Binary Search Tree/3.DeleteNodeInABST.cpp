577e84054379c7c79f27154aa527e1a932a51d40
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-03 11:41:34 -0100

    Delete Node in a BST

--- Binary Search Tree/3.DeleteNodeInABST.cpp
+++ Binary Search Tree/3.DeleteNodeInABST.cpp
@@ -17,9 +17,28 @@ using namespace std;
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
 
- class Solution {
+class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         
+        if (!root) return root; //only run in case we are not able to find node
+        
+        if (root->val == key){
+            if(!root->left)
+                return root->right;    //if only right child exist
+            if(!root->right) return root->left;     //if only left child exist
+            if(root->right!=NULL && root->left!=NULL){        //if both child exist then find min from right
+                TreeNode *temp = root->right;
+                while(temp->left!=NULL) temp = temp->left;
+                root->val = temp->val;
+                root->right = deleteNode(root->right,root->val);
+                
+            }
+        }
+        else{
+            if (root->val>key)  root->left = deleteNode(root->left,key);
+            else    root->right = deleteNode(root->right,key);
+        }
+        return root;
     }
 };
