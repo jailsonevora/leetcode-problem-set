09ce897de9b7818dd6a8618819cff2f196b7906a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-03 14:19:26 -0100

    Binary Search Tree Iterator

--- Binary Search Tree/4.BinarySearchTreeIterator.cpp
+++ Binary Search Tree/4.BinarySearchTreeIterator.cpp
@@ -16,3 +16,32 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+ class BSTIterator {
+
+    vector<int> nodes;
+    int curr = 0;
+
+    void inOrder(TreeNode* root) {
+        
+        if (!root)
+            return;
+        inOrder(root->left);
+        nodes.push_back(root->val);
+        inOrder(root->right);
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        inOrder(root);
+    }
+    
+    int next() {
+        curr++;
+        return nodes[curr-1];
+    }
+    
+    bool hasNext() {
+        return curr != nodes.size();
+    }
+};
