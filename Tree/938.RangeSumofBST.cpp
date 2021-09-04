06c5907273116355da37dd8e4c667e67d39b7077
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-04 17:49:09 -0100

    938. Range Sum of BST

--- Tree/938.RangeSumofBST.cpp
+++ Tree/938.RangeSumofBST.cpp
@@ -16,3 +16,50 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+class Solution {
+    //iteratively
+    void dfs_inorder(TreeNode* root, int low, int high, int& ans){
+        
+        stack<TreeNode*> stk;
+        
+        if(!root)
+            return;
+        
+        stk.push(root);
+        
+        while(!stk.empty()){
+            
+            TreeNode* tmp = stk.top();
+            stk.pop();
+            
+            if(low <= tmp->val && tmp->val <= high)
+                ans += tmp->val;
+            
+            if(tmp->left)
+                stk.push(tmp->left);
+            
+            if(tmp->right)
+                stk.push(tmp->right);                
+        }
+    }
+    
+    // recursive
+    /*void dfs_inorder(TreeNode* root, int low, int high, int& ans){
+        
+        if(!root)
+            return;
+        
+        if(low <= root->val && root->val <= high)
+            ans += root->val;
+        
+        dfs_inorder(root->left, low, high, ans);
+        dfs_inorder(root->right, low, high, ans);
+    }*/
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int ans = 0;
+        dfs_inorder(root, low, high, ans);
+        return ans;        
+    }
+};
