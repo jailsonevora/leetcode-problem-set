e337f53316b469debfd90de512bdde0c6996e17b
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-06 12:38:15 -0100

    1791. Find Center of Star Graph

--- Graph/1791.FindCenterofStarGraph.cpp
+++ Graph/1791.FindCenterofStarGraph.cpp
@@ -3,3 +3,31 @@
 #include <vector>
 
 using namespace std;
+
+
+// Data structure to store a graph edge
+struct Edge {
+    int src, dest;
+};
+ 
+// A class to represent a graph object
+class Graph
+{
+public:
+    // a vector of vectors to represent an adjacency list
+    vector<vector<int>> adjList;
+ 
+    // Graph Constructor
+    Graph(vector<Edge> const &edges, int N)
+    {
+        // resize the vector to hold `N` elements of type `vector<int>`
+        adjList.resize(N);
+ 
+        // add edges to the undirected graph
+        for (auto &edge: edges)
+        {
+            adjList[edge.src].push_back(edge.dest);
+            adjList[edge.dest].push_back(edge.src);
+        }
+    }
+};
