0df442e38d2b0037cda1ddf56c82fe89ca96d657
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-06 11:54:39 -0100

    617. Merge Two Binary Trees

--- Tree/617.MergeTwoBinaryTrees.cpp
+++ Tree/617.MergeTwoBinaryTrees.cpp
@@ -36,14 +36,17 @@ public:
     //iteratively with stack
     TreeNode* dfs_stack(TreeNode* root1, TreeNode* root2){
 
-        stack<TreeNode*> st1, st2;
+        if (!root1 && !root2)
+            return nullptr;
+        if (!root1 || !root2) 
+        return root1 ? root1 : root2;
 
-        if(root1)
+        stack<TreeNode*> st1, st2;
 
         st1.push(root1);
         st2.push(root2);
 
-        while(!st1.empty() || st2.empty()){
+        while(!st1.empty()){
             
             TreeNode* tmp1 = st1.top();
             TreeNode* tmp2 = st2.top();
@@ -53,16 +56,16 @@ public:
             
             if(!tmp1->left && tmp2->left)
                 tmp1->left = tmp2->left;                
-            else if (!tmp1->left && tmp2->left){
+            else if (tmp1->left && tmp2->left){
                 st1.push(tmp1->left);
-                st1.push(tmp2->left);
+                st2.push(tmp2->left);
             }
 
             if(!tmp1->right && tmp2->right)
                 tmp1->right = tmp2->right;                
-            else if (!tmp1->right && tmp2->right){
+            else if (tmp1->right && tmp2->right){
                 st1.push(tmp1->right);
-                st1.push(tmp2->right);
+                st2.push(tmp2->right);
             }
         }
         return root1;
