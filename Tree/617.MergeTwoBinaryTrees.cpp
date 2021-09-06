89f87a84f8d95022bd719b37431395ac3a159ba0
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-06 11:01:06 -0100

    617. Merge Two Binary Trees

--- Tree/617.MergeTwoBinaryTrees.cpp
+++ Tree/617.MergeTwoBinaryTrees.cpp
@@ -16,3 +16,19 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        
+        if (!root1 || !root2) 
+            return root1 ? root1 : root2;
+
+        TreeNode* node = new TreeNode(root1->val + root2->val);
+
+        node->left = mergeTrees(root1->left, root2->left);
+        node->right = mergeTrees(root1->right, root2->right);
+        
+        return node;
+    }
+};
