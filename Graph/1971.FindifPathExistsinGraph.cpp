3bef06c0454bea78fb1ac2455ba20458fc24dc2e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-07 10:01:56 -0100

    1971. Find if Path Exists in Graph

--- Graph/1971.FindifPathExistsinGraph.cpp
+++ Graph/1971.FindifPathExistsinGraph.cpp
@@ -32,3 +32,28 @@ public:
         }
     }
 };
+
+int main()
+{
+    // vector of graph edges as per the above diagram
+    vector<Edge> edges = {
+        {1, 2}, {2, 3}, {4, 2}
+        // vertex 0, 13, and 14 are single nodes
+    };
+ 
+    // total number of nodes in the graph
+    // number of node + zero index in c++
+    int N = 5;
+ 
+    // build a graph from the given edges
+    Graph graph(edges, N);
+ 
+    // to keep track of whether a vertex is discovered or not
+    vector<bool> discovered(N, false);
+    
+    Solution sl;
+
+    cout << sl.findCenter(graph.adjList);
+    
+    return 0;
+}
