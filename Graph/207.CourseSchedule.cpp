2ae994e2f4d062592472e0a1da64c8d390f801d5
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-08 00:20:15 -0100

    207. Course Schedule

---
+++ Graph/207.CourseSchedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    bool dfs(int v, vector<vector<int>>& graph, vector<bool> &discovered){
+
+        if(discovered[v] == 1) // viseted
+            return false;
+        if(discovered[v] == 2) //processed
+            return true;
+        
+        // mark the current node as discovered
+        discovered[v] = 2;
+
+        // do for every edge `v —> u`
+        for (int u: graph[v])
+            if(dfs(u, graph, discovered))
+                return true;
+        
+        discovered[v] = 1; //processed
+        return false;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        
+        vector<vector<int>> graph(numCourses);
+        for (auto &edge: prerequisites)
+        {
+             graph[edge[0]].push_back(edge[1]);
+             //graph[edge[1]].push_back(edge[0]);
+        }
+        
+        vector<bool> discovered(numCourses, 0);
+
+        
+        for(int node = 0; node < numCourses; ++node)
+            if (discovered[node] == 0)
+                if(dfs(node, graph, discovered))
+                    return false;
+        return true;
+    }   
+};
