54a609e7ae4dcde8d77e9f219860a4676ee5ad92
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-08 09:06:55 -0100

    207. Course Schedule

--- Graph/207.CourseSchedule.cpp
+++ Graph/207.CourseSchedule.cpp
@@ -9,6 +9,27 @@ struct Edge {
     int src, dest;
 };
 
+// A class to represent a graph object
+class Graph {
+public:
+    // a vector of vectors to represent an adjacency list
+    vector<vector<int>> adjList;
+ 
+    // Graph Constructor
+    Graph(vector<Edge> const &edges, int N)
+    {
+        // resize the vector to hold `N` elements of type `vector<int>`
+        adjList.resize(N);
+ 
+        // add edges to the undirected graph
+        for (auto &edge: edges)
+        {
+            adjList[edge.src].push_back(edge.dest);
+            adjList[edge.dest].push_back(edge.src);
+        }
+    }
+};
+
 class Solution {
     bool dfs(int v, vector<vector<int>>& graph, vector<bool> &discovered){
 
