a65a63f88ff34a833c768e921937b60f4aa4ed46
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-09 09:22:39 -0100

    210. Course Schedule II

--- Graph/210.CourseScheduleII.cpp
+++ Graph/210.CourseScheduleII.cpp
@@ -29,3 +29,29 @@ public:
         }
     }
 };
+
+int main()
+{
+    // vector of graph edges as per the above diagram
+    vector<Edge> edges = {
+        {0, 1}, {1, 2}, {2, 0}
+    };
+
+    // vector<Edge> edges = {
+    //     {1,0},{0,1}
+    // };
+
+    // vector<Edge> edges = {
+    //     {0,1},{3,1},{1,3},{3,2}
+    // };
+ 
+    // total number of nodes in the graph
+    // number of node + zero index in c++
+    int N = 4;
+ 
+    // build a graph from the given edges
+    Graph graph(edges, N);
+    
+    Solution sl;
+    std::cout << sl.canFinish(N, graph.adjList);
+}
