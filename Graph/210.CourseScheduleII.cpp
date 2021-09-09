28661d16c46d31a5e5324f064f99a23fab363184
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-09 09:23:01 -0100

    210. Course Schedule II

--- Graph/210.CourseScheduleII.cpp
+++ Graph/210.CourseScheduleII.cpp
@@ -30,6 +30,73 @@ public:
     }
 };
 
+class Solution {
+    //iteratively
+    bool dfs_iteratively(int v, vector<vector<int>>& graph, vector<int> &discovered){
+
+        // create a stack used to do iterative DFS
+        stack<int> stack;
+    
+        stack.push(v);
+
+        discovered[v] = 2;
+        
+        while (!stack.empty())
+        {
+            int node = stack.top();
+            stack.pop();
+            
+            for (int u : graph[node])
+            {
+                
+                if(discovered[u] == 2)
+                    return true;
+
+                if (!discovered[u])
+                    stack.push(u);
+            };
+        }
+        discovered[v] = 1;
+        return false;
+    }
+    // recursive
+    bool dfs(int v, vector<vector<int>>& graph, vector<int> &discovered){
+
+        if(discovered[v] == 1) // viseted
+            return false;
+        if(discovered[v] == 2) // processing
+            return true;
+        
+        // mark the current node as processing
+        discovered[v] = 2;
+
+        // do for every edge `v —> u`
+        for (int u: graph[v])
+            if(dfs(u, graph, discovered))
+                return true;
+        
+        discovered[v] = 1; // viseted
+        return false;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        //to convert edge list to adjacent list
+        //vector<vector<int>> graph(numCourses);
+        /*for (auto &edge: prerequisites)
+            graph[edge[0]].push_back(edge[1]);*/
+        
+        vector<int> discovered(numCourses, 0);
+
+        for(int node = 0; node < numCourses; node++)
+            //if (!discovered[node])
+                //if(dfs(node, graph, discovered))
+                    if(dfs_iteratively(node, prerequisites, discovered))
+                        return false;
+        return true;
+    }   
+};
+
 int main()
 {
     // vector of graph edges as per the above diagram
