890c88dc71096c6718914b7058ae02be058b1b72
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-10 10:29:37 -0100

    310. Minimum Height Trees

--- Graph/310.MinimumHeightTrees.cpp
+++ Graph/310.MinimumHeightTrees.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <unordered_set>
 #include <vector>
 #include <algorithm>
 
@@ -34,72 +35,66 @@ public:
 };
 
 class Solution {
-    // void _dfs(int u, vector<vector<int>>& graph, int& min, int ans = 1){
-
-    //     for (int v : graph[u])
-    //         _dfs(v, graph, min, ans+1);
-    //     ans = std::min(ans, min);
-    // }
-    int _bfs(int u, vector<vector<int>>& graph, vector<int>& discovery){
-
-        queue<int> queue;
-        int ans = 0;
-
-        queue.push(u);        
-        discovery[u] = 2;
-
-        while (!queue.empty())
-        {
-            int size = queue.size();
-            for (int i = 0; i < size; i++)
-            {
-                int node = queue.front();
-                queue.pop();
-
-                for(auto v: graph[node]){
-                    if (discovery[v] != 1){
-                        queue.push(v);
-                        if (!(discovery[v] == 2))
-                            discovery[v] = 1;
-                    }
-                }                
-            }
-            ans++;            
-        }
-        return ans;        
-    }
 public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& graph) {
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
 
-        //to convert edge list to adjacent list
-        // vector<vector<int>> graph(n);
-        // for (auto &edge: edges)
-        //     graph[edge[0]].push_back(edge[1]);
-        //     graph[edge[1]].push_back(edge[0]);
-
-        int min = INT_MAX;
-        vector<int> tmp(n), discovery(n, 0);
-
-        //for each unvisited node traverse and push to global stack
-        for(int node = 0; node < n; node++)
-        {
-            if(discovery[node] == 2)
-                continue;
-
-            int ans = _bfs(node, graph, discovery);
-            if(ans <= min){
-                min = ans;
-                tmp.insert(tmp.begin()+node,ans);
-            }
-            for(int i=0; i < tmp.size(); ++i)
-                if(tmp[i] >= ans)
-                    tmp[i] = INT_MAX;
+        if(n <= 1)
+            return n == 0 ? vector<int>{} : vector<int>{0};
+        
+        vector<int> indegree(n, 0);
+        int min_degree = INT_MAX;
+        
+        // create a graph
+        vector<vector<int>> graph(n);
+        for(auto &edge: edges) {
+            graph[edge[0]].emplace_back(edge[1]);
+            graph[edge[1]].emplace_back(edge[0]);
+            ++indegree[edge[0]], ++indegree[edge[1]];
         }
+        
         vector<int> result;
-        for(int i=0; i < tmp.size(); ++i)
-            if(tmp[i] != INT_MAX)
-                result.push_back(i);
-
+        queue<int> queue;
+        unordered_set<int> visited;
+        
+        // add leaf nodes(indegree 1)
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 1) {
+                queue.emplace(i);
+                visited.emplace(i);
+            }
+        }
+        
+        int n_nodes = queue.size();
+        
+        // we return the nodes in the last level
+        while(!queue.empty()) {
+            auto curr = queue.front();
+            queue.pop();
+            --n_nodes;
+            // add node of current level
+            result.emplace_back(curr);
+            
+            // remove the connection count for each neighbor
+            for(auto v: graph[curr]) {
+                if(!visited.count(v)) {
+                    --indegree[v];
+                    // it has become a leaf node, add and mark visited
+                    if(indegree[v] == 1) {
+                        queue.emplace(v);
+                        visited.emplace(v);
+                    }
+                }
+            }
+            
+            // end of level
+            if(n_nodes == 0) {
+                // not the last level
+                if(!queue.empty())
+                    result.clear();
+                n_nodes = queue.size();
+            }
+        }
+        
         return result;
     }
 };
@@ -113,24 +108,24 @@ int main()
     // };
     // int N = 4;
 
-    // vector<Edge> edges = {
-    //      {3,0},{3,1},{3,2},{3,4},{5,4}
-    // };
-    // int N = 6;
+    vector<vector<int>> edges = {
+         {3,0},{3,1},{3,2},{3,4},{5,4}
+    };
+    int N = 6;
 
     // vector<Edge> edges = {        
     // };
     // int N = 1;
 
-    vector<Edge> edges = {
-        {0, 1}        
-    };
-    int N = 2;
+    // vector<vector<int>> edges = {
+    //     {0, 1}        
+    // };
+    // int N = 2;
  
     // build a graph from the given edges
-    Graph graph(edges, N);
+    //Graph graph(edges, N);
     
     Solution sl;
-    for(auto v: sl.findMinHeightTrees(N, graph.adjList))
+    for(auto v: sl.findMinHeightTrees(N, edges))
         cout << v << "\n";
 }
