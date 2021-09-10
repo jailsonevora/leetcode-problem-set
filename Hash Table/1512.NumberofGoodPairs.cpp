7273620f7f016de62b5514dbc0817b3a7f4e53b5
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-10 18:17:18 -0100

    1512. Number of Good Pairs

--- Hash Table/1512.NumberofGoodPairs.cpp
+++ Hash Table/1512.NumberofGoodPairs.cpp
@@ -21,3 +21,15 @@ public:
         return ans;        
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    //string s = "abacbc";
+    vector<int> nums = {1,2,3,1,1,3};
+
+    Solution sl;
+    cout << sl.numIdenticalPairs(nums);
+}
