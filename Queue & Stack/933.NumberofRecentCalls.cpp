7bc8bcbb7b8ba220053ad6e0c9f41834f08115de
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-13 11:17:29 -0100

    933. Number of Recent Calls

--- Queue & Stack/933.NumberofRecentCalls.cpp
+++ Queue & Stack/933.NumberofRecentCalls.cpp
@@ -31,4 +31,8 @@ int main(){
     cout.tie();
 
     RecentCounter sl;
+    sl.ping(1);     // requests = [1], range is [-2999,1], return 1
+    sl.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
+    sl.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
+    sl.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], 
 }
