ab247f5f35af8df258702014615eb41fa77b4284
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-13 09:13:53 -0100

    237. Delete Node in a Linked List

--- Linked List/237.DeleteNodeinaLinkedList.cpp
+++ Linked List/237.DeleteNodeinaLinkedList.cpp
@@ -11,3 +11,16 @@ using namespace std;
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+
+int main(){
+    
+    Solution ll;
+    ll.pushBack(1);
+    ll.pushBack(2);
+    ll.pushBack(3);
+    ll.pushBack(4);
+    ll.pushBack(5);
+
+    cout << ll.middleNode(ll.head);
+}
