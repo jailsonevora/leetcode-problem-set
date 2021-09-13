7e462b306909b7b140de8bb334fa42c78f86fbd3
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-13 12:05:20 -0100

    1700. Number of Students Unable to Eat Lunch

--- Queue & Stack/1700.NumberofStudentsUnabletoEatLunch.cpp
+++ Queue & Stack/1700.NumberofStudentsUnabletoEatLunch.cpp
@@ -7,6 +7,15 @@ using namespace std;
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        
+        students.
     }
 };
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    
+
+}
