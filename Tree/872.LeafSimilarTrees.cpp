a296a106c29004a5e230cc0a2c4c734a94cc7cb3
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-15 15:28:26 -0100

    872. Leaf-Similar Trees

--- Tree/872.LeafSimilarTrees.cpp
+++ Tree/872.LeafSimilarTrees.cpp
@@ -47,9 +47,24 @@ public:
 };
 
 class Solution {
+    void dfs(TreeNode* root, vector<int>& leaves){
+        if(!root)
+            return;
+        
+        if(!root->left || !root->right)
+            leaves.push_back(root->val);
+        
+        dfs(root->left, leaves), dfs(root->right, leaves);
+    }
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        
+
+        vector<int> v1, v2;
+
+        dfs(root1, v1);
+        dfs(root2, v2);
+
+        return v1 == v2;              
     }
 };
 
@@ -58,12 +73,17 @@ int main(){
     cin.tie();
     cout.tie();
 
-    vector<int> preOrder = {3,5,6,2,7,4,1,9,8};
-    vector<int> inOrder = {6,5,7,2,4,3,9,1,8};
+    vector<int> preOrder1 = {3,5,6,2,7,4,1,9,8};
+    vector<int> inOrder1 = {6,5,7,2,4,3,9,1,8};
 
     BuildTreeFromPreOrderArray bl;
-    TreeNode* root1 = bl.buildTree(preOrder, inOrder);
+    TreeNode* root1 = bl.buildTree(preOrder1, inOrder1);
+
+    vector<int> preOrder2 = {3,5,6,7,1,4,2,9,8};
+    vector<int> inOrder2 = {6,5,7,3,4,1,9,2,8};
+
+    TreeNode* root2 = bl.buildTree(preOrder2, inOrder2);
 
     Solution sl;
-    //sl.leafSimilar(root1, root2);
+    cout << sl.leafSimilar(root1, root2);
 }
