ff0bd3b125cebc38bf93f586673f915230df93fc
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-15 14:24:19 -0100

    872. Leaf-Similar Trees

--- Tree/872.LeafSimilarTrees.cpp
+++ Tree/872.LeafSimilarTrees.cpp
@@ -16,3 +16,32 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+ 
+class BuildTreeFromPreOrderArray {
+    TreeNode* buildTree(vector<int>& preorder, int p1, int p2, vector<int>& inorder, int i1, int i2) {
+        if(p1 > p2)
+            return nullptr;
+        
+        TreeNode *root = new TreeNode(preorder[p1]);
+        int i = i1;
+        while (i <= i2) {
+            if (inorder[i] == root->val) break;
+            i ++;
+        }
+
+        int diff = i - i1 - 1;
+        p1++;
+        
+        root->left = buildTree(preorder, p1, p1 + diff, inorder, i1, i1 + diff);
+        root->right = buildTree(preorder, p1 + diff + 1, p2, inorder, i + 1,  i2);
+        
+        return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if(!inorder.size())
+            return nullptr;
+        return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
+    }
+};
