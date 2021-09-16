fa63d6a6ca43cfbca4df537d9f818f1a0b3ed501
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-16 09:41:51 -0100

    653. Two Sum IV - Input is a BST

--- Tree/653.TwoSumIV-InputisaBST.cpp
+++ Tree/653.TwoSumIV-InputisaBST.cpp
@@ -62,13 +62,8 @@ int main(){
     vector<int> inOrder1 = {6,5,7,2,4,3,9,1,8};
 
     BuildTreeFromPreOrderArray bl;
-    TreeNode* root1 = bl.buildTree(preOrder1, inOrder1);
-
-    vector<int> preOrder2 = {3,5,6,7,1,4,2,9,8};
-    vector<int> inOrder2 = {6,5,7,3,4,1,9,2,8};
-
-    TreeNode* root2 = bl.buildTree(preOrder2, inOrder2);
+    TreeNode* root = bl.buildTree(preOrder1, inOrder1);
 
     Solution sl;
-    cout << sl.findTarget(root1, root2);
+    cout << sl.findTarget(root);
 }
