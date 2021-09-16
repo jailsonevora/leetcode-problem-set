6c27f46c70b559bc76982180c6128003b37f68ef
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-16 09:40:49 -0100

    653. Two Sum IV - Input is a BST

--- Tree/653.TwoSumIV-InputisaBST.cpp
+++ Tree/653.TwoSumIV-InputisaBST.cpp
@@ -45,3 +45,24 @@ public:
         return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
     }
 }; 
+
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    vector<int> preOrder1 = {3,5,6,2,7,4,1,9,8};
+    vector<int> inOrder1 = {6,5,7,2,4,3,9,1,8};
+
+    BuildTreeFromPreOrderArray bl;
+    TreeNode* root1 = bl.buildTree(preOrder1, inOrder1);
+
+    vector<int> preOrder2 = {3,5,6,7,1,4,2,9,8};
+    vector<int> inOrder2 = {6,5,7,3,4,1,9,2,8};
+
+    TreeNode* root2 = bl.buildTree(preOrder2, inOrder2);
+
+    Solution sl;
+    cout << sl.leafSimilar(root1, root2);
+}
