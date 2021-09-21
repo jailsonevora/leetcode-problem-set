14c6009f9c5592095a20fb5fd2eb135379ba8062
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-21 19:42:38 -0100

    1365. How Many Numbers Are Smaller Than the Current Number

--- Hash Table/1365.HowManyNumbersAreSmallerThantheCurrentNumber.cpp
+++ Hash Table/1365.HowManyNumbersAreSmallerThantheCurrentNumber.cpp
@@ -7,6 +7,22 @@ using namespace std;
 
 class Solution {
 public:
+    // using sorting
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        
+        vector<int> ans(nums.size(), 0);
+        
+        
+        for(int i = 0; i < nums.size(); ++i)
+            for(int j = 1; j < nums.size(); j++)
+                if(nums[j] < nums[i])
+                    ans[i]++;
+        
+        return ans;
+        
+    }
+
+    // brute force
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         
         vector<int> ans(nums.size(), 0);
