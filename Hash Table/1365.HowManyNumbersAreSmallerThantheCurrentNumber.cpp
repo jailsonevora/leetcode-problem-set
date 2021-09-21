37e11d07efea5b97fff04e2a2adfc36404e3772c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-21 19:41:03 -0100

    1365. How Many Numbers Are Smaller Than the Current Number

--- Hash Table/1365.HowManyNumbersAreSmallerThantheCurrentNumber.cpp
+++ Hash Table/1365.HowManyNumbersAreSmallerThantheCurrentNumber.cpp
@@ -26,10 +26,9 @@ int main(){
     ios_base::sync_with_stdio();
     cin.tie();
     cout.tie();
-
-    //string s = "abacbc";
+    
     vector<int> nums = {1,2,3,1,1,3};
 
     Solution sl;
-    cout << sl.numIdenticalPairs(nums);
+    cout << sl.smallerNumbersThanCurrent(nums);
 }
