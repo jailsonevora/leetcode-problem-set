d5a82337b70a8ba8452f056b986b719e29740c36
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-22 10:10:11 -0100

    1021. Remove Outermost Parentheses

--- Queue & Stack/1021.RemoveOutermostParentheses.cpp
+++ Queue & Stack/1021.RemoveOutermostParentheses.cpp
@@ -3,3 +3,14 @@
 #include<stack>
 
 using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    string s = "(1+(2*3)+((8)/4))+1";
+
+    Solution sl;
+    cout << sl.maxDepth(s);
+}
