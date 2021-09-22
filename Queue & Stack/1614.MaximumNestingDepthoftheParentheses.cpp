d7530d3cfd98e9e3d3eb8cef5ef594670552f9ca
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-22 09:43:51 -0100

    1614. Maximum Nesting Depth of the Parentheses

--- Queue & Stack/1614.MaximumNestingDepthoftheParentheses.cpp
+++ Queue & Stack/1614.MaximumNestingDepthoftheParentheses.cpp
@@ -3,3 +3,17 @@
 #include<stack>
 
 using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    //vector<int> students {1,1,1,0,0,1}, sandwiches {1,0,0,0,1,1};
+    vector<int> students {1,1,1,0,0,1}, sandwiches {1,0,0,0,1,1};
+
+    Solution sl;
+
+    cout << sl.countStudents(students, sandwiches);
+
+}
