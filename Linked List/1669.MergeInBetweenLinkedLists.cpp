009748d991a05e68df7a393f7182537088f8b2d8
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-23 15:32:36 -0100

    1669. Merge In Between Linked Lists

--- Linked List/1669.MergeInBetweenLinkedLists.cpp
+++ Linked List/1669.MergeInBetweenLinkedLists.cpp
@@ -12,6 +12,12 @@ using namespace std;
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        
+    }
+};
 
 int main(){
     
