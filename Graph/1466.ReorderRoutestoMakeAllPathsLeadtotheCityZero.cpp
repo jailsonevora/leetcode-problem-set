6b2151eee7f88b109ca7c26ed27666a0506d426e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-23 10:41:22 -0100

    1466. Reorder Routes to Make All Paths Lead to the City Zero

--- Graph/1466.ReorderRoutestoMakeAllPathsLeadtotheCityZero.cpp
+++ Graph/1466.ReorderRoutestoMakeAllPathsLeadtotheCityZero.cpp
@@ -33,3 +33,37 @@ public:
         }
     }
 };
+
+
+
+int main()
+{
+    // vector of graph edges as per the above diagram
+    // vector<Edge> edges = {
+    //     {1, 0}
+    // };
+    // int N = 2;
+
+    // vector<Edge> edges = {
+    //      {1,0},{2,0},{3,1},{3,2}
+    // };
+    // int N = 4;
+
+    // vector<Edge> edges = {
+        
+    // };
+    // int N = 1;
+ 
+    // build a graph from the given edges
+    //Graph graph(edges, N);
+
+    vector<vector<int>> adjList = {
+        {{1,2},{3},{3},{}}
+    };
+    int N = 4;
+    
+    Solution sl;
+    for(auto v: sl.allPathsSourceTarget(adjList))
+        for(auto u: v)
+            cout << u << "\n";
+}
