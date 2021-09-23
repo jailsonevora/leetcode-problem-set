92293a07745f05fcd7f1ba90bfa7fef8d0a77656
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-23 09:27:54 -0100

    547. Number of Provinces

--- Graph/547.NumberofProvinces.cpp
+++ Graph/547.NumberofProvinces.cpp
@@ -34,6 +34,44 @@ public:
     }
 };
 
+class SolutionDSU {
+        
+    int _find(int v, vector<int>& parent) {
+        if (v == parent[v])
+            return v;
+        return parent[v] = _find(parent[v]);
+    }
+    
+    void _union(int from, int to, vector<int>& parent) {
+        from = _find(from);
+        to = _find(to);
+        if (from != to)
+            parent[to] = from;
+    }
+public:
+    
+    int findCircleNum(vector<vector<int>>& M) {
+        
+        int i, j, province = 0, n = M.size();
+        
+        // initialize leads for every kid as themselves
+        vector<int> parent(n, 0);
+        for (int i = 0; i < n; i++) 
+            parent[i] = i;  
+          
+        for(i = 0; i < n; i++)
+            for(j = i + 1; j < n; j++)
+                if(M[i][j])
+                    _union(i, j);
+        
+        for(i = 0; i < n; i++)
+            if(i == parent[i])
+                province++;
+        
+        return province;
+    }
+};
+
 class Solution {
     void DFS(vector<vector<int>> &grid, int v, vector<int>& visited){
                
