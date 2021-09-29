7482ddf07cd03e2c08d86476a99bcbf59f556731
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-29 11:54:48 -0100

    1472. Design Browser History

--- Linked List/1472.DesignBrowserHistory.cpp
+++ Linked List/1472.DesignBrowserHistory.cpp
@@ -43,19 +43,7 @@ int main(){
     
     ListNode* l1 = nullptr, *l2 = nullptr;
 
-    Solution ll;
-    ll.pushBack(0, l1);
-    ll.pushBack(1, l1);
-    ll.pushBack(2, l1);
-    ll.pushBack(3, l1);
-    ll.pushBack(4, l1);
-    ll.pushBack(5, l1);
-
-    ll.pushBack(1000000, l2);
-    ll.pushBack(1000001, l2);
-    ll.pushBack(1000002, l2);
-
-    ll.mergeInBetween(l1, 3, 4, l2);
+    BrowserHistory ll;
 
     //[0,3,2,1,4,5] 3, 4, [1000000,1000001,1000002]
 }
