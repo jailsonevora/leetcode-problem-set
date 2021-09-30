089f2dac84221c9e20771577ff4f0783ed8a3a1a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-30 12:39:43 -0100

    1721. Swapping Nodes in a Linked List

--- Linked List/1721.SwappingNodesinaLinkedList.cpp
+++ Linked List/1721.SwappingNodesinaLinkedList.cpp
@@ -11,3 +11,25 @@ using namespace std;
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+
+int main(){
+    
+    ListNode* l1 = nullptr, *l2 = nullptr;
+
+    Solution ll;
+    ll.pushBack(0, l1);
+    ll.pushBack(1, l1);
+    ll.pushBack(2, l1);
+    ll.pushBack(3, l1);
+    ll.pushBack(4, l1);
+    ll.pushBack(5, l1);
+
+    ll.pushBack(1000000, l2);
+    ll.pushBack(1000001, l2);
+    ll.pushBack(1000002, l2);
+
+    ll.mergeInBetween(l1, 3, 4, l2);
+
+    //[0,3,2,1,4,5] 3, 4, [1000000,1000001,1000002]
+}
