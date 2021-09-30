0aa7ccdaf31a65da1d91e6e090c20bd6e041dfd1
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-30 11:16:07 -0100

    1472. Design Browser History

--- Linked List/1472.DesignBrowserHistory.cpp
+++ Linked List/1472.DesignBrowserHistory.cpp
@@ -9,77 +9,40 @@ class BrowserHistory {
     struct ListNode {
         string val;
         ListNode *next;
+        ListNode *prev;
         ListNode() : val(0), next(nullptr) {}
         ListNode(string x) : val(x), next(nullptr) {}
         ListNode(string x, ListNode *next) : val(x), next(next) {}
     };
 
-    ListNode* head = nullptr, *curr = head, *tail = nullptr;
-    int size = 0, currSize = 0;
-
-    void pushBack(string data, ListNode*& head)
-    {
-        ListNode* temp = new ListNode(data);
-        ListNode* curr = head;
-
-        if (!head){  
-            head = temp;
-            tail = head; 
-            return;
-        }     
-        while(curr && curr->next) 
-            curr = curr->next;
-        curr->next = temp;
-
-        tail = curr->next;
-    }
+    ListNode* curr = nullptr;
 
 public:
 
     BrowserHistory(string homepage) {
-        pushBack(homepage,head);
-        curr = tail;
-        size++;
-        currSize = size;        
+        curr = new ListNode(homepage);       
     }
     
     void visit(string url) {
-        pushBack(url,head);
-        curr = tail;
-        size++;
-        currSize = size;
+        curr->next = new ListNode(url);
+        ListNode* prev = curr;
+        curr = curr->next;
+        curr->prev = prev;
     }
     
     string back(int steps) {
 
-        ListNode* temp = head;
-        int back = abs(currSize - steps);
-        int b = 0;
+        while (steps-- && curr->prev)
+            curr = curr->prev;
+        return curr->val;
 
-        while (--back && temp->next){
-            temp = temp->next;
-            b++;
-        }
-
-        curr = temp;
-        currSize -= b;
-        return temp->val;      
     }
     
     string forward(int steps) {
 
-        ListNode* temp = curr;
-        int b = 0;
-
-        while (steps-- && temp->next){
-            temp = temp->next;
-            b ++;
-        }
-
-        
-        curr = temp;
-        currSize += b;
-        return temp->val;
+        while (steps-- && curr->next)
+            curr = curr->next;
+        return curr->val;
     }
 };
 
