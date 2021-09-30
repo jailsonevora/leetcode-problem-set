5f078df7f14e6e6169ddc732f42a7300f1729d7e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-09-30 19:14:38 -0100

    1721. Swapping Nodes in a Linked List

--- Linked List/1721.SwappingNodesinaLinkedList.cpp
+++ Linked List/1721.SwappingNodesinaLinkedList.cpp
@@ -39,6 +39,20 @@ public:
         return head;
     }
 
+    //one pass
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *n1 = nullptr, *n2 = nullptr;
+        for (auto p = head; p != nullptr; p = p->next) {
+            n2 = n2 == nullptr ? nullptr : n2->next;
+            if (--k == 0) {
+                n1 = p;
+                n2 = head;
+            }
+        }
+        swap(n1->val, n2->val);
+        return head;
+    }
+
     // 2 pass
     ListNode* swapNodes(ListNode* head, int k) {
         ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
