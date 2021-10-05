25975ec064974d6e82fa01cb5653874872409359
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-05 17:13:55 -0100

    make rank vector

--- Graph/1319.NumberofOperationstoMakeNetworkConnected.cpp
+++ Graph/1319.NumberofOperationstoMakeNetworkConnected.cpp
@@ -104,6 +104,8 @@ public:
 
         // initialize leads
         vector<int> parent(n, 0);
+        vector<int> rank(n, 0);
+        
         for (int i = 0; i < n; i++)
             parent[i] = i;
 
