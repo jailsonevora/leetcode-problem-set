53bfc98f9be6d0f4993a744711f302d2d7f2d743
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-06 14:45:43 -0100

    993. Cousins in Binary Tree

--- Tree/993.CousinsinBinaryTree.cpp
+++ Tree/993.CousinsinBinaryTree.cpp
@@ -64,19 +64,19 @@ int main(){
     // // 4                
     // vector<int> inOrder1 = {4,2,1,3};
 
-    // int x = 5, y = 4;
-    // vector<int> preOrder1 = {1,2,4,3,5};
-    // //         1
-    // //     2       3
-    // //        4         5         
-    // vector<int> inOrder1 = {4,2,1,3,5};
-
-    int x = 2, y = 3;
-    vector<int> preOrder1 = {1,2,4,3};
+    int x = 5, y = 4;
+    vector<int> preOrder1 = {1,2,4,3,5};
     //         1
     //     2       3
-    //        4               
-    vector<int> inOrder1 = {2,4,1,3};
+    //        4         5         
+    vector<int> inOrder1 = {4,2,1,3,5};
+
+    // int x = 2, y = 3;
+    // vector<int> preOrder1 = {1,2,4,3};
+    // //         1
+    // //     2       3
+    // //        4               
+    // vector<int> inOrder1 = {2,4,1,3};
 
     BuildTreeFromPreOrderArray bl;
     TreeNode* root = bl.buildTree(preOrder1, inOrder1);
