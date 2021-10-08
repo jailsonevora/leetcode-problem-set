b3c0cfe17f422700949fa239ea486e1017a04edc
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-08 13:13:34 -0100

    399. Evaluate Division

--- Graph/399.EvaluateDivision.cpp
+++ Graph/399.EvaluateDivision.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        
+    }
+};
+
 int main(){
 
     ios_base::sync_with_stdio();
