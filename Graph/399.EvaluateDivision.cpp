1d47d4dee5e20fbea3597361675bf5be5dd503ce
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-08 14:25:49 -0100

    399

--- Graph/399.EvaluateDivision.cpp
+++ Graph/399.EvaluateDivision.cpp
@@ -20,26 +20,34 @@ class Solution {
 
     }
 
+    // deph first search
+    void _dfs(int u, int v, unordered_map<string, vector<pair<string,double>>> graph, vector<int>& visited){
 
+        visited[u] = 1;
+        for(auto& v: graph[u])
+            if(!visited[v])
+                _dfs(v, graph, visited);        
+    }
 
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         
 
         // adjacent list
-        vector<string, pair<string,double>> graph[10000];
+        unordered_map<string, vector<pair<string,double>>> graph;
 
         // undirected graph
-        for(int i=0;i<equations.size();i++)
-        {
-            string from=equations[i][0];
-            string to=equations[i][1];
-            graph[from].insert({to,values[i]});
-            graph[to].insert({from,1.0/values[i]});
-        }
+        for(int i=0; i<equations.size(); i++)
+            graph[equations[i][0]].push_back({equations[i][1],values[i]}),
+            graph[equations[i][1]].push_back({equations[i][0],1.0/values[i]});
 
+        vector<double> ans(queries.size());
+        vector<string> visited;
 
-
+        for (auto q: queries)
+            if(!visited[q[0]])
+                _dfs(q[0], q[1], graph, visited, ans);       
+        return ans;
     }
 };
 
