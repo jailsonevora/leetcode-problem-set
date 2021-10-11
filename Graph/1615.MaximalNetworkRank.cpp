bb2e8f00e84c9a31d7c227a4b8a4446e36faca08
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-11 13:26:16 -0100

    1615. Maximal Network Rank

--- Graph/1615.MaximalNetworkRank.cpp
+++ Graph/1615.MaximalNetworkRank.cpp
@@ -37,8 +37,18 @@ int main(){
     // };
     // int n = 5;
 
+    // adjacency matrix
+    // vector<vector<int>> roads = { 
+    //     {0,1},
+    //     {1,2},
+    //     {2,3},
+    //     {2,4},
+    //     {5,6},
+    //     {5,7}
+    // };
+    // int n = 8;
+
     Solution sl;
 
     cout << sl.maximalNetworkRank(n, roads) << "\n";
-
 }
