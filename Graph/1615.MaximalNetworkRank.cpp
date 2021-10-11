8e11fbb310846e6533c67ea2cb79fc6e9a813e4f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-11 16:16:56 -0100

    1615. Maximal Network Rank

--- Graph/1615.MaximalNetworkRank.cpp
+++ Graph/1615.MaximalNetworkRank.cpp
@@ -17,8 +17,16 @@ public:
 
         int maxRank = INT_MIN;
 
-        for(auto edge: roads)
-            maxRank = std::max((degree[edge[0]] + degree[edge[1]] - 1), maxRank);
+        for(int from = 0; from < n && from < roads.size(); from++)
+            for(int to = from + 1; to < n; to++)
+                if(roads[from][to] == from && roads[to][from] == to)
+                    maxRank = std::max((degree[from] + degree[to] - 1), maxRank);
+                else
+                    maxRank = std::max((degree[from] + degree[to]), maxRank);
+
+
+        // for(auto edge: roads)
+        //     maxRank = std::max((degree[edge[0]] + degree[edge[1]] - 1), maxRank);
 
         return maxRank;
     }
@@ -37,26 +45,26 @@ int main()
     // int n = 4;
 
     // // adjacency matrix
-    vector<vector<int>> roads = { 
-        {0,1},
-        {0,3},
-        {1,2},
-        {1,3},
-        {2,3},
-        {2,4}
-    };
-    int n = 5;
-
-    // adjacency matrix
     // vector<vector<int>> roads = { 
     //     {0,1},
+    //     {0,3},
     //     {1,2},
+    //     {1,3},
     //     {2,3},
-    //     {2,4},
-    //     {5,6},
-    //     {5,7}
+    //     {2,4}
     // };
-    // int n = 8;
+    // int n = 5;
+
+    // adjacency matrix
+    vector<vector<int>> roads = { 
+        {0,1},
+        {1,2},
+        {2,3},
+        {2,4},
+        {5,6},
+        {5,7}
+    };
+    int n = 8;
 
     Solution sl;
 
