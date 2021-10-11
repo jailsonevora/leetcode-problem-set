f02d3a41dbc86c3903e9c7a2cb92d84ce1089ca8
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-11 16:33:32 -0100

    1615. Maximal Network Rank

--- Graph/1615.MaximalNetworkRank.cpp
+++ Graph/1615.MaximalNetworkRank.cpp
@@ -5,25 +5,33 @@
 using namespace std;
 
 class Solution {
-
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         
-        vector<int> degree(n + 1, 0);
+        vector<int> degree(n, 0);
+        vector<vector<int>> edgepair(n, vector<int>(n,0));
 
         for(auto edge: roads)
             degree[edge[0]]++,
-                degree[edge[1]]++;
+                degree[edge[1]]++,
+                    edgepair[edge[0]][edge[1]] = 1,
+                        edgepair[edge[1]][edge[0]] = 1;
+                         
 
         int maxRank = INT_MIN;
 
-        for(int from = 0; from < n && from < roads.size(); from++)
+        // faster
+        for(int from = 0; from < n; from++)
             for(int to = from + 1; to < n; to++)
-                if(roads[from][to] == from && roads[to][from] == to)
-                    maxRank = std::max((degree[from] + degree[to] - 1), maxRank);
-                else
-                    maxRank = std::max((degree[from] + degree[to]), maxRank);
+                    maxRank = std::max(degree[from] + degree[to] - edgepair[from][to], maxRank);
 
+        // the same as above
+        // for(int from = 0; from < n; from++)
+        //     for(int to = from + 1; to < n; to++)
+        //         if(edgepair[from][to] == 1) 
+        //             maxRank = std::max((degree[from] + degree[to] - 1), maxRank);
+        //         else
+        //             maxRank = std::max((degree[from] + degree[to]), maxRank);
 
         // for(auto edge: roads)
         //     maxRank = std::max((degree[edge[0]] + degree[edge[1]] - 1), maxRank);
