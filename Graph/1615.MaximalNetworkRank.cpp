1d64eb03022490dafe149068cc733c2f66b01eb3
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-11 13:18:47 -0100

    1615. Maximal Network Rank

--- Graph/1615.MaximalNetworkRank.cpp
+++ Graph/1615.MaximalNetworkRank.cpp
@@ -5,3 +5,35 @@
 #include <unordered_set>
 
 using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    // adjacency matrix
+    vector<vector<string>> equations = { 
+        {"a","b"},
+        {"b","c"}
+    };
+
+    // represent the equation Ai / Bi = values[i]
+    vector<double> values = {
+        2.0, 3.0
+    };
+
+    vector<vector<string>> queries = {
+        {"a","c"},
+        {"b","a"},
+        {"a","e"},
+        {"a","a"},
+        {"x","x"}
+    };
+
+    Solution sl;
+
+    for(auto q: sl.calcEquation(equations, values, queries))
+        cout << q << "\n";
+
+}
