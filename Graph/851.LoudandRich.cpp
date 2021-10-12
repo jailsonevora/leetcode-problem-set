5a9a0a1e01b5e15985e44173843298b11502bdf6
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-12 14:58:06 -0100

    851. Loud and Rich

--- Graph/851.LoudandRich.cpp
+++ Graph/851.LoudandRich.cpp
@@ -3,3 +3,44 @@
 #include <algorithm>
 
 using namespace std;
+
+
+
+int main()
+{
+
+    // adjacency matrix
+    // vector<vector<int>> roads = { 
+    //     {0,1},
+    //     {0,3},
+    //     {1,2},
+    //     {1,3}
+    // };
+    // int n = 4;
+
+    // // adjacency matrix
+    // vector<vector<int>> roads = { 
+    //     {0,1},
+    //     {0,3},
+    //     {1,2},
+    //     {1,3},
+    //     {2,3},
+    //     {2,4}
+    // };
+    // int n = 5;
+
+    // adjacency matrix
+    vector<vector<int>> roads = { 
+        {0,1},
+        {1,2},
+        {2,3},
+        {2,4},
+        {5,6},
+        {5,7}
+    };
+    int n = 8;
+
+    Solution sl;
+
+    cout << sl.maximalNetworkRank(n, roads);
+}
