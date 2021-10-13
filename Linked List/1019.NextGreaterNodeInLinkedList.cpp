01a0172c49ce9b485039685e7ba711cf57a90091
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-13 14:05:20 -0100

    1019. Next Greater Node In Linked List

--- Linked List/1019.NextGreaterNodeInLinkedList.cpp
+++ Linked List/1019.NextGreaterNodeInLinkedList.cpp
@@ -12,16 +12,35 @@ using namespace std;
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        
+    }
+    void pushBack(int data, ListNode*& head)
+    {
+        ListNode* temp = new ListNode(data);
+        ListNode* curr = head;
+
+        if (!head){  
+            head = temp; 
+            return;
+        }     
+        while(curr && curr->next) 
+            curr = curr->next;
+        curr->next = temp;
+    }
+};
+
 int main(){
     
     ListNode* l1 = nullptr;
 
     Solution ll;
-    ll.pushBack(1, l1);
     ll.pushBack(2, l1);
     ll.pushBack(3, l1);
     ll.pushBack(4, l1);
     ll.pushBack(5, l1);
 
-    ll.swapNodes(l1,2);
+    ll.nextLargerNodes(l1);
 }
