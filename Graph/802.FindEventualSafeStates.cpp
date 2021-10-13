132db1b0a4719e7f762953852526d4c0ecf1b1a2
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-13 10:39:54 -0100

    802. Find Eventual Safe States

--- Graph/802.FindEventualSafeStates.cpp
+++ Graph/802.FindEventualSafeStates.cpp
@@ -5,3 +5,39 @@
 
 using namespace std;
 
+
+
+
+int main()
+{
+
+    // adjacency matrix
+    // vector<vector<int>> richer = { 
+    // };
+
+    // // vector
+    // vector<int> quiet = {
+    //     0
+    // };
+
+    // adjacency matrix
+    vector<vector<int>> richer = { 
+        {1,0},
+        {2,1},
+        {3,1},
+        {3,7},
+        {4,3},
+        {5,3},
+        {6,3}
+    };
+
+    // vector
+    vector<int> quiet = {
+        3,2,5,4,6,1,7,0
+    };
+
+    Solution sl;
+
+    for(auto it: sl.loudAndRich(richer, quiet))
+        std::cout << it << ",";
+}
