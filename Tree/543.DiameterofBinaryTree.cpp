f262a69dc057c779d4a71cfdd3f28b10c49b4d69
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 10:22:23 -0100

    543. Diameter of Binary Tree

--- Tree/543.DiameterofBinaryTree.cpp
+++ Tree/543.DiameterofBinaryTree.cpp
@@ -45,3 +45,37 @@ public:
     }
 };
 
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    int x = 4, y = 3;
+    vector<int> preOrder1 = {1,2,4,3};
+    //         1
+    //     2       3
+    // 4                
+    vector<int> inOrder1 = {4,2,1,3};
+
+    // int x = 5, y = 4;
+    // vector<int> preOrder1 = {1,2,4,3,5};
+    // //         1
+    // //     2       3
+    // //        4         5         
+    // vector<int> inOrder1 = {2,4,1,3,5};
+
+    // int x = 2, y = 3;
+    // vector<int> preOrder1 = {1,2,4,3};
+    // //         1
+    // //     2       3
+    // //        4               
+    // vector<int> inOrder1 = {2,4,1,3};
+
+    BuildTreeFromPreOrderArray bl;
+    TreeNode* root = bl.buildTree(preOrder1, inOrder1);
+
+    Solution sl;
+    cout << sl.isCousins(root, x, y);
+}
+
