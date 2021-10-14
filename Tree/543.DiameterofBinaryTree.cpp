c3e80efab79ec94450ee1608171e700e61edaabb
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 13:25:52 -0100

    543. Diameter of Binary Tree

--- Tree/543.DiameterofBinaryTree.cpp
+++ Tree/543.DiameterofBinaryTree.cpp
@@ -23,18 +23,10 @@ int main(){
     vector<int> inOrder1 = {4,2,5,1,3};
 
     // int x = 5, y = 4;
-    // vector<int> preOrder1 = {1,2,4,3,5};
+    // vector<int> preOrder1 = {1,2};
     // //         1
-    // //     2       3
-    // //        4         5         
-    // vector<int> inOrder1 = {2,4,1,3,5};
-
-    // int x = 2, y = 3;
-    // vector<int> preOrder1 = {1,2,4,3};
-    // //         1
-    // //     2       3
-    // //        4               
-    // vector<int> inOrder1 = {2,4,1,3};
+    // //     2             
+    // vector<int> inOrder1 = {2,1};
 
     BuildTreeFromPreOrderArray bl;
     TreeNode* root = bl.buildTree(preOrder1,inOrder1);
