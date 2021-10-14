bc70603a7c434e7cceb7f2e1ebf14a2c16d7526e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 15:06:33 -0100

    refactoring with OOP paradigm

--- Tree/11.PopulatingNextRightPointersInEachNodeII.cpp
+++ Tree/11.PopulatingNextRightPointersInEachNodeII.cpp
@@ -1,27 +1,10 @@
 #include<iostream>
 #include<queue>
 #include<stack>
-#include<unordered_set>
+#include "Util\Include\BuildTreeFromPreOrderArray.h"
 
 using namespace std;
 
-/*
-// Definition for a Node.
-*/
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
 
 class Solution {
 public:
