86d51bf06b361e8881c81348ced113c55e97ff11
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 14:53:19 -0100

    refactoring with OOP

--- Tree/5.MaximumDepthofBinaryTree.cpp
+++ Tree/5.MaximumDepthofBinaryTree.cpp
@@ -2,20 +2,10 @@
 #include<queue>
 #include<stack>
 #include<unordered_set>
+#include "Util\Include\BuildTreeFromPreOrderArray.h"
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- **/ 
- struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
 
 class Solution {
     int maxDepth(TreeNode* root) {
