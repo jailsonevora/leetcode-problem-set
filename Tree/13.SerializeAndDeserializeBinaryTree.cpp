9668f102e3b530b78ec8e2931cbfbb097f4c706c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 15:10:28 -0100

    refactoring with OOP paradigm

--- Tree/13.SerializeAndDeserializeBinaryTree.cpp
+++ Tree/13.SerializeAndDeserializeBinaryTree.cpp
@@ -1,18 +1,9 @@
 #include<iostream>
 #include<queue>
 #include<stack>
-#include<unordered_set>
+#include "Util\Include\BuildTreeFromPreOrderArray.h"
 
 using namespace std;
-
-/**
- * Definition for a binary tree node.*/
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 class Codec {
 public:
