67acd01b01c33d881105ea45607660925e36b7fd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 20:41:14 -0100

    1334

---
+++ Graph/1334.FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+    //using BFS + ADjList + PriorityQueue as MinHeap
+    void dijkstra(vector<pair<int, int>> graph[], int u, int n, int distanceThreshold, vector<pair<int,int>>& ans){
+        
+        // use priority queue as min heap
+        priority_queue<
+            pair<int, int>, 
+            vector<pair<int, int>>, 
+            greater<pair<int, int>>
+        > pq;
+        
+        // distance vector
+        vector<int> distance(n+1, INT_MAX);
+
+        pq.push(make_pair(0, u));
+        distance[u] = 0;
+
+        // normal BFS traversal
+        while(!pq.empty()){
+
+            int u = pq.top().second;
+            pq.pop();
+
+            // traverse for the neighbors of u
+            for(auto it : graph[u]){
+
+                int v = it.first;
+                int w = it.second;
+
+                if(distance[v] > distance[u] + w)
+                    distance[v] = distance[u] + w,
+                        pq.push(make_pair(distance[v], v));
+            }
+        }
+        
+        cout<<"Vertex   Distance from src\n";
+        for(int i=0; i<n; i++)
+            cout<<i<<"\t"<<distance[i]<<endl;
+
+        int count = 0;
+        for(int node=0; node < n; ++node)
+            if(node != u && distance[node] <= distanceThreshold)
+                count++;    
+        ans.push_back({count,u});
+    }
+
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        
+        // adjacencylist graph
+        vector<pair<int,int>> graph[n];
+        vector<pair<int,int>> ans;
+
+        for(auto edge: edges)
+        {
+            int u = edge[0], v = edge[1], w = edge[2];   
+            graph[u].push_back({v,w});
+            graph[v].push_back({u,w});
+        }
+
+        // for each vertice
+        for(int u=0; u < n; ++u)
+            dijkstra(graph, u, n, distanceThreshold, ans);
+    
+        sort(ans.begin(),ans.end(),[](const pair<int,int> p1 ,const pair<int,int> p2){
+            if(p1.first != p2.first)
+                return p1.first < p2.first;
+            return p1.second > p2.second;
+        });
+        return ans[0].second;
+    }
+};
+
+int main()
+{
+
+    // adjacency edges
+    vector<vector<int>> edges = { 
+        {0,1,3},
+        {1,2,1},
+        {1,3,4},
+        {2,3,1}
+    };
+    int n = 4, distanceThreshold = 4;
+
+    Solution sl;
+
+    std::cout << sl.findTheCity(n, edges, distanceThreshold) << ",";
+}
