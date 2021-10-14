c124cc02d1f1b89df5fc05884d5c6762f7f014ee
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 14:50:38 -0100

    refactoring with OOP

--- Tree/993.CousinsinBinaryTree.cpp
+++ Tree/993.CousinsinBinaryTree.cpp
@@ -2,20 +2,10 @@
 #include<queue>
 #include<stack>
 #include<unordered_set>
+#include "Util\Include\BuildTreeFromPreOrderArray.h"
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- **/ 
- struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
 
  class BuildTreeFromPreOrderArray {
     TreeNode* buildTree(vector<int>& preorder, int p1, int p2, vector<int>& inorder, int i1, int i2) {
@@ -45,17 +35,6 @@ public:
     }
 };
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 
     void _dfs(TreeNode* root, int x, int depth, int parent, int& depthN, int& parentN){
