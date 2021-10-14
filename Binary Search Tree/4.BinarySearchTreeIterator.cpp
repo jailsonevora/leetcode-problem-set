c01f3439205bd520727ebcf8639bc30b70ee129f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 15:36:48 -0100

    refactoring with OOP

--- Binary Search Tree/4.BinarySearchTreeIterator.cpp
+++ Binary Search Tree/4.BinarySearchTreeIterator.cpp
@@ -1,21 +1,9 @@
 #include<iostream>
 #include<queue>
 #include<stack>
-#include<unordered_set>
+#include "Util\Include\TreeNode.h"
 
 using namespace std;
-
-/**
- * Definition for a binary tree node.
- **/ 
- struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
 
  class BSTIterator {
 
--- Binary Search Tree/7.BalancedBinaryTree.cpp
+++ Binary Search Tree/7.BalancedBinaryTree.cpp
@@ -1,20 +1,9 @@
 #include<iostream>
 #include<queue>
 #include<map>
+#include "Util\Include\TreeNode.h"
 
 using namespace std;
-
-/**
- * Definition for a binary tree node.
- **/ 
- struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
 
 class Solution {
     bool ans = true;
