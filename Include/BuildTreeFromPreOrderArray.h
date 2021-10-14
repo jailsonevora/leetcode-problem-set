9c58cfda2e2a9f09cabb8731c53baaa71baf387c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-14 10:43:22 -0100

    oop

--- Include/BuildTreeFromPreOrderArray.h
+++ Include/BuildTreeFromPreOrderArray.h
@@ -1,11 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ **/ 
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
 
 class BuildTreeFromPreOrderArray {
 
     private:
+    TreeNode* buildTree(vector<int>& preorder, int p1, int p2, vector<int>& inorder, int i1, int i2);
 
     protected:
 
     public:
-
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);
 
 };
