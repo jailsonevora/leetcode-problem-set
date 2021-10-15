0ca6f61f5e5d1da76fb5309428a090f943e8f969
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-15 14:13:44 -0100

    817. Linked List Components

--- Linked List/817.LinkedListComponents.cpp
+++ Linked List/817.LinkedListComponents.cpp
@@ -4,3 +4,20 @@
 #include "Util\Include\LinkedListNode.h"
 
 using namespace std;
+
+
+int main(){
+    
+    ListNode* l1 = nullptr;
+
+    Solution ll;
+    ll.pushBack(1, l1);
+    ll.pushBack(2, l1);
+    ll.pushBack(3, l1);
+    ll.pushBack(4, l1);
+    ll.pushBack(5, l1);
+
+    ll.swapNodes(l1,2);
+
+    //[0,3,2,1,4,5] 3, 4, [1000000,1000001,1000002]
+}
