1a6b7849ac39ff802830cf4a10fe573c7b0fd70d
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-16 13:21:40 -0100

    refactoring

--- Graph/947.MostStonesRemovedwithSameRoworColumn.cpp
+++ Graph/947.MostStonesRemovedwithSameRoworColumn.cpp
@@ -3,36 +3,9 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include "Util\Include\Graph.h"
 
 using namespace std;
-
-// Data structure to store a graph edge
-struct Edge {
-    int src, dest;
-};
-
-// A class to represent a graph object
-class Graph {
-public:
-    // a vector of vectors to represent an adjacency matrix
-    vector<vector<int>> adjMatrix;
- 
-    // Graph Constructor
-    Graph(vector<Edge> const &edges, int N)
-    {
-        // resize the vector to hold `N` elements of type `vector<int>`
-        adjMatrix.resize(N);
- 
-        // add edges to the undirected graph
-        if(!edges.empty()){
-            for (auto &edge: edges)
-            {
-                adjMatrix[edge.src].push_back(edge.dest);
-                //adjList[edge.dest].push_back(edge.src);
-            }
-        }
-    }
-};
 
 class Solution {
     int _find(int v, vector<int>& parent) {
@@ -89,13 +62,13 @@ int main()
     // build a graph from the given edges
     //Graph graph(edges, N);
 
-    // vector<vector<int>> adjList = { 
+    // vector<vector<int>> edge = { 
     //     {1,2},
     //     {1,3},
     //     {2,3}
     // };
 
-    vector<vector<int>> adjList = { 
+    vector<vector<int>> edge = { 
         {0,0},
         {0,1},
         {1,0},
@@ -105,5 +78,5 @@ int main()
     };
     
     Solution sl;
-    cout << sl.removeStones(adjList) << "\n";
+    cout << sl.removeStones(edge) << "\n";
 }
--- Graph/959.RegionsCutBySlashes.cpp
+++ Graph/959.RegionsCutBySlashes.cpp
@@ -3,36 +3,9 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include "Util\Include\Graph.h"
 
 using namespace std;
-
-// Data structure to store a graph edge
-struct Edge {
-    int src, dest;
-};
-
-// A class to represent a graph object
-class Graph {
-public:
-    // a vector of vectors to represent an adjacency matrix
-    vector<vector<int>> adjMatrix;
- 
-    // Graph Constructor
-    Graph(vector<Edge> const &edges, int N)
-    {
-        // resize the vector to hold `N` elements of type `vector<int>`
-        adjMatrix.resize(N);
- 
-        // add edges to the undirected graph
-        if(!edges.empty()){
-            for (auto &edge: edges)
-            {
-                adjMatrix[edge.src].push_back(edge.dest);
-                //adjList[edge.dest].push_back(edge.src);
-            }
-        }
-    }
-};
 
 // Union find without path compression
 class Solution {
@@ -106,10 +79,10 @@ int main()
     // build a graph from the given edges
     //Graph graph(edges, N);
 
-    vector<string> adjList = { 
+    vector<string> edge = { 
         {" /"},{"/ "}
     };
     
     Solution sl;
-    std::cout << sl.regionsBySlashes(adjList);
+    std::cout << sl.regionsBySlashes(edge);
 }
