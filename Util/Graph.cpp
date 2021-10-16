533d6eba956640b568b4392324360cf49be50f00
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-16 13:06:47 -0100

    refactoring

---
+++ Util/Graph.cpp
@@ -0,0 +1,25 @@
+#include "Util\Include\Edge.h"
+#include <vector>
+
+// A class to represent a graph object
+class Graph {
+public:
+    // a vector of vectors to represent an adjacency matrix
+    vector<vector<int>> adjMatrix;
+ 
+    // Graph Constructor
+    Graph(vector<Edge> const &edges, int N)
+    {
+        // resize the vector to hold `N` elements of type `vector<int>`
+        adjMatrix.resize(N);
+ 
+        // add edges to the undirected graph
+        if(!edges.empty()){
+            for (auto &edge: edges)
+            {
+                adjMatrix[edge.src].push_back(edge.dest);
+                adjMatrix[edge.dest].push_back(edge.src);
+            }
+        }
+    }
+};
