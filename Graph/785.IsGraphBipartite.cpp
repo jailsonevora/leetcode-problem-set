20a10a0c1fe6d645a89d7ff5772ba2430c323920
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-18 15:22:46 -0100

    785. Is Graph Bipartite?

--- Graph/785.IsGraphBipartite.cpp
+++ Graph/785.IsGraphBipartite.cpp
@@ -10,23 +10,23 @@ using namespace std;
 class Solution {
     // find path compression
     int _find(int v, vector<int>& parent){
+        
         if(v == parent[v])
             return v;
         return parent[v] = _find(parent[v], parent); // path compression
     }
     // union by rank
     void _union(int from, int to, vector<int>& parent, vector<int>& rank){
-        //if(from != to){
-            if(rank[to] < rank[from])
-                parent[to] = from;
-            else if(rank[from] < rank[to])
-                parent[from] = to;
-            else
-            {
-                parent[to] = from;
-                rank[from]++;
-            }
-        //}
+        
+        if(rank[to] < rank[from])
+            parent[to] = from;
+        else if(rank[from] < rank[to])
+            parent[from] = to;
+        else
+        {
+            parent[to] = from;
+            rank[from]++;
+        }
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
