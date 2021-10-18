8e683ea6a610c61cb9b1d81fde8fa79efdc85778
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-18 13:26:53 -0100

    785. Is Graph Bipartite?

--- Graph/785.IsGraphBipartite.cpp
+++ Graph/785.IsGraphBipartite.cpp
@@ -28,3 +28,18 @@ void _union(int from, int to, vector<int>& parent, vector<int>& rank){
         rank[from]++;
     }
 }
+
+int main()
+{
+    // adjacency edges
+    vector<vector<int>> edges = { 
+        {0,1,3},
+        {1,2,1},
+        {1,3,4},
+        {2,3,1}
+    };
+    int n = 4, distanceThreshold = 4;
+
+    Solution sl;
+    std::cout << sl.findTheCity(n, edges, distanceThreshold);
+}
