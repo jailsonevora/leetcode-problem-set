89497093093cca2565debcf5e542ab835e4f9722
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-18 16:51:20 -0100

    743. Network Delay Time

--- Graph/743.NetworkDelayTime.cpp
+++ Graph/743.NetworkDelayTime.cpp
@@ -7,3 +7,34 @@
 
 using namespace std;
 
+
+int main()
+{
+    // vector of graph edges as per the above diagram
+    // vector<Edge> edges = {
+    //     {1, 0}
+    // };
+    // int N = 2;
+
+    // vector<Edge> edges = {
+    //      {1,0},{2,0},{3,1},{3,2}
+    // };
+    // int N = 4;
+
+    // vector<Edge> edges = {
+        
+    // };
+    // int N = 1;
+ 
+    // build a graph from the given edges
+    //Graph graph(edges, N);
+
+    //edge as adjMatrix
+    vector<vector<int>> edge = { 
+        {1,3},{2,3}
+    };
+    int N = 3;
+    
+    Solution sl;
+    cout << sl.findJudge(N, edge);
+}
