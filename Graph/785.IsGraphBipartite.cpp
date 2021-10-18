44e9214fa2cd9542b2b0129f025b21003448e01b
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-18 13:25:12 -0100

    785. Is Graph Bipartite?

--- Graph/785.IsGraphBipartite.cpp
+++ Graph/785.IsGraphBipartite.cpp
@@ -4,3 +4,13 @@
 #include <algorithm>
 #include <map>
 #include <unordered_map>
+
+using namespace std;
+
+// find path compression
+int _find(int v, vector<int>& parent){
+
+    if(v == parent[v])
+        return v;
+    return parent[v] = _find(parent[v], parent); // path compression
+}
