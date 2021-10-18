7c7bb4015571ff1f46939b9cf31761322ea15d70
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-18 15:13:41 -0100

    785. Is Graph Bipartite?

--- Graph/785.IsGraphBipartite.cpp
+++ Graph/785.IsGraphBipartite.cpp
@@ -61,20 +61,20 @@ public:
 int main()
 {
     // adjacency Matrix
-    // vector<vector<int>> graph = { 
-    //     {1,2,3},
-    //     {0,2},
-    //     {0,1,3},
-    //     {0,2}
-    // };
-
-    // adjacency Matrix
     vector<vector<int>> graph = { 
-        {1,3},
+        {1,2,3},
         {0,2},
-        {1,3},
+        {0,1,3},
         {0,2}
     };
+
+    // adjacency Matrix
+    // vector<vector<int>> graph = { 
+    //     {1,3},
+    //     {0,2},
+    //     {1,3},
+    //     {0,2}
+    // };
 
     Solution sl;
     std::cout << sl.isBipartite(graph);
