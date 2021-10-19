da59d7e5444d73adc11942a6e0012a5d33aa8acf
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-19 12:02:00 -0100

    743. Network Delay Time

--- Graph/743.NetworkDelayTime.cpp
+++ Graph/743.NetworkDelayTime.cpp
@@ -6,18 +6,31 @@
 using namespace std;
 
 class Solution {
+
+    void dfs(int u, vector<vector<int>> graph, vector<int>& visited){
+
+
+
+    }
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
         // adj Matrix graph
         vector<vector<int>> graph(n,vector<int>(n,0));
-        vector<int> outdegree(n,0);
+        vector<int> outdegree(n,0), visited(n,0);
 
         for(auto edge: times)
             graph[edge[0]][edge[1]] = edge[2],
             outdegree[edge[0]]++;
 
+        sort(outdegree.begin(),outdegree.end(), greater<>());
+
+        int i = 0;
+        if(!visited[k])
+            dfs(outdegree[i], graph, visited);
         
+
+        return 1;
 
     }
 };
