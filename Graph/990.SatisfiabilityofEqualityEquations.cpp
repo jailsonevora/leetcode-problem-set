649bcd14992f734a43acfed61441f6c63106c6a4
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-26 22:17:43 -0100

    990. Satisfiability of Equality Equations

--- Graph/990.SatisfiabilityofEqualityEquations.cpp
+++ Graph/990.SatisfiabilityofEqualityEquations.cpp
@@ -5,3 +5,19 @@
 #include <queue>
 
 using namespace std;
+
+
+int main()
+{
+    // adjacency edges
+    vector<vector<int>> edges = { 
+        {0,1,3},
+        {1,2,1},
+        {1,3,4},
+        {2,3,1}
+    };
+    int n = 4, distanceThreshold = 4;
+
+    Solution sl;
+    std::cout << sl.findTheCity(n, edges, distanceThreshold);
+}
