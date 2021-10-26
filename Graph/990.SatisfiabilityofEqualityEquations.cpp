7e59498f4ecb219540a458718b7a1e1ebd787cfa
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-10-26 22:23:18 -0100

    990. Satisfiability of Equality Equations

--- Graph/990.SatisfiabilityofEqualityEquations.cpp
+++ Graph/990.SatisfiabilityofEqualityEquations.cpp
@@ -18,24 +18,30 @@ int main()
     vector<string> equations = { 
         "a==b",
         "b!=a"
-    };
+    // };
 
-    vector<string> equations = { 
-        "b==a",
-        "a==b"
-    };
+    // vector<string> equations = { 
+    //     "b==a",
+    //     "a==b"
+    // };
 
-    vector<string> equations = { 
-        "a==b",
-        "b==c",
-        "a==c"
-    };
+    // vector<string> equations = { 
+    //     "a==b",
+    //     "b==c",
+    //     "a==c"
+    // };
 
-    vector<string> equations = { 
-        "a==b",
-        "b!=c",
-        "c==a"
-    };
+    // vector<string> equations = { 
+    //     "a==b",
+    //     "b!=c",
+    //     "c==a"
+    // };
+
+    // vector<string> equations = { 
+    //     "c==c",
+    //     "b==d",
+    //     "x!=z"
+    // };
 
     Solution sl;
     std::cout << sl.equationsPossible(equations);
