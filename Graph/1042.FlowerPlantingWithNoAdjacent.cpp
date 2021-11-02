2bd001b914c0ef6cfbc113bb4d5dbcbf273214e0
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-02 15:18:15 -0100

    1042. Flower Planting With No Adjacent

--- Graph/1042.FlowerPlantingWithNoAdjacent.cpp
+++ Graph/1042.FlowerPlantingWithNoAdjacent.cpp
@@ -7,6 +7,20 @@
 using namespace std;
 
 class Solution {
+    void bfs(vector<int> graph[], int n, int u){
+
+        // bfs
+        queue<int> queue;
+        vector<int> visited;
+
+        queue.push(u);
+
+        while (!queue.empty())
+        {
+            /* code */
+        }
+        
+    }
 public:
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
         
@@ -15,6 +29,10 @@ public:
         for(auto edge: paths)
             graph[edge[0]].push_back(edge[1]),
                 graph[edge[0]].push_back(edge[1]);
+
+        for (int u = 1; u < n; u++)
+            bfs(graph, n, u);
+        
 
         
     }
