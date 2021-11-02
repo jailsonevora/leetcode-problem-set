601bf6f9836678570229b92926550d84802b3ebe
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-02 14:48:54 -0100

    1042. Flower Planting With No Adjacent

--- Graph/1042.FlowerPlantingWithNoAdjacent.cpp
+++ Graph/1042.FlowerPlantingWithNoAdjacent.cpp
@@ -5,3 +5,17 @@
 #include <queue>
 
 using namespace std;
+
+
+int main()
+{
+    // adjacency edges
+    vector<vector<int>> paths = { 
+        {1,2},
+        {2,3},
+        {3,1}
+    };
+    int n = 3;
+
+    Solution sl;
+}
