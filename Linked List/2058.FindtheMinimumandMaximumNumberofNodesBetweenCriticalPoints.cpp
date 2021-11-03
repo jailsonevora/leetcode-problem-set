99fe1fd2ef4449e536590d7cb1be0a71da0f9e6c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-03 11:47:47 -0100

    2058. Find the Minimum and Maximum Number of Nodes Between Critical Points

--- Linked List/2058.FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints.cpp
+++ Linked List/2058.FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints.cpp
@@ -8,16 +8,16 @@ class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
 
-        ListNode* cur = head->next, *prev = head;
+        ListNode* cur = head->next;
+        int i = 0, prev = 0;
         vector<int> min, max;
-        int i = 0;
-
+        
         while (cur->next)
         {
-            /* min critical point */
+            /* all min critical point */
             if(cur->val < prev && cur->val < cur->next)
                 min.push_back(i);
-            /* max critical point */
+            /* all max critical point */
             else if(cur->val > prev && cur->val > cur->next)
                 max.push_back(i);
 
@@ -26,7 +26,7 @@ public:
             i++;
         }
         
-
+        return {};
         
     }
 };
