9cd7479f21514c9115e4b26788169a2b1ffb80fb
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-03 11:25:16 -0100

    2058. Find the Minimum and Maximum Number of Nodes Between Critical Points

--- Linked List/2058.FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints.cpp
+++ Linked List/2058.FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints.cpp
@@ -3,3 +3,21 @@
 #include "Util\Include\BuildLinkedList.h"
 
 using namespace std;
+
+
+int main(){
+    
+    ListNode* l1 = nullptr;
+
+    Solution ll;
+    BuildLinkedList bl;
+    bl.pushBack(1, l1);
+    bl.pushBack(2, l1);
+    bl.pushBack(3, l1);
+    bl.pushBack(4, l1);
+    bl.pushBack(5, l1);
+
+    ll.swapNodes(l1,2);
+
+    //[0,3,2,1,4,5] 3, 4, [1000000,1000001,1000002]
+}
