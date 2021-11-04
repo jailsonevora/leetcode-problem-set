997772fa0d66d9786401b5c874d0f97bd22ca5e2
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-04 11:23:19 -0100

    950. Reveal Cards In Increasing Order

--- Queue & Stack/950.RevealCardsInIncreasingOrder.cpp
+++ Queue & Stack/950.RevealCardsInIncreasingOrder.cpp
@@ -1,2 +1,23 @@
 #include<stdio.h>
 #include<queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    //string s = "abacbc";
+    string s = "aaabb";
+
+    Solution sl;
+    cout << sl.areOccurrencesEqual(s);
+}
