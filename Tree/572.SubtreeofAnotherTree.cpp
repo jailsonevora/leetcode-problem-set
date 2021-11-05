b7f3236f445a8eb01b774d1bed9fdc8a8188e23e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-05 11:05:24 -0100

    572. Subtree of Another Tree

--- Tree/572.SubtreeofAnotherTree.cpp
+++ Tree/572.SubtreeofAnotherTree.cpp
@@ -4,3 +4,25 @@
 #include "Util\Include\BuildTreeFromPreOrderArray.h"
 
 using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie();
+    cout.tie();
+
+    vector<int> preOrder1 = {1,2,2};
+    vector<int> inOrder1 = {1,2,2};
+
+    BuildTreeFromPreOrderArray bl;
+    TreeNode* root = bl.buildTree(preOrder1, inOrder1);
+
+    // vector<int> preOrder1 = {0};
+    // vector<int> inOrder1 = {0};
+
+    // TreeNode* root1 = bl.buildTree(preOrder1, inOrder1);
+
+    Solution sl;
+    for(auto it: sl.findMode(root))
+        cout << it << endl;
+}
