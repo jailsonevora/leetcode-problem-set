41a88c2d6394840eed12aae067cddbef311232d8
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-05 11:10:57 -0100

    572. Subtree of Another Tree

--- Tree/572.SubtreeofAnotherTree.cpp
+++ Tree/572.SubtreeofAnotherTree.cpp
@@ -28,6 +28,13 @@ int main(){
     vector<int> inOrder1 = {1,4,2};
     TreeNode* subRoot = bl.buildTree(preOrder1, inOrder1);
 
+    //example 2
+    vector<int> preOrder1 = {3,4,1,0,2,5};
+    vector<int> inOrder1 = {0,1,2,4,3,5};
+
+    BuildTreeFromPreOrderArray bl;
+    TreeNode* root = bl.buildTree(preOrder1, inOrder1);
+
     Solution sl;
     cout << sl.isSubtree(root, subRoot);
 }
