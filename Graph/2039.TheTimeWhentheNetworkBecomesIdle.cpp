39b3dd53a7adb675dce7470bc47803199d57ab09
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-05 14:55:05 -0100

    2039. The Time When the Network Becomes Idle

--- Graph/2039.TheTimeWhentheNetworkBecomesIdle.cpp
+++ Graph/2039.TheTimeWhentheNetworkBecomesIdle.cpp
@@ -7,6 +7,25 @@ using namespace std;
 class Solution {
 public:
     int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+
+        
+        queue<int> queue;
+        vector<int> visited(edges.size(),0);
+
+        queue.push(0);
+
+        while (!queue.empty())
+        {
+            int u = queue.front();
+            queue.pop();
+
+            for(int v: edges[u]){
+
+            }
+
+        }
+        
+
         
     }
 };
