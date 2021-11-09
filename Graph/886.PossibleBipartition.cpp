82f3f0cfdc284988d0bb9743ed69ae05b893262f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-08 23:06:02 -0100

    886. Possible Bipartition

--- Graph/886.PossibleBipartition.cpp
+++ Graph/886.PossibleBipartition.cpp
@@ -18,8 +18,14 @@ int main()
         {1,3},
         {2,4}
     };
-
     int n = 4;
+
+    // vector<vector<int>> dislikes = { 
+    //     {1,2},
+    //     {1,3},
+    //     {2,3}
+    // };
+    // int n = 3;
 
     Solution sl;
     std::cout << sl.possibleBipartition(4, dislikes);
