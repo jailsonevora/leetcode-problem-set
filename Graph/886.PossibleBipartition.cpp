21d7d7b37fd10e66b85106606c145b4a26f6f8e3
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-08 23:01:43 -0100

    886. Possible Bipartition

--- Graph/886.PossibleBipartition.cpp
+++ Graph/886.PossibleBipartition.cpp
@@ -3,3 +3,38 @@
 #include <algorithm>
 
 using namespace std;
+
+
+int main()
+{
+    vector<string> equations = { 
+        "a==b",
+        "b!=a"
+    };
+
+    // vector<string> equations = { 
+    //     "b==a",
+    //     "a==b"
+    // };
+
+    // vector<string> equations = { 
+    //     "a==b",
+    //     "b==c",
+    //     "a==c"
+    // };
+
+    // vector<string> equations = { 
+    //     "a==b",
+    //     "b!=c",
+    //     "c==a"
+    // };
+
+    // vector<string> equations = { 
+    //     "c==c",
+    //     "b==d",
+    //     "x!=z"
+    // };
+
+    Solution sl;
+    std::cout << sl.equationsPossible(equations);
+}
