f8ce4748b089cd0a75b935814000229a4600e92e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-10 15:12:24 -0100

    804. Unique Morse Code Words

--- Hash Table/804.UniqueMorseCodeWords.cpp
+++ Hash Table/804.UniqueMorseCodeWords.cpp
@@ -2,18 +2,25 @@
 #include<algorithm>
 #include<vector>
 
+
 using namespace std;
 
 class Solution {
+    vector<string> fullTable = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
+    "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
+
+
         
     }
 };
 
 int main(){
 
-    vector<string> words = {"gin","zen","gig","msg"};
+    vector<string> words = {"gin","zen","gig","msg"},
+    fullTable = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
+    "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 
     Solution sl;
     cout << sl.uniqueMorseRepresentations(words);
