685bf8819c5137b53366e4707f6b3bed3f4171bc
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-10 00:18:11 -0100

    886. Possible Bipartition

--- Graph/886.PossibleBipartition.cpp
+++ Graph/886.PossibleBipartition.cpp
@@ -3,70 +3,40 @@
 #include <algorithm>
 
 using namespace std;
-
 class Solution {
     int find(int v, vector<int>& parent){
         if(v == parent[v])
             return v;
         return parent[v] = find(parent[v], parent);
     }
-
-    void _union(int u, int v, vector<int>& parent, vector<int>& rank){
-
-        if(rank[v] < rank[u])
-            parent[v] = u;
-        else if(rank[u] < rank[v])
-            parent[u] = v;
-        else{
-            parent[v] = u;
-            rank[u]++;
-        }
-    }
 public:
     //graph adj list
-    // bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        
-    //     vector<int> parent(n,0), rank(n,0);
-    //     vector<int> graph[n];
-
-    //     for(auto edge: dislikes)
-    //         graph[edge[0]-1].push_back(edge[1]-1),
-    //         graph[edge[1]-1].push_back(edge[0]-1);
-
-    //     for(int i = 0; i < n; ++i)
-    //         parent[i] = i;
-        
-    //     for(int u = 0; u < n; ++u){
-    //         for(auto v: graph[u]){ 
-    //             int absFrom = find(u,parent);
-    //             int absTo = find(v,parent);
-
-    //             if(absFrom == absTo)
-    //                 return false;
-
-    //             //parent[absTo]=parent[graph[u][0]];
-    //             _union(graph[u][0], absTo, parent, rank);
-    //         }
-    //     }
-    //     return true;
-    // }
-
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         
-        n = n+1;
+        if(n<=2) 
+            return true;
+        
         vector<int> parent(n,0), rank(n,0);
+        vector<int> graph[n];
+
+        for(auto edge: dislikes)
+            graph[edge[0]-1].push_back(edge[1]-1),
+            graph[edge[1]-1].push_back(edge[0]-1);
 
         for(int i = 0; i < n; ++i)
             parent[i] = i;
+        
+        for(int u = 0; u < n; ++u){
+            int absFrom = find(u,parent);
 
-        for(auto edge: dislikes){
-            int absFrom = find(edge[0],parent);
-            int absTo = find(edge[1],parent);
+            for(auto v: graph[u]){ 
+                int absTo = find(v,parent);
 
-            if(absFrom == absTo)
-                return false;
-  
-            _union(absFrom, absTo, parent, rank);
+                if(absFrom == absTo)
+                    return false;
+                
+                parent[absTo]=parent[graph[u][0]];
+            }
         }
         return true;
     }
