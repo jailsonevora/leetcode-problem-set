4034c0fefbc1d19b0e8251ea1135ab416a88e8ae
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-11 09:22:51 -0100

    787. Cheapest Flights Within K Stops

--- Graph/787.CheapestFlightsWithinKStops.cpp
+++ Graph/787.CheapestFlightsWithinKStops.cpp
@@ -3,3 +3,10 @@
 #include<algorithm>
 
 using namespace std;
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+    }
+};
