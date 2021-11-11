79248c347f8971c03cbc5dbe721aaab9d49211fa
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-11 10:42:21 -0100

    787. Cheapest Flights Within K Stops

--- Graph/787.CheapestFlightsWithinKStops.cpp
+++ Graph/787.CheapestFlightsWithinKStops.cpp
@@ -8,6 +8,10 @@ class Solution {
     //using bfs + adjList + MinHeap in O(E logV)
     int dijkstra(int n, vector<pair<int,int>> graph[], int src, int dst, int k){
 
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<>>
+
+        
+
     }
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
