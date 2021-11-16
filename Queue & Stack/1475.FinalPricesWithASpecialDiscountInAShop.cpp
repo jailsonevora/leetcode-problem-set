25948502ff815458100e4108a3b2eac7be389aa7
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-16 22:47:55 -0100

    1475. Final Prices With a Special Discount in a Shop

--- Queue & Stack/1475.FinalPricesWithASpecialDiscountInAShop.cpp
+++ Queue & Stack/1475.FinalPricesWithASpecialDiscountInAShop.cpp
@@ -16,6 +16,9 @@ int main(){
     //ex1
     vector<int>  prices = {8,4,6,2,3};
 
+    //ex2
+    vector<int>  prices = {1,2,3,4,5};
+
     Solution sl;
     for(auto it: sl.finalPrices(prices))
         cout << it;
