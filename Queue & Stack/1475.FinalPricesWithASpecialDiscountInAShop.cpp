2302ac0e638f112e6837736f43a50f938a5300e1
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-18 10:36:22 -0100

    1475. Final Prices With a Special Discount in a Shop

--- Queue & Stack/1475.FinalPricesWithASpecialDiscountInAShop.cpp
+++ Queue & Stack/1475.FinalPricesWithASpecialDiscountInAShop.cpp
@@ -17,7 +17,7 @@ public:
         for (int i = 0; i < prices.size(); ++i)
         {   
             int dif = prices[i];
-            while (!stack.empty() && prices[i] <= stack.top()){
+            while (!stack.empty() && prices[i] <= stack.top())
                 dif = stack.top() - prices[i],
                     stack.pop();
 
@@ -31,10 +31,10 @@ public:
 int main(){
 
     //ex1
-    vector<int>  prices = {8,4,6,2,3};
+    // vector<int>  prices = {8,4,6,2,3};
 
     //ex2
-    //vector<int>  prices = {1,2,3,4,5};
+    vector<int>  prices = {1,2,3,4,5};
 
     Solution sl;
     for(auto it: sl.finalPrices(prices))
