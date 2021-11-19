409406823a3d9a539a4fbb11e93b0afce105ebcf
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-19 12:03:15 -0100

    1475

--- Queue & Stack/1475.FinalPricesWithASpecialDiscountInAShop.cpp
+++ Queue & Stack/1475.FinalPricesWithASpecialDiscountInAShop.cpp
@@ -13,7 +13,6 @@ public:
         // using monotonic stack
         for (int i = 0; i < prices.size(); ++i)
         {   
-            int dif = prices[i];
             while (!stack.empty() && prices[stack.top()] >= prices[i])
                 prices[stack.top()] -= prices[i],
                     stack.pop();
