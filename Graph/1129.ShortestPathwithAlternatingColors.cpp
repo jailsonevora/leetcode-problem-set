5c4e18a606cebd48d69aed51ae690a416079bad8
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-26 14:07:14 -0100

    1129. Shortest Path with Alternating Colors

--- Graph/1129.ShortestPathwithAlternatingColors.cpp
+++ Graph/1129.ShortestPathwithAlternatingColors.cpp
@@ -9,12 +9,32 @@ class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) {
 
-        vector<pair<int,char>> graph[n+1];
+        vector<vector<pair<int,char>>> graph[n];
 
-        for(int i = 0; i < n; ++i)
-            graph[red_edges[0][i]].push_back({red_edges[i][1],'r'}),
-                graph[blue_edges[0][i]].push_back({blue_edges[i][1],'b'});
-        
+        for(auto edge: red_edges){
+            int u = edge[0], v = edge[1];
+            graph[u].push_back({{v,'r'}});
+        }
+
+        for(auto edge: blue_edges){
+            int u = edge[0], v = edge[1];
+            graph[u].push_back({{v,'b'}});
+        }
+
+        int w = 0;
+        // vector<vector<char>> graph(n,vector<char>(n,'n'));
+
+        // for(auto edge: red_edges){
+        //     int u = edge[0], v = edge[1];
+        //     graph[u][v] = 'r';
+        // }
+
+        // for(auto edge: blue_edges){
+        //     int u = edge[0], v = edge[1];
+        //     graph[u][v] = 'b';
+        // }
+
+        return {};
     }
 };
 
