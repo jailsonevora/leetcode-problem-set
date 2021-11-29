db371a6ad60bf953ed3ca7522c70d47b93c4fd11
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-11-29 10:16:52 -0100

    1129

--- Graph/1129.ShortestPathwithAlternatingColors.cpp
+++ Graph/1129.ShortestPathwithAlternatingColors.cpp
@@ -9,6 +9,10 @@ class Solution {
 
     void bfs(vector<pair<int, char>> graph[], int start, int end, int n, vector<int>& ans){
 
+        queue<int, char> queue;
+
+        vector<int> distance(n,0);
+
     }
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) {
