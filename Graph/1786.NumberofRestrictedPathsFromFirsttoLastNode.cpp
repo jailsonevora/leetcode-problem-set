138c22e3af6162cdd79b59640abdca5baa7e946e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-01 13:36:49 -0100

    1786

--- Graph/1786.NumberofRestrictedPathsFromFirsttoLastNode.cpp
+++ Graph/1786.NumberofRestrictedPathsFromFirsttoLastNode.cpp
@@ -10,13 +10,22 @@ class Solution {
 
 
     // using dijkstra shortest path with adjLis + minHeap
-    void dijkstra(vector<pair<int, int>> graph[], int n, int src, int dst, vector<int> ans){
+    void dijkstra(vector<pair<int, int>> graph[], int src, int dst, vector<int>& ans){
 
     }
 
 public:
     int countRestrictedPaths(int n, vector<vector<int>>& edges) {
         
+        vector<pair<int, int>> graph[n];
+        vector<int> ans(n, 0);
+
+        for(auto edge: edges)
+            graph[edge[0]].push_back({edge[1],edge[2]}),
+            graph[edge[1]].push_back({edge[0],edge[2]});
+
+        for(int u = 1; u < n; ++u)
+            dijkstra(graph, u, n, ans);
     }
 };
 
@@ -32,12 +41,6 @@ int main(){
         {5,4,10}
     };
     int n = 3;
-
-    vector<pair<int, int>> graph[n];
-
-    for(auto edge: edges)
-        graph[edge[0]].push_back({edge[1],edge[2]}),
-        graph[edge[1]].push_back({edge[0],edge[2]});
 
     
 
