39191b097c947e09e4ebd2cadbc13fb66e2befd8
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-01 12:33:19 -0100

    1786

--- Graph/1786.NumberofRestrictedPathsFromFirsttoLastNode.cpp
+++ Graph/1786.NumberofRestrictedPathsFromFirsttoLastNode.cpp
@@ -6,8 +6,14 @@
 using namespace std;
 
 class Solution {
+    // using topological sort
+
+
+    // using dijkstra shortest path with adjLis + minHeap
+
 public:
     int countRestrictedPaths(int n, vector<vector<int>>& edges) {
         
     }
 };
+
