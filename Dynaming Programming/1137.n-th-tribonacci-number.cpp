bef72fe7f2519638edab719e2a5b7f5ff99d7a42
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-09 13:28:00 -0100

    1137

--- Dynaming Programming/1137.n-th-tribonacci-number.cpp
+++ Dynaming Programming/1137.n-th-tribonacci-number.cpp
@@ -21,8 +21,9 @@ class Solution {
         dp[2] = 1;
 
         for(int i = 3; i < n; ++i)
-            
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
 
+        return dp[n];
     }
 
     // top-down aproach memoization
