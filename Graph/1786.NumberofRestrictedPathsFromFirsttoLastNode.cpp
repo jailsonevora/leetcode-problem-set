76e546e31f51a881ccc46fee091843fb2e1384e7
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-08 23:00:28 -0100

    1786

--- Graph/1786.NumberofRestrictedPathsFromFirsttoLastNode.cpp
+++ Graph/1786.NumberofRestrictedPathsFromFirsttoLastNode.cpp
@@ -7,8 +7,7 @@
 using namespace std;
 
 class Solution {
-    // using dfs + Dynaminc programing A top-down approach that is more concretely known as Memoization
-    // top-down
+    // using dfs + Dynaminc programing
     int dfs(int n, vector<pair<int, int>> graph[], vector<int>& distance, vector<int>& dp, const int mod = 1e9 + 7){
         
         if(n == 1) 
@@ -17,7 +16,7 @@ class Solution {
         if(dp[n] != -1) 
             return dp[n];
 
-        int paths = 0;
+        long long paths = 0;
 
         for(auto it: graph[n]){
             if(distance[it.first] > distance[n]){
@@ -27,35 +26,13 @@ class Solution {
 
         return dp[n] = paths%mod;
     }
-    // using dfs + Dynaminc programing A botton-up 
-    // botton-up
-    // int dfs(int n, vector<pair<int, int>> graph[], vector<int>& distance, vector<int>& dp, const int mod = 1e9 + 7){
-        
-    //     if(n == 1) 
-    //         return 1;
-        
-    //     if(dp[n] != -1) 
-    //         return dp[n];
-
-    //     vector<int> dp;
-    //     int paths = 0;
-    //     dp[1] = 1;
-
-    //     for(auto it: graph[n]){
-    //         if(distance[it.first] > distance[n]){
-    //             paths += (dp[it.first] % mod) % mod;
-    //         }
-    //     }
-    //     return dp[n] = paths%mod;
-    // }
-
     // Run a Dijkstra from node numbered n to compute distance from the last node.
     // using dijkstra shortest path with adjLis + minHeap O(e logv)
     void dijkstra(vector<pair<int, int>> graph[], int n, vector<int>& distance){
 
         priority_queue< pair<int,int>, vector<pair<int,int>> , greater<> > p_queue;
         p_queue.push({0,n});
-
+        
         distance[n] = 0;
 
         while (!p_queue.empty())
@@ -69,10 +46,9 @@ class Solution {
                 int v = it.first;
                 int w = it.second;
 
-                if(distance[v] > dis + w){
-                    distance[v] = dis + w;
-                        p_queue.push({dis + w, v});
-                }
+                if(distance[v] > dis + w)
+                    distance[v] = dis + w,
+                        p_queue.push({distance[v], v});
             }
         }
     }
@@ -94,6 +70,36 @@ public:
         vector<int> dp(n+1,-1);
         return dfs(n, graph, distance, dp, 1e9 + 7);
     }
+    // int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+    //     int kMod = 1e9 + 7;
+    //     vector<vector<pii>> graph(n+1);
+    //     for(auto &e : edges) {
+    //         graph[e[0]].push_back({e[1], e[2]});
+    //         graph[e[1]].push_back({e[0], e[2]});
+    //     }
+    //     priority_queue<pii, vector<pii>, greater<pii>> pq;
+    //     vector<int> dists(n+1, INT_MAX);
+    //     vector<int> dp(n+1);
+    //     dists[n] = 0;
+    //     dp[n] = 1;
+    //     pq.push({0, n});
+        
+    //     while(!pq.empty()) {
+    //         auto[d, u] = pq.top(); pq.pop();
+    //         if(d > dists[u]) continue;
+    //         if(u == 1) break;
+    //         for(auto &[v,w] : graph[u]) {
+    //             if(dists[v] > dists[u] + w) {
+    //                 dists[v] = dists[u] + w;
+    //                 pq.push({dists[v], v});
+    //             }
+    //             if(dists[v] > dists[u]) {
+    //                 dp[v] = (dp[v] + dp[u]) % kMod;
+    //             }
+    //         }
+    //     }
+    //     return dp[1];
+    // }
 };
 
 int main(){
