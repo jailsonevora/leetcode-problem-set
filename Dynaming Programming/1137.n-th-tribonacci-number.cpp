e8fafe1a819cfd34fa19e94f2754e4950b8cd2a1
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-08 23:20:13 -0100

    1137

---
+++ Dynaming Programming/1137.n-th-tribonacci-number.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1137 lang=cpp
+ *
+ * [1137] N-th Tribonacci Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+    }
+};
+// @lc code=end
+
