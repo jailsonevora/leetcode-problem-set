6c5608834f473d43a8388cacd907948a4d1043b7
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-13 23:49:18 -0100

    1137

--- Dynaming Programming/1137.n-th-tribonacci-number.cpp
+++ Dynaming Programming/1137.n-th-tribonacci-number.cpp
@@ -49,9 +49,10 @@ class Solution {
         if(dp[n] != -1)
             return dp[n];
 
-        return dp[n] = topDown(n - 1, vector<int> dp)
+        if(dp[n] == -1)
+            dp[n] = topDown(n - 1, dp) + topDown(n - 2, dp) + topDown(n - 3, dp);
 
-        
+        return dp[n]         
 
     }
 
