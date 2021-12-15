b3bbdc43a8e1bf6225c52ea6fc35f7a1dc75507d
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-15 13:08:00 -0100

    416

--- Graph/416.partition-equal-subset-sum.cpp
+++ Graph/416.partition-equal-subset-sum.cpp
@@ -24,7 +24,7 @@ int main(){
     vector<int> nums = {1,5,11,5};
 
     // ex2
-    vector<int> nums = {1,5,11,5};
+    // vector<int> nums = {1,2,3,5};
 
     Solution sl;
     cout << sl.canPartition(nums);
