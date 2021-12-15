ad9c9708d2062bd190af82a18278235ced237106
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-15 15:14:43 -0100

    416

--- Dynaming Programming/416.partition-equal-subset-sum.cpp
+++ Dynaming Programming/416.partition-equal-subset-sum.cpp
@@ -45,12 +45,10 @@ public:
 
         int n = nums.size();
         int w = 0; 
-        std::accumulate(nums.begin(), nums.end(), w);
-        w /=2;
+        w = std::accumulate(nums.begin(), nums.end(), w)/2;
 
         // dynamic programming aproach
         return bottonUp(nums, w, n); 
-        
     }
 };
 // @lc code=end
@@ -61,7 +59,7 @@ int main(){
     vector<int> nums = {1,5,11,5};
 
     // ex2
-    // vector<int> nums = {1,2,3,5};
+    //vector<int> nums = {1,2,3,5};
 
     Solution sl;
     cout << sl.canPartition(nums);
