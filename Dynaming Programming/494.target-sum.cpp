e9cc77d3a5753bb78bd7bc9442d6584933fab93a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-17 14:03:38 -0100

    494

---
+++ Dynaming Programming/494.target-sum.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=494 lang=cpp
+ *
+ * [494] Target Sum
+ */
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+// @lc code=start
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
