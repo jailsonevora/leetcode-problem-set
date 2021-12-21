1d63183881b210bf1f27c2836a1c8c682e2a1192
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-21 14:23:08 -0100

    518

--- Dynaming Programming/518.coin-change-2.cpp
+++ Dynaming Programming/518.coin-change-2.cpp
@@ -20,16 +20,16 @@ class Solution {
         for (int row = 0; row <= n; row++)
             dp[row][0] = 1; 
         for (int col = 1; col <= amount; col++)
-            dp[0][col] = 1;
+            dp[0][col] = 0;
 
-        for (int row = 0; row <= n; row++)
+        for (int row = 1; row <= n; row++)
             for (int col = 1; col <= amount; col++){
                 // exclude
                 if(col < coins[row-1])
                     dp[row][col] = dp[row-1][col];
                 // include + exclude
                 else
-                    dp[row][col] = dp[row-1][col - coins[row-1]] + dp[row-1][col];
+                    dp[row][col] = dp[row][col - coins[row-1]] + dp[row-1][col];
             }
         return dp[n][amount];
     }
