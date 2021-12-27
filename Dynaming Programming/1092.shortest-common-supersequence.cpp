4f544baddce08152464db048e5d9a8e01443dc0b
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-27 10:23:14 -0100

    1092

---
+++ Dynaming Programming/1092.shortest-common-supersequence.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1092 lang=cpp
+ *
+ * [1092] Shortest Common Supersequence 
+ */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+// @lc code=start
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        
+    }
+};
+// @lc code=end
+
