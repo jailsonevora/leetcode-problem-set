eee01ac850ef261817f858f66ccbc66be9c48b70
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-12-30 12:38:26 -0100

    354

--- Dynaming Programming/354.russian-doll-envelopes.cpp
+++ Dynaming Programming/354.russian-doll-envelopes.cpp
@@ -30,7 +30,8 @@ public:
         for (int i = 1; i < n; i++)
             for (int j = 0; j < i; j++){
 
-                if(envelopes[i][0] > envelopes[j][0] && )
+                if(envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]
+                    && 1 + dp[j]  )
 
             }
 
