47a1a5c99ed9a87099c5ab11cc425246f817157e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-03 11:58:33 -0100

    918

--- Dynaming Programming/Kadanes algorithm/918.maximum-sum-circular-subarray.cpp
+++ Dynaming Programming/Kadanes algorithm/918.maximum-sum-circular-subarray.cpp
@@ -13,29 +13,32 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-    // kadanes algorithm + inverting the vector aproach
-    int invertingVector(vector<int>& nums){
-
-        for (int i = 0; i < nums.size(); i++)
-            nums[i]*=-1;
-
-        int arr_sum = accumulate(nums.begin(), nums.end(),0);
-
-        int maxnd = 0, maxsfar = INT_MIN;
-        for(int i = 0; i < nums.size(); ++i){
-            maxnd = maxnd + nums[i];
-            if(maxnd < nums[i])
-                maxnd = nums[i];
-            if(maxsfar < maxnd)
-                maxsfar = maxnd;
-        }
-        return max(arr_sum,maxsfar);
-    }
     // kadanes algorithm
+    int kadanesAlgorithm(vector<int>& nums){
+
+        int max_straight_SUM = INT_MIN, min_straight_SUM = INT_MAX, array_SUM = 0;
+        
+        int temp_maxSUM = 0, temp_minSUM = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            array_SUM +=nums[i];
+            
+            temp_maxSUM += nums[i];
+            max_straight_SUM = max_straight_SUM<temp_maxSUM? temp_maxSUM:max_straight_SUM;
+            temp_maxSUM = temp_maxSUM<0?0:temp_maxSUM;
+            
+            temp_minSUM += nums[i];
+            min_straight_SUM = min_straight_SUM>temp_minSUM? temp_minSUM:min_straight_SUM;
+            temp_minSUM = temp_minSUM>0?0:temp_minSUM;
+        }
+        if(array_SUM==min_straight_SUM)
+            return max_straight_SUM;
+        return max(max_straight_SUM,(array_SUM-min_straight_SUM));
+    }
+
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
 
-        return invertingVector(nums);        
+        //return invertingVector(nums);        
     }
 };
 // @lc code=end
