12cc765f5040140a4c479ac46788544ec6208707
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-04 13:09:08 -0100

    63

--- Dynaming Programming/DIP on Grid/63.unique-paths-ii.cpp
+++ Dynaming Programming/DIP on Grid/63.unique-paths-ii.cpp
@@ -12,6 +12,24 @@ using namespace std;
 
 // @lc code=start
 class Solution {
+    int bottomUp(vector<vector<int>>& obstacleGrid){
+        
+        int m = obstacleGrid.size(),
+        n = obstacleGrid[0].size();
+        int dp[m][n];
+
+        for (int row = 0; row < m; row++)
+            dp[row][0] = 1;
+
+        for (int col = 1; col < n; col++)
+            dp[0][col] = 1;
+
+        for (int row = 1; row < m; row++)
+            for (int col = 1; col < n; col++)
+                dp[row][col] = dp[row][col-1] + dp[row-1][col];
+
+        return dp[m-1][n-1];
+    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
