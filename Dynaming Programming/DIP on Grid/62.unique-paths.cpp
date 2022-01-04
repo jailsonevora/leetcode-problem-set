f394bad71870525bee5dfbe6472ddbd2ecbdd76a
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-04 10:28:57 -0100

    62

--- Dynaming Programming/DIP on Grid/62.unique-paths.cpp
+++ Dynaming Programming/DIP on Grid/62.unique-paths.cpp
@@ -15,19 +15,19 @@ class Solution {
     // bottom-Up
     int bottomUp(int m, int n){
 
-        int dp[m+1][n+1];
+        int dp[m][n];
 
-        for (int row = 0; row <= m; row++)
+        for (int row = 0; row < m; row++)
             dp[row][0] = 1;
 
-        for (int col = 1; col <= n; col++)
+        for (int col = 1; col < n; col++)
             dp[0][col] = 1;
 
-        for (int row = 1; row <= m; row++)
-            for (int col = 1; col <= n; col++)
+        for (int row = 1; row < m; row++)
+            for (int col = 1; col < n; col++)
                 dp[row][col] = dp[row][col-1] + dp[row-1][col];
 
-        return dp[m][n];
+        return dp[m-1][n-1];
     }
     // memoization
 public:
