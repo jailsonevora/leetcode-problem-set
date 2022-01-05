3a3704dc9e344b12141d26081c93f1d98b333d08
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-05 12:27:09 -0100

    221

---
+++ Graph/221.maximal-square.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=221 lang=cpp
+ *
+ * [221] Maximal Square
+ */
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        
+    }
+};
+// @lc code=end
+
