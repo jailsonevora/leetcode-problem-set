c6678d0ea3b1dc44f6c623a29de43ca97e70bc74
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-06 16:16:21 -0100

    139

---
+++ Graph/139.word-break.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=139 lang=cpp
+ *
+ * [139] Word Break
+ */
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+// @lc code=start
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+    }
+};
+// @lc code=end
+
