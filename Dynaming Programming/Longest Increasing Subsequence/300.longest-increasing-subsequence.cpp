3acd794821c053ccb16f0b1a29951bc0c7cabfc6
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-11 16:15:55 -0100

    300

--- Dynaming Programming/Longest Increasing Subsequence/300.longest-increasing-subsequence.cpp
+++ Dynaming Programming/Longest Increasing Subsequence/300.longest-increasing-subsequence.cpp
@@ -12,25 +12,44 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
+    // DP LIS Longest increasing subsequence
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
-        int dip[n]; 
+        int dp[n]; 
 
-        dip[0] = 1; 
+        dp[0] = 1; 
 
         /* Compute optimized LIS values in bottom up manner */
         for (int i = 1; i < n; i++ ) 
         { 
-            dip[i] = 1; 
+            dp[i] = 1; 
             for (int j = 0; j < i; j++ ) 
-                if ( nums[i] > nums[j] && dip[i] < dip[j] + 1) 
-                    dip[i] = dip[j] + 1; 
+                if ( nums[i] > nums[j] && dp[i] < dp[j] + 1) 
+                    dp[i] = dp[j] + 1; 
         } 
 
         // Return maximum value in lis[] 
-        return *max_element(dip, dip+n); 
+        return *max_element(dp, dp+n); 
     }
+
+    //
+    // DP Maximum Sum Increasing Subsequence MSIS
+    // int maxSubArray(vector<int>& nums) {
+
+    //     int n = nums.size();
+    //     int dp[n];
+
+    //     // initialize dp array
+    //     for (int i = 0; i < n; i++)
+    //         dp[i] = nums[i];
+
+    //     for (int i = 1; i < n; i++)
+    //         for (int j = 0; j < i; j++)
+    //             if(nums[i] > nums[j] && dp[i] < dp[j] + nums[i])
+    //                 dp[i] = dp[j] + nums[i];
+    //     return *max_element(dp, dp+n);      
+    // }
 };
 // @lc code=end
 
