1b64b44ec477c5619d7a468aeda1be685687a210
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-27 14:50:58 -0100

    2

--- Queue & Stack/2.NumberofIslands.cpp
+++ Queue & Stack/2.NumberofIslands.cpp
@@ -6,17 +6,17 @@ using namespace std;
 class Solution {
 public:
 
-    void DFS(vector<vector<char>> &grid, int x, int y, int r, int c){
+    void dfsOnGrid(vector<vector<char>> &grid, int x, int y, int r, int c){
         if (x < 0 || x >= r || y < 0 || y >= c || grid[x][y] != '1')
             return;
         
         grid[x][y] = '2';
 
         //recursive call to 4 adjacent directions
-        DFS(grid, x+1, y, r, c);
-        DFS(grid, x, y+1, r, c);
-        DFS(grid, x-1, y, r, c);
-        DFS(grid, x, y-1, r, c);
+        dfsOnGrid(grid, x+1, y, r, c);
+        dfsOnGrid(grid, x, y+1, r, c);
+        dfsOnGrid(grid, x-1, y, r, c);
+        dfsOnGrid(grid, x, y-1, r, c);
     }
 
     int numIslands(vector<vector<char>>& grid) {
@@ -33,7 +33,7 @@ public:
             for (int c = 0; c < cols; c++)
                 if (grid[r][c] == '1')
                 {   
-                    DFS(grid, r,c, rows, cols);
+                    dfsOnGrid(grid, r,c, rows, cols);
                     ans++;
                 }
         return ans;        
