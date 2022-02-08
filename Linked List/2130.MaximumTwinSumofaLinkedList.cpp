d2677114831c1efa61b8824bc3c775042b0a9125
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-08 15:14:01 -0100

    2130

--- Linked List/2130.MaximumTwinSumofaLinkedList.cpp
+++ Linked List/2130.MaximumTwinSumofaLinkedList.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include"Util\Include\BuildLinkedList.h"
 
 using namespace std;
 
+class olution{
+    public:
+
+};
+
+int main(){
+
+    Solution sl;
+    ListNode* head = nullptr;
+
+    BuildLinkedList bl;
+
+    vector<int> data = {2,1,3,5,6,4,7};
+    for(auto it: data)
+        bl.pushBack(it, head);
+
+    sl.oddEvenList(head);
+}
