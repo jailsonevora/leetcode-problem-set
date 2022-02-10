ad15727fdb4d7a5b9ab73e4368ee157b454b6a8e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-10 13:57:37 -0100

    1863

--- Backtracking/1863.sum-of-all-subset-xor-totals.cpp
+++ Backtracking/1863.sum-of-all-subset-xor-totals.cpp
@@ -12,14 +12,25 @@
 using namespace std;
 
 class Solution {
+    void backtracking(vector<int>& nums, int& ans){
+
+        
+
+    }
 public:
     int subsetXORSum(vector<int>& nums) {
-        
+        int ans = 0;
+        backtracking(nums,ans);
+        return ans;
     }
 };
 // @lc code=end
 int main(){
 
-    
+    vector<int> nums = {1,3};
+
+    Solution sl;
+
+    cout << sl.subsetXORSum(nums);
 }
 
