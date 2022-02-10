f5d5510645a87681b52662d414eb214269d2826e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-10 13:52:07 -0100

    1863

---
+++ Backtracking/1863.sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1863 lang=cpp
+ *
+ * [1863] Sum of All Subset XOR Totals
+ */
+
+// @lc code=start
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
