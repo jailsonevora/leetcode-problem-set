10e3be6105497de5c120d8d1c70e062c34629850
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-14 12:55:09 -0100

    23

--- G/23.merge-k-sorted-lists.cpp
+++ G/23.merge-k-sorted-lists.cpp
@@ -25,8 +25,25 @@ using namespace std;
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+        return nullptr;
     }
 };
 // @lc code=end
+int main(){
 
+    Solution sl;
+    ListNode* head = nullptr;
+    vector<ListNode*> lists;
+
+    BuildLinkedList bl;
+
+    vector<vector<int>> data = {{1,4,5},{1,3,4},{2,6}};
+    for(auto it: data){
+        ListNode* head = nullptr;
+        for(auto i: it)
+            bl.pushBack(i, head);
+        lists.push_back(head);
+    }
+
+    sl.mergeKLists(lists);
+}
