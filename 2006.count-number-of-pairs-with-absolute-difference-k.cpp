39feb9423640862c370410c11cfa221aeb21499f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-14 10:52:21 -0100

    2006

---
+++ 2006.count-number-of-pairs-with-absolute-difference-k.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=2006 lang=cpp
+ *
+ * [2006] Count Number of Pairs With Absolute Difference K
+ */
+
+// @lc code=start
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+    void backtracking(vector<int>& nums, int k){
+
+    }
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        
+        
+    }
+};
+// @lc code=end
+int main(){
+
+    vector<int> nums = {1,2,2,1}; int k = 1;
+
+    Solution sl;
+    cout << sl.countKDifference(nums,k);
+}
