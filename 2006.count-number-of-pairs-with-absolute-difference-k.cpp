19241d094b7414c8478c24118c9984d13c475a58
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-14 10:58:15 -0100

    2006

--- 2006.count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006.count-number-of-pairs-with-absolute-difference-k.cpp
@@ -12,13 +12,26 @@
 using namespace std;
 
 class Solution {
-    void backtracking(vector<int>& nums, int k){
+    void backtracking(vector<int>& nums, int k, int i, vector<int>& ans){
+
+        if(i == nums.size()){
+            ans.push_back(i);
+        }
+        else{
+            for (int j = i+1; j < nums.size(); i++)
+            {
+                if(abs(nums[i]-nums[j]) == k)
+                    backtracking(nums,k,i+1,ans);
+                nums.pop_back();
+            }            
+        }
 
     }
 public:
     int countKDifference(vector<int>& nums, int k) {
-        
-        
+        vector<int> ans;
+        backtracking(nums,k,0,ans);
+        return ans.size();        
     }
 };
 // @lc code=end
