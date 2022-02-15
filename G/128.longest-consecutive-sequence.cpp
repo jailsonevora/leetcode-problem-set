14bc097ba09b92d9f1b76ab69adb51edc4b8942c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-15 17:24:03 -0100

    128 G Medium Problem

--- G/128.longest-consecutive-sequence.cpp
+++ G/128.longest-consecutive-sequence.cpp
@@ -59,8 +59,9 @@ public:
 // @lc code=end
 int main(){
 
-    vector<int> nums = {100,4,200,1,3,2};
+    //vector<int> nums = {100,4,200,1,3,2};
     //vector<int> nums = {0,-1};
+    vector<int> nums = {0,3,7,2,5,8,4,6,0,1};
 
     Solution sl;
     cout << sl.longestConsecutive(nums);
