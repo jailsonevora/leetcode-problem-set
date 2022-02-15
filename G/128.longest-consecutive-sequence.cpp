f67795d5c6bcedeff27ef266938088a317bf153d
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-15 13:19:26 -0100

    128 G Medium Problem

--- G/128.longest-consecutive-sequence.cpp
+++ G/128.longest-consecutive-sequence.cpp
@@ -9,19 +9,19 @@
 #include<algorithm>
 #include<vector>
 #include<unordered_set>
+#include<unordered_map>
 #include<map>
 
 using namespace std;
 
 class Solution {
     // find path compression
-    int _find(vector<int>& parent, int v){
+    int _find(unordered_map<int,int>& parent, int v){
         if(v == parent[v])
             return v;
         return parent[v] = _find(parent,parent[v]);
     }
-
-    void _union(vector<int>& parent, int u, int v){
+    void _union(unordered_map<int,int>& parent, int u, int v){
         
         u = _find(parent,u);
         v = _find(parent,v);
@@ -32,25 +32,35 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
 
-        vector<int> parent(nums.size(),0);
+        unordered_map<int,int> parent;
         for (int i = 0; i < nums.size(); i++)
-            parent[i] = i;
+            parent[nums[i]] = nums[i];
 
         unordered_set<int> set;
         for(auto i:nums)
             set.insert(i);
 
+        // checking if the exists a element consecutive inferior than i in the set
         for(auto i:nums)
             if(set.count(i-1))
                 _union(parent,i,i-1);
-        
-        return 0;
+
+        // checking which DSU is having maximum elements
+        unordered_map<int,int>mp;
+        int ans = 0;
+        for(int i=0; i < nums.size(); i++) {
+            int u = _find(parent,i);
+            mp[u]++;
+            ans = max(ans,mp[u]);
+        }
+        return ans;
     }
 };
 // @lc code=end
 int main(){
 
     vector<int> nums = {100,4,200,1,3,2};
+    //vector<int> nums = {0,-1};
 
     Solution sl;
     cout << sl.longestConsecutive(nums);
