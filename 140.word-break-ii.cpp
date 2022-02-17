4c154efc77e540518f330bfae310702aa639364e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-17 14:47:12 -0100

    140 G Hard Problem

---
+++ 140.word-break-ii.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=140 lang=cpp
+ *
+ * [140] Word Break II
+ */
+
+// @lc code=start
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        
+    }
+};
+// @lc code=end
+int main(){
+    string s = "catsanddog";
+    vector<string> wordDict = {"cat","cats","and","sand","dog"};
+
+    Solution sl;
+    sl.wordBreak(s,wordDict);
+}
