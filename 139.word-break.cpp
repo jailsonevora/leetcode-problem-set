c0798c9e979e31962e57390203856512bd45b801
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-17 11:30:07 -0100

    139 G Medium Problem

--- 139.word-break.cpp
+++ 139.word-break.cpp
@@ -16,40 +16,40 @@ using namespace std;
 class Solution {
 public:
     // two dimension
-    bool wordBreak(string s, vector<string>& wordDict) {
+    // bool wordBreak(string s, vector<string>& wordDict) {
 
-        int n = s.size();
+    //     int n = s.size();
 
-        unordered_set<string> set;
-        for(string s: wordDict)
-            set.insert(s);
+    //     unordered_set<string> set;
+    //     for(string s: wordDict)
+    //         set.insert(s);
 
-        bool dp[n+1][n+1];
-        int len = 1;
+    //     bool dp[n+1][n+1];
+    //     int len = 1;
 
-        while(len <= n)
-        {   
-            int left = 1, right = len;
-            while(right <= n)
-            {
-                if(set.count(s.substr(left-1,len)))
-                    dp[left][right] = true;
-                else{
-                        bool flag = false;
-                        for(int i=left; i<right; ++i)
-                            if(dp[left][i] && dp[i+1][right]){
-                                flag = true;
-                                break;
-                            }                    
-                        dp[left][right] = flag;
-                    }
-                left++;
-                right++;
-            }
-            len++;
-        }
-        return dp[1][n];        
-    }
+    //     while(len <= n)
+    //     {   
+    //         int left = 1, right = len;
+    //         while(right <= n)
+    //         {
+    //             if(set.count(s.substr(left-1,len)))
+    //                 dp[left][right] = true;
+    //             else{
+    //                     bool flag = false;
+    //                     for(int i=left; i<right; ++i)
+    //                         if(dp[left][i] && dp[i+1][right]){
+    //                             flag = true;
+    //                             break;
+    //                         }                    
+    //                     dp[left][right] = flag;
+    //                 }
+    //             left++;
+    //             right++;
+    //         }
+    //         len++;
+    //     }
+    //     return dp[1][n];        
+    // }
 
     // one dimension DP problem
     bool wordBreak(string s, vector<string>& wd) {
