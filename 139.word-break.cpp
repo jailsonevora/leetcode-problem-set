489cac9bcdfd6a76aa2bc72795c1f87101c776fc
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-17 10:22:36 -0100

    139 G Medium Problem

---
+++ 139.word-break.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=139 lang=cpp
+ *
+ * [139] Word Break
+ */
+
+// @lc code=start
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+    }
+};
+// @lc code=end
+
