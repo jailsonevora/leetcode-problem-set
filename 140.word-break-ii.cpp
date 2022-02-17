6c3cc1039c0073a920b9d8e36cffe1b8c65dff57
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-17 15:12:07 -0100

    140 G Hard Problem

--- 140.word-break-ii.cpp
+++ 140.word-break-ii.cpp
@@ -8,13 +8,37 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
+    // backtracking + memo 
+    bool backtracking(vector<int>& dp, int i, string s, vector<string>& ans, string sentence, unordered_set<string> set){
+
+        if(i == s.size()){
+            ans.push_back(sentence);
+            return true;
+        }
+
+        if(dp[i] == -1)
+            return dp[i];
+
+        for (int j = i+1; i < s.size(); j++)
+            if(set.count(s.substr(i,j-i))) 
+                if(backtracking(dp,j+1,s,ans,""+s.substr(i,j-i),set))
+                    return dp[i] = true;
+        return dp[i] = false;
+
+    }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         
+        unordered_set<string> set(wordDict.begin(),wordDict.end());
+        vector<int> dp(s.size(),-1);
+        vector<string> ans;
+        backtracking(dp,0,s,ans,"",set);
+        return ans;
     }
 };
 // @lc code=end
