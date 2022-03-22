088e56381d7b8a485d3e85f3c835c913b6e9002c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-03-22 22:45:20 -0100

    2192

--- Graph/Topological Sort/DFS+Stack/2192.AllAncestorsofaNodeinaDirectedAcyclicGraph.cpp
+++ Graph/Topological Sort/DFS+Stack/2192.AllAncestorsofaNodeinaDirectedAcyclicGraph.cpp
@@ -9,11 +9,11 @@ public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
 
         //vector<int> graph[n], indegree[n];
-        vector<vector<int>> indegree(n);
+        //vector<vector<int>> indegree(n);
 
         for(auto edge: edges){
-            //graph[edge[0]].push_back(edge[1]);
-            indegree[edge[1]].push_back(edge[0]);
+            graph[edge[0]].push_back(edge[1]);
+            //indegree[edge[1]].push_back(edge[0]);
         }
 
         return indegree;
