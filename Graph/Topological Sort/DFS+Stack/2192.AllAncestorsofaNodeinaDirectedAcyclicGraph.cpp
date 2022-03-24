6a8bf8dd514d9b073c5cda1b19bbe7f60aa34a90
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-03-24 19:12:23 -0100

    2192

--- Graph/Topological Sort/DFS+Stack/2192.AllAncestorsofaNodeinaDirectedAcyclicGraph.cpp
+++ Graph/Topological Sort/DFS+Stack/2192.AllAncestorsofaNodeinaDirectedAcyclicGraph.cpp
@@ -23,15 +23,14 @@ public:
         for(auto edge: edges)
             graph[edge[1]].push_back(edge[0]);
 
-        //now apply dfs on each node to find all node that connect to it
-        vector<int> visited(n+1,0);
+        //now apply dfs on each node to find all node that connect to it     
 
         for (int i = 0; i < n; i++){
+            vector<int> visited(n,0);
             vector<int> ans;
             dfs(ans, graph, i, visited);
             sort(ans.begin(),ans.end());
             ancestors[i] = ans;
-            visited.clear();
         }
         return ancestors;
     }
