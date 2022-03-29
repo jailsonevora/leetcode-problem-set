95e503314e4cfa89a1db2ed24b3fb66ac2d3dd71
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-03-29 22:48:29 -0100

    1976

--- 1976.number-of-ways-to-arrive-at-destination.cpp
+++ 1976.number-of-ways-to-arrive-at-destination.cpp
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode id=1976 lang=cpp
  *
- * [1976] Number of Ways to Arrive at Destination
+ * {1976} Number of Ways to Arrive at Destination
  */
 
 #include<iostream>
@@ -19,5 +19,21 @@ public:
 };
 // @lc code=end
 int main(){
-    
+
+    int n = 7;
+
+    vector<vector<int>> roads = {
+        {0,6,7},
+        {0,1,2},
+        {1,2,3},
+        {1,3,3},
+        {6,3,3},
+        {3,5,1},
+        {6,5,1},
+        {2,5,1},
+        {0,4,5},
+        {4,6,2}
+    };
+
+
 }
