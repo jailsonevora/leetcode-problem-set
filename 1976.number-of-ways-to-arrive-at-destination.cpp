6d796508eb6bc1cefc5a51cd987a769666e34515
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-03-29 22:43:11 -0100

    1976

---
+++ 1976.number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1976 lang=cpp
+ *
+ * [1976] Number of Ways to Arrive at Destination
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        
+    }
+};
+// @lc code=end
+
