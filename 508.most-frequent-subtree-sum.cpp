b74e5533779df375dccf0ea0d1e1d8302fcf484d
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-04-11 22:56:20 -0100

    508

---
+++ 508.most-frequent-subtree-sum.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=508 lang=cpp
+ *
+ * [508] Most Frequent Subtree Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        
+    }
+};
+// @lc code=end
+
